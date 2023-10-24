dc469ec4409e2855888a8df868e39f9578bb262d
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-24 14:07:37 -0300

    fix: ident

--- src/App.vue
+++ src/App.vue
@@ -29,15 +29,13 @@
   // IMPORTING COMPONENTS //
   //////////////////////////
   
-import ButtonBlockList from './templates/button-block-list/index.vue';
-import FeaturedPost from './templates/article/featured-post/index.vue';
-import Footer from './templates/footer/index.vue';
-import PostCardGridList from './templates/article/card-grid-list/index.vue';
-import NewsletterSingleLine from './templates/newsletter/single-line/index.vue';
-import NewsletterWide from './templates/newsletter/wide/index.vue';
-import TitleFlexList from './templates/article/title-grid-list/index.vue';
-
-
+  import ButtonBlockList from './templates/button-block-list/index.vue';
+  import FeaturedPost from './templates/article/featured-post/index.vue';
+  import Footer from './templates/footer/index.vue';
+  import PostCardGridList from './templates/article/card-grid-list/index.vue';
+  import NewsletterSingleLine from './templates/newsletter/single-line/index.vue';
+  import NewsletterWide from './templates/newsletter/wide/index.vue';
+  import TitleFlexList from './templates/article/title-grid-list/index.vue';
 
   ////////////////////////
   // MOCKING PARAMETERS /
