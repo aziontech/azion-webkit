5dc673b1a3bcccd4ab5a11584515285abdc4ed30
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-24 13:54:59 -0300

    chore: rename files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import ButtonBlockList from './templates/ButtonBlockList/index.vue';
-import FeaturedPost from './templates/Article/FeaturedPost/index.vue';
-import Footer from './templates/Footer/index.vue';
-import PostCardGridList from './templates/Article/CardGridList/index.vue';
-import NewsletterSingleLine from './templates/Newsletter/SingleLine/index.vue';
-import NewsletterWide from './templates/Newsletter/Wide/index.vue';
-import TitleFlexList from './templates/Article/TitleGridList/index.vue';
+import ButtonBlockList from './templates/button-block-list/index.vue';
+import FeaturedPost from './templates/article/featured-post/index.vue';
+import Footer from './templates/footer/index.vue';
+import PostCardGridList from './templates/article/card-grid-list/index.vue';
+import NewsletterSingleLine from './templates/newsletter/single-line/index.vue';
+import NewsletterWide from './templates/newsletter/wide/index.vue';
+import TitleFlexList from './templates/article/title-grid-list/index.vue';
 
 export default {};
 
