a12e6f70d3300df44c0e7abc7070e8bbfbd9836c
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-30 13:05:06 -0300

    feat: pageBlogPost top storybook

---
+++ src/stories/PageBlogPost.stories.js
@@ -0,0 +1,13 @@
+import BlogPost from '../views/BlogPost.vue';
+
+export default {
+  title: 'Pages/BlogPost',
+  component: BlogPost,
+  tags: ['autodocs'],
+  argTypes: {}
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/vue/writing-stories/args
+export const Default = {
+  args: {}
+};
