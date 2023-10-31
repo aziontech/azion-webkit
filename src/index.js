be2139327e6ab46de0cd39ccb16e84aca692e620
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-31 10:36:51 -0300

    chore: adjust exporting components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 
 import ButtonBlockList from './templates/button-block-list/index.vue';
+import CardGridList from './templates/article/card-grid-list/index.vue'
+// import Categories from './templates/categories/index.vue'
 import FeaturedPost from './templates/article/featured-post/index.vue';
 import Footer from './templates/footer/index.vue';
 import PostCardGridList from './templates/article/card-grid-list/index.vue';
 import NewsletterSingleLine from './templates/newsletter/single-line/index.vue';
+// import NewsletterCard from './templates/newsletter/single-line/index.vue';
 import NewsletterWide from './templates/newsletter/wide/index.vue';
 import TitleFlexList from './templates/article/title-grid-list/index.vue';
 
@@ -11,6 +14,8 @@ export default {};
 
 export {
     ButtonBlockList,
+    CardGridList,
+    // Categories,
     FeaturedPost,
     Footer,
     PostCardGridList,
