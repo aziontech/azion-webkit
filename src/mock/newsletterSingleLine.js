75f0df5da9cfc75e90d999a0cf87aecaa8ae8e4b
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-31 10:10:09 -0300

    fix: param buttonText to NewsletterSingleLine

--- src/mock/newsletterSingleLine.js
+++ src/mock/newsletterSingleLine.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Become an expert in Edge Computing',
-    textButton: 'subscribe our newsletter',
+    buttonText: 'subscribe our newsletter',
     placeholderInput: 'Your e-mail',
     onSubmit: function() {
         console.log('[NewsletterSingleLine] onSubmit');
