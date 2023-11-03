abd4ea3a44dc2b3152b305fb05f9ceb940d6fbba
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-11-03 13:22:13 -0300

    feat: adicionado theme no storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,8 +3,10 @@
 // required //
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/lara-light-blue/theme.css';
-import '../src/assets/styles/main.css';
+// import 'primevue/resources/themes/lara-light-blue/theme.css';
+import '../src/assets/main.css';
+import '../src/assets/themes/scss/themes/azion-light/theme.scss'
+import '../src/assets/themes/scss/themes/azion-dark/theme.scss'
 //end required //
 
 const preview = {
@@ -20,3 +22,18 @@ const preview = {
 }
 
 export default preview
+
+export const decorators = [
+    (Story) => {
+      const addCustomClassToSbShowMain = () => {
+        const sbShowMainElement = document.querySelector('.sb-show-main');
+        if (sbShowMainElement) {
+          sbShowMainElement.classList.add('azion-dark');
+        }
+      };
+  
+      addCustomClassToSbShowMain();
+      return Story();
+    },
+  ];
+  
