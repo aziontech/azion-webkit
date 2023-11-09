e81cfa89a5ba248ccd69311a1bfd589e3cf63a4d
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-11-09 16:18:28 -0300

    fix: storybook with azion theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,9 +5,25 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 
 import '../src/assets/main.css';
+// import "primevue/resources/themes/lara-light-blue/theme.css";
 import '../src/assets/themes/scss/themes/azion-light/theme.scss';
 import '../src/assets/themes/scss/themes/azion-dark/theme.scss';
 //end required //
+
+(function setTheme(theme) {
+    let html = window.document.querySelector('html');
+    html.classList.add(`azion-${theme}`);
+})('dark');
+
+setTimeout(function() {
+    let sbdocs = document.querySelectorAll('.sbdocs');
+    if(sbdocs.length) {
+        for(var i = 0; i <= sbdocs.length; i++) {
+            let item = sbdocs[i];
+            item.style.background = 'transparent';
+        }
+    }
+}, 1000)
 
 const preview = {
     parameters: {
@@ -22,3 +38,5 @@ const preview = {
 }
 
 export default preview
+
+
