208d41e2652941dab772f3cc7560f2662c761156
Luis Igreja <lfsigreja@gmail.com>
2023-11-14 17:13:10 -0300

    FEAT: New Link Button component + link button list block

---
+++ src/components/link-button/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<a :href="link" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-sm min-w-fit no-underline">
+		{{ text }}
+	</a>
+</template>
+
+<script setup>
+const props = defineProps({
+	text: {
+		type: String,
+		required: true
+	},
+	link: {
+		type: String,
+		required: true
+	}
+});
+
+const { text } = props;
+</script>
---
+++ src/templates/button-block-list/link.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-wrap gap-4">
+    <LinkButton v-for="({text, link}, index) in data" :key="index" :text="text" :link="link" />
+  </div>
+</template>
+<script setup>
+import LinkButton from '../../components/link-button/index.vue'
+
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true
+  },
+});
+
+const { data } = props;
+
+// interface data {
+//   text: String,
+//   link: String
+// }
+</script>
