e0dbc1821f25dbcddc8a9c90e78cad054f80000d
Luis Igreja <lfsigreja@gmail.com>
2023-11-14 17:13:35 -0300

    CHORE: Export all new blocks + overline component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 
 import ButtonBlockList from './templates/button-block-list/index.vue';
+import LinkButtonBlockList from './templates/button-block-list/link.vue';
+import SocialButtonBlockList from './templates/button-block-list/social.vue';
 import CardGridList from './templates/card-grid-list/index.vue'
 import FeaturedPost from './templates/featured-post/index.vue';
 import PostCardGridList from './templates/card-grid-list/index.vue';
@@ -12,17 +14,22 @@ import TitleVerticalList from './templates/title-grid-list/vertical.vue';
 import ArticleHero from "./templates/post/hero/index.vue";
 import ReadableContent from "./templates/post/readable-content/index.vue";
 
+import Overline from "./components/overline/index.vue"
+
 export {
-    ButtonBlockList,
-    CardGridList,
-    Footer,
-    FeaturedPost,
-    PostCardGridList,
-    NewsletterSingleLine,
-    NewsletterWide,
-    TitleFlexList,
-    ArticleHero,
-    ReadableContent,
-    TitleVerticalList,
-    NewsletterCard,
+  ButtonBlockList,
+  CardGridList,
+  Footer,
+  FeaturedPost,
+  PostCardGridList,
+  NewsletterSingleLine,
+  NewsletterWide,
+  TitleFlexList,
+  ArticleHero,
+  ReadableContent,
+  TitleVerticalList,
+  NewsletterCard,
+  Overline,
+  LinkButtonBlockList,
+  SocialButtonBlockList
 }
