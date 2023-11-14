58e714bb3d983e014c09a5efc66b3638e6dee901
Luis Igreja <lfsigreja@gmail.com>
2023-11-14 17:12:08 -0300

    CHORE: Adjustments on padding and image size

--- src/templates/post/hero/index.vue
+++ src/templates/post/hero/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="container flex mx-auto gap-8 mb-8">
-      <div class="w-full">
+  <div class="w-full surface-section">
+    <div class="container flex mx-auto lg:gap-20">
+      <div class="w-full lg:w-2/3 pt-12">
         <div class="flex justify-between">
           <div class="flex">
             <Tag class="mr-2" v-for="(tag, index) in tagList" :key="index" :value="tag" severity="info" />
@@ -21,9 +22,10 @@
           </p>
         </div>
       </div>
-      <figure class="hidden lg:block p-0 m-0 min-w-fit" v-if="postImage">
-        <img :alt="postImage.alt" class="h-auto w-full" :src="postImage.url">
+      <figure class="hidden lg:block p-0 m-0 w-1/3" v-if="postImage">
+        <img :alt="postImage.alt" class="h-fit w-fit" :src="postImage.url">
       </figure>
+    </div>
     </div>
 </template>
 
