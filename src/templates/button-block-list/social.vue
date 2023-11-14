e639f18c893ac803eb27a70d300037b23312a0bb
Luis Igreja <lfsigreja@gmail.com>
2023-11-14 17:10:49 -0300

    feat: social block button list

---
+++ src/templates/button-block-list/social.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex gap-2">
+    <a href="https://twitter.com/aziontech" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
+      <span class="p-button-icon pi pi-twitter" data-pc-section="icon"></span>
+      <span class="p-button-label" data-pc-section="label">&nbsp;</span>
+    </a>
+    <a href="https://www.linkedin.com/company/aziontech/" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
+      <span class="p-button-icon pi pi-linkedin" data-pc-section="icon"></span>
+      <span class="p-button-label" data-pc-section="label">&nbsp;</span>
+    </a>
+    <a href="https://www.facebook.com/aziontech" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
+      <span class="p-button-icon pi pi-facebook" data-pc-section="icon"></span>
+      <span class="p-button-label" data-pc-section="label">&nbsp;</span>
+    </a>
+    <a href="https://discord.com/invite/Yp9N7RMVZy" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
+      <span class="p-button-icon pi pi-discord" data-pc-section="icon"></span>
+      <span class="p-button-label" data-pc-section="label">&nbsp;</span>
+    </a>
+    <a href="https://www.instagram.com/aziontech/" target="_blank" rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
+      <span class="p-button-icon pi pi-instagram" data-pc-section="icon"></span>
+      <span class="p-button-label" data-pc-section="label">&nbsp;</span>
+    </a>
+  </div>
+</template>
+<script setup>
+
+// const props = defineProps({
+//   data: {
+//     type: Array,
+//     required: true
+//   },
+// });
+
+// const { data } = props;
+
+// interface data {
+//   text: String,
+//   link: String
+// }
+</script>
