3d6efc025e5b99797b09ae0db312739b62d1232c
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-11-16 14:03:39 -0300

    fix: padding of NewsletterWide

--- src/templates/newsletter/wide/index.vue
+++ src/templates/newsletter/wide/index.vue
@@ -1,39 +1,41 @@
 <template>
     <section class="newsletter-signup surface-section">
-        <div class="lg:px-40 lg:py-28 py-8 px-4">
-            <div class="lg:flex justify-between lg:gap-4">
-                <div class="lg:w-2/4">
-                    <Overline v-if="props.overline">
-                        {{ props.overline }}
-                    </Overline>
-                    <h3 class="text-6xl font-normal text-color max-w-xl mt-2 mb-0">
-                        {{ props.title }}
-                    </h3>
-                </div>
-                <div class="lg:w-2/4">
-                    <form
-                        :id="formIdElement"
-                        :name="formIdElement"
-                        class="mt-8">
+        <div class="container mx-auto">
+          <div class="lg:py-28 py-8">
+              <div class="lg:flex justify-between lg:gap-4">
+                  <div class="lg:w-2/4">
+                      <Overline v-if="props.overline">
+                          {{ props.overline }}
+                      </Overline>
+                      <h3 class="text-6xl font-normal text-color max-w-xl mt-2 mb-0">
+                          {{ props.title }}
+                      </h3>
+                  </div>
+                  <div class="lg:w-2/4">
+                      <form
+                          :id="formIdElement"
+                          :name="formIdElement"
+                          class="mt-8">
 
-                        <InputText
-                            class='min-w-full'
-                            :placeholder="props.placeholderInput" />
-                        <div class="flex lg:gap-4 mt-4 gap-2">
-                            <InputText
-                                type="e-mail"
-                                class='w-1/2'
-                                :placeholder="props.placeholderEmail" />
-                            <Button
-                                icon="pi pi-chevron-right"
-                                iconPos="right" @click.prevent="onSubmit"
-                                type="submit" class="w-1/2"
-                                :label="buttonText" size="small"
-                              />
-                        </div>
-                    </form>
-                </div>
-            </div>
+                          <InputText
+                              class='min-w-full'
+                              :placeholder="props.placeholderInput" />
+                          <div class="flex lg:gap-4 mt-4 gap-2">
+                              <InputText
+                                  type="e-mail"
+                                  class='w-1/2'
+                                  :placeholder="props.placeholderEmail" />
+                              <Button
+                                  icon="pi pi-chevron-right"
+                                  iconPos="right" @click.prevent="onSubmit"
+                                  type="submit" class="w-1/2"
+                                  :label="buttonText" size="small"
+                                />
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
         </div>
     </section>
 </template>
