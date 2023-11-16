60337db84a4f3148006fd269f12adda412f163c1
Luis Igreja <lfsigreja@gmail.com>
2023-11-16 15:29:12 -0300

    CHORE: Prose custom colors

--- src/templates/post/readable-content/index.vue
+++ src/templates/post/readable-content/index.vue
@@ -3,3 +3,15 @@
     <slot />
   </article>
 </template>
+
+<style lang="scss">
+.prose *:not(a, a > u) {
+  color: var(--text-color) !important;
+}
+
+.prose {
+  a, a > u {
+    color: #F3652B !important;
+  }
+}
+</style>
