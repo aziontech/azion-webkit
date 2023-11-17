86abf8ed172f2f2dffe7f1dfa4344ae256ede05a
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-11-17 14:49:26 -0300

    fix: config ident

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,56 +2,56 @@
 import typography from '@tailwindcss/typography';
 
 export default {
-    important: true,
-    content: [
-        './src/**/*.{html,md,mdx,vue}',
-        'index.html'
-    ],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            transitionProperty: {
-                width: "width"
-            },
-            width: {
-                'slide': '300px',
-            },
-            padding: {
-              '5%': '5%',
-            },
-            colors: {
-                header: '#171717',
-                'header-input': '#292929',
-                'header-button-enabled': '#ffffff32',
-                'header-button-hover': 'rgba(244, 244, 244, 0.04)',
-                'header-avatar': '#363636',
-                footer: '#1e1e1e'
-            },
-            borderColor: {
-                header: '#3e3e3e',
-                'header-hover': '#F3652B'
-            },
-                textColor: {
-                header: '#b5b5b5',
-                footer: '#CCCCCC'
-            },
-            animation: {
-                fadeIn: 'fadeIn 220ms ease-in-out',
-                fadeOut: 'fadeOut 220ms ease-in-out'
-            },
-            keyframes: {
-                fadeIn: {
-                    '0%': { opacity: '0' },
-                    '100%': { opacity: '1' }
-                },
-                fadeOut: {
-                    '0%': { opacity: '1' },
-                    '100%': { opacity: '0' }
-                }
-            },
+  important: true,
+  content: [
+    './src/**/*.{html,md,mdx,vue}',
+    'index.html'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      transitionProperty: {
+        width: "width"
+      },
+      width: {
+        'slide': '300px',
+      },
+      padding: {
+        '5%': '5%',
+      },
+      colors: {
+        header: '#171717',
+        'header-input': '#292929',
+        'header-button-enabled': '#ffffff32',
+        'header-button-hover': 'rgba(244, 244, 244, 0.04)',
+        'header-avatar': '#363636',
+        footer: '#1e1e1e'
+      },
+      borderColor: {
+        header: '#3e3e3e',
+        'header-hover': '#F3652B'
+      },
+      textColor: {
+        header: '#b5b5b5',
+        footer: '#CCCCCC'
+      },
+      animation: {
+        fadeIn: 'fadeIn 220ms ease-in-out',
+        fadeOut: 'fadeOut 220ms ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
         },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' }
+        }
+      },
     },
-    plugins: [
-      typography,
-    ],
+  },
+  plugins: [
+    typography,
+  ],
 }
