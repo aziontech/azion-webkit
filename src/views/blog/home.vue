93b9b8b90e67b36e8d407a077e3da127e348d4db
Luis Igreja <lfsigreja@gmail.com>
2023-11-22 21:58:21 -0300

    FIX: Home error

--- src/views/blog/home.vue
+++ src/views/blog/home.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <div class="py-8 mb-20">
+    <div class="py-8 mb-20 surface-ground">
       <FeaturedPost
         :imgSrc="featuredPostData.imgSrc"
         :imgAlt="featuredPostData.imgAlt"
@@ -12,16 +12,12 @@
         :linkTarget="featuredPostData.linkTarget" />
     </div>
 
-    <div class="container mx-auto py-8">
-      <TitleFlexList :dataList="titleFlexListData" />
-    </div>
-
     <div class="my-20">
       <NewsletterSingleLine
-        :title="newsletterSingleLineData.title"
-        :buttonText="newsletterSingleLineData.buttonText"
-        :placeholderInput="newsletterSingleLineData.placeholderInput"
-        :onSubmit="newsletterSingleLineData.onSubmit" />
+        :title="newsletterData.title"
+        :buttonText="newsletterData.buttonText"
+        :placeholderInput="newsletterData.placeholderInput"
+        :onSubmit="newsletterData.onSubmit" />
     </div>
 
     <section>
@@ -37,11 +33,10 @@
     </section>
       <NewsletterWide
         class="py-8"
-        :overline="newsletterWideData.overline"
-        :title="newsletterWideData.title"
-        :buttonText="newsletterWideData.buttonText"
-        :placeholderInput="newsletterWideData.placeholderInput"
-        :onSubmit="newsletterWideData.onSubmit"
+        :overline="newsletterData.overline"
+        :title="newsletterData.title"
+        :buttonText="newsletterData.buttonText"
+        :placeholderInput="newsletterData.placeholderInput"
         />
   </main>
 
@@ -58,17 +53,14 @@
   import PostCardGridList from '../../templates/card-grid-list/index.vue';
   import NewsletterSingleLine from '../../templates/newsletter/single-line/index.vue';
   import NewsletterWide from '../../templates/newsletter/wide/index.vue';
-  import TitleFlexList from '../../templates/title-grid-list/index.vue';
 
   ////////////////////////
   // MOCKING PARAMETERS /
   ///////////////////////
 
   import featuredPostData from '../../mock/featuredPost.js';
-  import titleFlexListData from '../../mock/titleFlexListData.js';
   import buttonBlockListData from '../../mock/buttonBlockList.js';
-  import newsletterSingleLineData from '../../mock/newsletterSingleLine.js';
-  import newsletterWideData from '../../mock/newsletterWide.js';
+  import newsletterData from '../../mock/newsletterSingleLine.js';
   import footerData from '../../mock/footer.js';
   import cardData from '../../mock/cards.js';
 </script>
