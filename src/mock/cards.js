90b4e5a440dd6085691c1ebb0af963e1777d2825
Luis Igreja <lfsigreja@gmail.com>
2023-11-22 21:31:15 -0300

    CHORE: New card/cardlist storybook view

--- src/mock/cards.js
+++ src/mock/cards.js
@@ -1,90 +1,58 @@
 export default [
   {
-    "image": "/static/images/uploads/thumbblog-limitpayloadsize.png",
-    "title": "Azion Announces Solution That Blocks Oversized Requests",
-    "description": "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
-    "link": "/en/blog/azion-announces-solution-that-blocks-oversized-requests/"
+    image: '/static/images/uploads/thumbblog-limitpayloadsize.png',
+    title: 'Azion Announces Solution That Blocks Oversized Requests',
+    description: "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
+    link: '/en/blog/azion-announces-solution-that-blocks-oversized-requests/',
+    tagList: [ 'security', 'developers' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Vivian Seixas' }
   },
   {
-    "image": "/static/images/uploads/thumb-blog-multiport-kv.png",
-    "title": "Simultaneous Multiport Support Makes Your Edge Migration Easier",
-    "description": "New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.",
-    "link": "/en/blog/edge-app-multiport-support/"
+    image: '/static/images/uploads/thumb-blog-multiport-kv.png',
+    title: 'Simultaneous Multiport Support Makes Your Edge Migration Easier',
+    description: 'New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.',
+    link: '/en/blog/edge-app-multiport-support/',
+    tagList: [ 'developers', 'company-news' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Rafael Rigues' }
   },
   {
-    "image": "/static/images/uploads/thumb-blog-cipher-suites-1-1.png",
-    "title": "Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge",
-    "description": "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
-    "link": "/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/"
+    image: '/static/images/uploads/thumb-blog-cipher-suites-1-1.png',
+    title: 'Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge',
+    description: "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
+    link: '/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/',
+    tagList: [ 'security' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Isidro Iturat Hernández' }
   },
   {
-    "image": "/static/images/uploads/thumbblog-limitpayloadsize.png",
-    "title": "Azion Announces Solution That Blocks Oversized Requests",
-    "description": "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
-    "link": "/en/blog/azion-announces-solution-that-blocks-oversized-requests/"
+    image: '/static/images/blog/edge-computing-trends/featured-img.png',
+    title: 'The Main Trends for Edge Computing',
+    description: 'Follow the trends of Edge Computing and see how your company can stand out by following them.',
+    link: '/en/blog/main-trends-edge-computing/',
+    tagList: [ 'serverless' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Azion Technologies' }
   },
   {
-    "image": "/static/images/uploads/thumb-blog-multiport-kv.png",
-    "title": "Simultaneous Multiport Support Makes Your Edge Migration Easier",
-    "description": "New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.",
-    "link": "/en/blog/edge-app-multiport-support/"
+    image: '/static/images/uploads/thumb-blog-let-s-encrypt-kv.png',
+    title: "Now It's Much Easier to Get a Let's Encrypt TLS Certificate at Azion",
+    description: "TLS certificates at your fingertips! See how much easier and faster it is to generate or renew a free SSL certificate from Let's Encrypt at Azion's Edge Computing Platform.",
+    link: '/en/blog/now-its-much-easier-to-get-a-lets-encrypt-tls-certificate-at-azion/',
+    tagList: [ 'security', 'developers' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Vivian Seixas' }
   },
   {
-    "image": "/static/images/uploads/thumb-blog-cipher-suites-1-1.png",
-    "title": "Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge",
-    "description": "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
-    "link": "/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/"
+    image: '/static/images/uploads/origin-shield-now-available-for-real-time-manager-and-api.png',
+    title: 'Origin Shield, Available for RTM and API',
+    description: 'Automate the configuration of your firewall rules and release or block access at its source only to specific IP addresses.',
+    link: '/en/blog/origin-shield-available-rtm-api/',
+    tagList: [ 'security' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Rodrigo Costa' }
   },
   {
-    "image": "/static/images/uploads/thumbblog-limitpayloadsize.png",
-    "title": "Azion Announces Solution That Blocks Oversized Requests",
-    "description": "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
-    "link": "/en/blog/azion-announces-solution-that-blocks-oversized-requests/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-multiport-kv.png",
-    "title": "Simultaneous Multiport Support Makes Your Edge Migration Easier",
-    "description": "New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.",
-    "link": "/en/blog/edge-app-multiport-support/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-cipher-suites-1-1.png",
-    "title": "Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge",
-    "description": "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
-    "link": "/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/"
-  }, {
-    "image": "/static/images/uploads/thumbblog-limitpayloadsize.png",
-    "title": "Azion Announces Solution That Blocks Oversized Requests",
-    "description": "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
-    "link": "/en/blog/azion-announces-solution-that-blocks-oversized-requests/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-multiport-kv.png",
-    "title": "Simultaneous Multiport Support Makes Your Edge Migration Easier",
-    "description": "New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.",
-    "link": "/en/blog/edge-app-multiport-support/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-cipher-suites-1-1.png",
-    "title": "Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge",
-    "description": "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
-    "link": "/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/"
-  }, {
-    "image": "/static/images/uploads/thumbblog-limitpayloadsize.png",
-    "title": "Azion Announces Solution That Blocks Oversized Requests",
-    "description": "Find out why blocking oversized requests simplifies operations and gives you a competitive advantage. Get more security and performance for your applications with Azion's Limit Payload Size solution.",
-    "link": "/en/blog/azion-announces-solution-that-blocks-oversized-requests/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-multiport-kv.png",
-    "title": "Simultaneous Multiport Support Makes Your Edge Migration Easier",
-    "description": "New feature gives you more flexibility in the way your applications are delivered, and helps to avoid extra development costs.",
-    "link": "/en/blog/edge-app-multiport-support/"
-  },
-  {
-    "image": "/static/images/uploads/thumb-blog-cipher-suites-1-1.png",
-    "title": "Discover How Azion Cipher Suites Manages Your TLS Ciphers at the Edge",
-    "description": "Now Azion's graphical interface, Real-Time Manager, allows you to easily set up cipher suites for your edge applications with just a few clicks.",
-    "link": "/en/blog/how-cipher-suites-manages-tls-ciphers-on-the-edge/"
+    image: '/static/images/uploads/thumbblog-10-technologies-mobile-1.png',
+    title: 'Three Edge-Driven Tech Trends Reshaping the Future of IT',
+    description: 'Discover three tech trends driven by edge computing and that will be great agents of evolution in IT: digital super resilience, Zero Trust, and phygital.',
+    link: '/en/blog/three-edge-driven-tech-trends-reshaping-future-it-and-transforming-business/',
+    tagList: [ 'market-trends' ],
+    authors: { avatars: ['https://assets.azion.com/static/images/uploads/vivian-seixas2.png'], title: 'Isidro Iturat Hernández' }
   }
 ]
--- src/templates/card-grid-list/index.vue
+++ src/templates/card-grid-list/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container grid m-0 md:grid-cols-2 lg:grid-cols-3 gap-y-16">
+  <div class="container surface-ground grid m-0 md:grid-cols-2 lg:grid-cols-3 gap-y-16">
       <Card v-for="({ image, link, title, description, tagList, authors }, index) in data" :href="link" :key="index" :imgSrc="image" :imgAlt="title" :tagList="tagList" :title="title" :description="description" :authors="authors" />
   </div>
 </template>
