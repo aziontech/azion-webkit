abbff094da07a8f30177d89f8c76265013059ae6
Luis Igreja <lfsigreja@gmail.com>
2023-11-24 14:19:25 -0300

    CHORE: Vertical title list  style

--- src/templates/title-grid-list/vertical.vue
+++ src/templates/title-grid-list/vertical.vue
@@ -1,24 +1,24 @@
 <template>
-  <ul class="list-none p-0 gap-4 lg:gap-11 flex flex-col">
-    <li v-for="({tagList, title, description, link, linkTarget}, index) in props.data" :key="index">
-      <a :href="link" :title="title" :target="linkTarget" class="block no-underline text-current mb-2 group">
-        <Tag class="mr-2" v-for="(tag, index) in tagList" :key="index" :value="tag" severity="info" />
-        <div class="hover:underline my-2">
-          <p class="lg:text-2xl font-bold text-color">{{ title }}</p>
-          <p class="truncate text-small font-normal text-color-secondary">{{ description }}</p>
-        </div>
-      </a>
-    </li>
-  </ul>
+  <section class="flex flex-col gap-4">
+    <ul class="list-none p-0 gap-2 flex flex-col">
+      <li v-for="({ title, link, linkTarget }, index) in props.data" :key="index">
+        <a :href="link" :title="title" :target="linkTarget ? linkTarget : '_self'" class="block no-underline text-current group">
+          <div class="hover:underline">
+            <p class="text-sm text-color-secondary">{{ title }}</p>
+          </div>
+        </a>
+      </li>
+    </ul>
+    <div>
+      <LinkButton class="p-button p-button-outlined" severity="primary" :label="buttonText" outlined :link="link" />
+    </div>
+  </section>
 </template>
 <script setup>
-import Tag from 'primevue/tag'
+import LinkButton from '../button/link.vue';
 
 // interface listItem {
-//   tagList: Array<string>,
 //   title: string,
-//   description: String,
-//   buttonText: String,
 //   link: String,
 //   linkTarget: _self, _blank
 // }
@@ -26,6 +26,15 @@ import Tag from 'primevue/tag'
 const props = defineProps({
   data: {
     type: Array,
+    required: true
+  },
+  buttonText: {
+    type: String,
+    required: false,
+    default: "See all posts"
+  },
+  link: {
+    type: String,
     required: true
   }
 })
