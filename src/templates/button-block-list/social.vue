6202542b1c5eab79a1ddec95dde82984ed32a5eb
Luis Igreja <lfsigreja@gmail.com>
2023-11-24 14:18:40 -0300

    CHORE: New Sharer  block style

--- src/templates/button-block-list/social.vue
+++ src/templates/button-block-list/social.vue
@@ -1,27 +1,22 @@
 <template>
-  <div class="flex gap-2">
-    <a :href="`https://www.facebook.com/sharer/sharer.php?u=${sharedURL}`" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
-      <span class="p-button-icon pi pi-facebook" data-pc-section="icon"></span>
-    </a>
-    <a :href="`https://x.com/intent/tweet?url=${sharedURL}&via=@aziontech`" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
-      <span class="p-button-icon pi pi-twitter" data-pc-section="icon"></span>
-    </a>
-    <a :href="`https://www.linkedin.com/sharing/share-offsite/?url=${sharedURL}`" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
-      <span class="p-button-icon pi pi-linkedin" data-pc-section="icon"></span>
-    </a>
-    <!-- <a href="https://discord.com/invite/Yp9N7RMVZy" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
-      <span class="p-button-icon pi pi-discord" data-pc-section="icon"></span>
-    </a>
-    <a href="https://www.instagram.com/aziontech/" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-icon-only p-button-text p-button-sm no-underline">
-      <span class="p-button-icon pi pi-instagram" data-pc-section="icon"></span>
-    </a> -->
-    <div id="sharedLink" tabindex="-1" v-tooltip.focus.top="{ value: tooltipText, showDelay: 200 }">
-      <Button id="sharedLinkButton" size="sm" icon="pi pi-link" text @click="copyToClipboard" />
+  <div class="flex gap-3 flex-col font-medium">
+    <h3> {{ title }}</h3>
+    <div class="flex gap-2">
+      <a :href="`https://www.facebook.com/sharer/sharer.php?u=${sharedURL}`" target="_blank" rel="noopener noreferrer"
+        class="p-button p-component p-button-icon-only p-button-primary p-button-outlined">
+        <span class="p-button-icon pi pi-facebook" data-pc-section="icon"></span>
+      </a>
+      <a :href="`https://x.com/intent/tweet?url=${sharedURL}&via=@aziontech`" target="_blank" rel="noopener noreferrer"
+        class="p-button p-component p-button-icon-only p-button-primary p-button-outlined">
+        <span class="p-button-icon pi pi-twitter" data-pc-section="icon"></span>
+      </a>
+      <a :href="`https://www.linkedin.com/sharing/share-offsite/?url=${sharedURL}`" target="_blank"
+        rel="noopener noreferrer" class="p-button p-component p-button-icon-only p-button-primary p-button-outlined">
+        <span class="p-button-icon pi pi-linkedin" data-pc-section="icon"></span>
+      </a>
+      <div id="sharedLink" tabindex="-1" v-tooltip.focus.top="{ value: tooltipText, showDelay: 200 }">
+        <Button severity="primary" id="sharedLinkButton" icon="pi pi-link" @click="copyToClipboard" outlined />
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +31,11 @@ const props = defineProps({
   tooltipText: {
     type: String,
     required: true
+  },
+  title: {
+    type: String,
+    required: false,
+    default: "Share this post"
   }
 });
 
