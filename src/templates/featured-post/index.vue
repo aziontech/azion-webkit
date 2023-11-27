d989413efa8e980cb579a6e4b3230ade3e741358
Luis Igreja <lfsigreja@gmail.com>
2023-11-27 13:34:15 -0300

    CHORE: Add border radius to the featured post

--- src/templates/featured-post/index.vue
+++ src/templates/featured-post/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <a :href="link" :title="linkText" :target="linkTarget" class="container block">
+  <a :href="link" :title="linkText" :target="linkTarget" class="block">
     <div
-      class="flex flex-col lg:flex-row gap-4 lg:gap-8 border border-solid surface-border surface-card">
+      class="flex flex-col lg:flex-row gap-4 lg:gap-8 border border-solid surface-border surface-card rounded-md">
       <div class="h-full">
         <picture v-if="imgSrc" class="p-0 m-0">
           <img fetchpriority="high" class="w-full md:object-cover" width="576" height="324" :alt="imgAlt"
