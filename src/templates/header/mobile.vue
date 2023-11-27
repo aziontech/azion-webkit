d58df2fa962e86f42f8c7c4edc5ae1c85e9d6cf2
Robson Junior <robson.ga.junior@gmail.com>
2023-11-27 15:14:24 -0300

    FIX: large screens lateral padding

--- src/templates/header/mobile.vue
+++ src/templates/header/mobile.vue
@@ -1,10 +1,11 @@
 <template>
-  <Button outlined @click="visibleRight = true">
-    <i class="pi pi-bars"></i>
-  </Button>
-  <Sidebar v-model:visible="visibleRight" header="Right Sidebar" position="right">
-    <p>Azion 123</p>
-  </Sidebar>
+  <!-- <Button icon="pi pi-arrow-right" @click="visible = true" /> -->
+  <div class="card flex justify-content-center">
+    <Sidebar v-model:visible="visibleRight" header="Right Sidebar" position="right">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Sidebar>
+    <Button icon="pi pi-arrow-left" @click="visibleRight = true" />
+  </div>
 </template>
 
 <script setup>
