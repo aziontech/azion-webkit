dea3bb099da72e7d09592d58490b78aef15758aa
Luis Igreja <lfsigreja@gmail.com>
2023-11-27 11:25:31 -0300

    chore: set a negative margin to override padding

--- src/templates/post/hero/index.vue
+++ src/templates/post/hero/index.vue
@@ -2,7 +2,7 @@
   <div class="surface-section">
     <div class="container mx-auto py-6 lg:py-14 flex flex-col px-4">
       <div class="py-4 flex gap-4 lg:gap-8 flex-col w-full">
-        <Breadcrumb :model="breadcrumbItems" />
+        <Breadcrumb :model="breadcrumbItems" class="-ml-1" />
         <h1 class="text-5xl font-bold text-color">
           {{ title }}
         </h1>
