906fca87527f9f8a3fd24410ec75c7f097308d4e
Luis Igreja <lfsigreja@gmail.com>
2023-11-29 16:24:30 -0300

    FEAT: Empty result block

---
+++ src/templates/empty-state/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col h-full">
+    <div class="flex flex-col h-full border surface-border gap-7 justify-center items-center rounded-md">
+      <slot name="illustration" />
+      <div class="flex flex-col gap-2">
+        <p class="text-center text-color text-lg font-bold leading-7">
+          {{ title }}
+        </p>
+        <p class="text-center text-color-secondary text-sm font-normal leading-tight">
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'empty-results-block' })
+
+defineProps({
+  pageTitle: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+})
+
+</script>
