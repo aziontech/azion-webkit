9684b6b46e33c252cc0d53477d5bf26ce03ddbf7
Luis Igreja <lfsigreja@gmail.com>
2023-12-01 23:34:44 -0300

    CHORE: Added new card props

--- src/templates/card-grid-list/index.vue
+++ src/templates/card-grid-list/index.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="transition-all container surface-ground grid m-0 p-0 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 lg:gap-16 justify-between">
-      <Card v-for="({ image, link, title, description, tagList, authors }, index) in data" :href="link" :key="index" :imgSrc="image" :imgAlt="title" :tagList="tagList" :title="title" :description="description" :authors="authors" />
+      <Card v-for="({ image, link, title, description, tagList, authors, estimateReadTime, date }, index) in data" :link="link" :key="index" :imgSrc="image" :imgAlt="title" :tagList="tagList" :title="title" :description="description" :authors="authors" :estimateReadTime="estimateReadTime" :date="date" />
   </div>
 </template>
-
 
 <script setup>
 import Card from '../card/index.vue';
--- src/templates/card/index.vue
+++ src/templates/card/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <a :href="props.href" :title="props.title" class="no-underline flex group border-radius md:max-w-sm">
+  <a :href="link" :title="title" class="no-underline flex group border-radius md:max-w-sm">
     <Card class="p-0 mt-0"
       :pt="{ header: { class: 'group-hover:shadow-[0_0_0_0.2rem_rgba(243,101,43,0.32)] rounded' }, root: { class: 'shadow-none surface-ground flex flex-col gap-6' } }">
-      <template #header v-if="props.imgSrc">
-        <img class="w-full aspect-video rounded" lazy alt="`${imgAlt}`" :src="`https://assets.azion.com${props.imgSrc}?ims=372x209`" />
+      <template #header v-if="imgSrc">
+        <img class="w-full aspect-video rounded" lazy alt="`${imgAlt}`" :src="`https://assets.azion.com${imgSrc}?ims=372x209`" />
       </template>
       <template #content>
         <div class="flex flex-col justify-between gap-4">
-          <div class="flex gap-2" v-if="props.tagList?.length">
-            <Tag v-for="(tag, index) in props.tagList" :key="index" :value="tag" severity="info" />
+          <div class="flex gap-2" v-if="tagList?.length">
+            <Tag v-for="(tag, index) in tagList" :key="index" :value="tag" severity="info" />
           </div>
-          <h3 class="text-xl font-bold text-color">{{ props.title }}</h3>
-          <p class="text-sm text-color-secondary">{{ props.description }}</p>
-          <p class="text-xs text-color-secondary">JUL, 13, 2020 • 11min read</p>
+          <h3 class="text-xl font-bold text-color">{{ title }}</h3>
+          <p class="text-sm text-color-secondary">{{ description }}</p>
+          <p class="text-xs text-color-secondary">{{ date }} • {{ estimateReadTime }}</p>
           <div v-if="authors">
             <AvatarGroup>
               <Avatar v-for="(image, index) in authors.avatars" :key="index" :image="image" shape="circle" />
@@ -30,7 +30,7 @@ import Tag from 'primevue/tag';
 import Avatar from 'primevue/avatar'
 import AvatarGroup from 'primevue/avatargroup'
 
-const props = defineProps({
+defineProps({
   imgSrc: {
     type: String,
     required: false,
@@ -54,6 +54,18 @@ const props = defineProps({
   authors: {
     type: Object,
     required: false
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  estimateReadTime: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
   }
 });
 </script>
