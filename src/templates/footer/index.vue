82d91dbbaf3c191e9627244dad5ee44ad3676f9b
Luis Igreja <lfsigreja@gmail.com>
2023-12-04 16:32:14 -0300

    CHORE: Footer i18n Pages integration

--- src/templates/footer/index.vue
+++ src/templates/footer/index.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col lg:flex-row gap-9 lg:gap-12">
         <div class="w-full lg:w-1/4">
           <a :href="`/${lang}/`" title="Azion Technologies" class="mb-4 block">
-            <AznTechLogo />
+            <AzionLogo class="h-[26px]" />
           </a>
           <p>
             {{ cta.text }}
@@ -12,7 +12,6 @@
             <span class="text-[#F3652B]">{{ cta.phone }}</span>
           </p>
         </div>
-
         <div class="w-full lg:w-3/4 grid grid-cols-1 gap-8 md:grid-cols-5">
           <div :key="index" v-for="({ title, list }, index) in listData" :class="indicesListsLargerThanFive.includes(index) ? 'col-span-2' : 'col-span-1'">
             <p class="mb-4 w-full mt-0 text-xs text-color-secondary px-4">
@@ -36,8 +35,14 @@
           {{ copyright }}
         </p>
         <div class="flex gap-3">
-          <Dropdown v-if="i18nPages" :modelValue="selectedLanguague" v-model="selectedLanguague" :options="i18nPages"
-            optionLabel="lang" />
+          <Dropdown v-if="i18nPages" :options="i18nPages" :autoOptionFocus="false" optionLabel="lang"
+            :pt="{ item: { class: 'p-0' } }" :placeholder="modelValue.lang">
+            <template #option="slotProps">
+              <a :href="slotProps.option.slug" target="_self" class="w-full px-2 py-3">
+                {{ slotProps.option.lang }}
+              </a>
+            </template>
+          </Dropdown>
           <ButtonThemeToggle />
         </div>
       </div>
@@ -46,12 +51,11 @@
 </template>
 
 <script setup>
-import AznTechLogo from "../../assets/icons/azion-logo.vue";
+import AzionLogo from "../../assets/icons/azion-logo-default.vue";
 import Divider from 'primevue/divider';
 import ButtonThemeToggle from '../button-toggle/theme/index.vue';
 import LinkButton from '../button/link.vue'
 import Dropdown from 'primevue/dropdown';
-import { ref } from 'vue'
 
 // interface List {
 //   link: string,
@@ -98,7 +102,7 @@ const props = defineProps({
   }
 })
 
-const selectedLanguague = ref('English')
+const modelValue = props.i18nPages ? props.i18nPages.find(p => p.lang.toLowerCase() === props.lang.toLowerCase()) : null
 
 const indicesListsLargerThanFive = (props.listData || []).map((item, index) => {
   if (item.list.length <= 5) return -1;
