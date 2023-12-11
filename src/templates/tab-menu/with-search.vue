eed93f49c4b5148457974312b015f88e46381c72
Luis Igreja <lfsigreja@gmail.com>
2023-12-11 16:06:19 -0300

    FIX: Revert with-search dropdown changes

--- src/templates/tab-menu/with-search.vue
+++ src/templates/tab-menu/with-search.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="flex justify-between flex-col lg:flex-row">
     <div class="lg:w-2/3 w-ful">
-      <TabMenu class="hidden lg:block overflow-y-auto" :pt="{
+      <TabMenu class="hidden lg:block overflow-y-auto"
+      :pt="{
         label: 'whitespace-nowrap'
-      }" v-model:activeIndex="active" :model="mappedDataTabMenu" @click="$emit('categorieChanged', tabList[active])"
-    />
-      <Dropdown class="lg:hidden w-full" v-model="active.activeIndex" :options="mappedDataTabMenu" optionLabel="label">
-        <template #value="slotProps">
-          <div v-if="slotProps.value">
-            <div>{{ slotProps }}</div>
-          </div>
-        </template>
-      </Dropdown>
+      }"
+      v-model:activeIndex="active" :model="mappedDataTabMenu" @click="$emit('categorieChanged', tabList[active]);
+"/>
+      <Dropdown class="lg:hidden w-full" v-model="active" :options="tabList" />
     </div>
     <div class="w-full lg:w-1/3 flex justify-end pt-4 lg:pt-0">
       <span class="p-input-icon-left w-full lg:max-w-xs">
@@ -48,12 +44,11 @@ const props = defineProps({
 // }>
 // }
 
+const searchInput = ref('')
 const { tabList } = props
 
-const mappedDataTabMenu = tabList.map((item, index) => {
-  return { label: item, activeIndex: index }
+const mappedDataTabMenu = tabList.map(item => {
+  return { label: item }
 })
-
-const searchInput = ref(mappedDataTabMenu[0].activeIndex)
 
 </script>
