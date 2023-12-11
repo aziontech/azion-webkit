f6f6298df4be12731599f39ae0df0231ff509be1
Luis Igreja <lfsigreja@gmail.com>
2023-12-11 15:48:50 -0300

    CHORE: Remove size lg for icon buttons

--- src/templates/button/link-icon.vue
+++ src/templates/button/link-icon.vue
@@ -1,6 +1,6 @@
 <template>
   <a :href="link" target="_blank" rel="noopener noreferrer"
-    class="p-button p-component p-button-icon-only p-button-primary p-button-outlined p-button-lg"
+    class="p-button p-component p-button-icon-only p-button-primary p-button-outlined"
     v-tooltip.top="{ value: tooltipText, showDelay: 200 }"
   >
     <span :class="`p-button-icon ${icon}`" data-pc-section="icon"></span>
