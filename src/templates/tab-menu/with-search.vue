ef0eeed16973a6440eefe1296ad090ef8e916ed3
Luis Igreja <lfsigreja@gmail.com>
2023-12-11 16:04:53 -0300

    CHORE: Package upgrade

--- src/templates/tab-menu/with-search.vue
+++ src/templates/tab-menu/with-search.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex justify-between flex-col lg:flex-row">
     <div class="lg:w-2/3 w-ful">
-      <TabMenu class="hidden lg:block overflow-y-auto"
-      :pt="{
+      <TabMenu class="hidden lg:block overflow-y-auto" :pt="{
         label: 'whitespace-nowrap'
-      }"
-      v-model:activeIndex="active" :model="mappedDataTabMenu" @click="$emit('categorieChanged', tabList[active]);
-"/>
-      <Dropdown class="lg:hidden w-full" v-model="active" :options="tabList" />
+      }" v-model:activeIndex="active" :model="mappedDataTabMenu" @click="$emit('categorieChanged', tabList[active])"
+    />
+      <Dropdown class="lg:hidden w-full" v-model="active.activeIndex" :options="mappedDataTabMenu" optionLabel="label">
+        <template #value="slotProps">
+          <div v-if="slotProps.value">
+            <div>{{ slotProps }}</div>
+          </div>
+        </template>
+      </Dropdown>
     </div>
     <div class="w-full lg:w-1/3 flex justify-end pt-4 lg:pt-0">
       <span class="p-input-icon-left w-full lg:max-w-xs">
@@ -44,11 +48,12 @@ const props = defineProps({
 // }>
 // }
 
-const searchInput = ref('')
 const { tabList } = props
 
-const mappedDataTabMenu = tabList.map(item => {
-  return { label: item }
+const mappedDataTabMenu = tabList.map((item, index) => {
+  return { label: item, activeIndex: index }
 })
+
+const searchInput = ref(mappedDataTabMenu[0].activeIndex)
 
 </script>
