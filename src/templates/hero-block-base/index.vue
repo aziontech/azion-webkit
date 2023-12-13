cf14241e6214e209cee3b1e26a85897b25da67f3
Luis Igreja <lfsigreja@gmail.com>
2023-12-13 17:32:36 -0300

    FEAT: Hero block base

---
+++ src/templates/hero-block-base/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="py-16 surface-section">
+    <div class="px-container flex flex-col w-full gap-10" :class="[
+      { 'lg:flex-row': !centered },
+      { 'items-center text-center': centered },
+    ]">
+      <div class=" w-full flex flex-col gap-8" :class="[
+        { 'lg:max-w-xl': !centered },
+        { 'items-center text-center': centered },
+      ]">
+        <div class="flex flex-col gap-4" :class="[
+          { 'max-w-2xl': centered },
+        ]">
+          <Overline :label="overline" />
+          <h1 class="text-3xl">
+            {{ title }}
+          </h1>
+          <p class="text-color-secondary text-sm"> {{ description }}</p>
+          <div class="flex flex-row gap-3" :class="[
+            { 'justify-center': centered },
+          ]">
+          <slot name="buttons">
+
+          </slot>
+          </div>
+        </div>
+        <slot name="content" />
+      </div>
+      <div class="w-full">
+        <slot name="main" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import Overline from "../../templates/overline/index.vue"
+
+defineProps({
+  overline: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  centered: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+</script>
