8467cf9354224a31feab38fdd3bdf351090a4c2e
Luis Igreja <lfsigreja@gmail.com>
2023-12-14 15:59:58 -0300

    CHORE: Slots name update, actions slot created, text color and size fixes

--- src/templates/form-block/index.vue
+++ src/templates/form-block/index.vue
@@ -3,15 +3,15 @@
   <div class="border surface-border rounded-md max-w-xl">
     <div class="p-8 flex flex-col gap-8">
         <div class="flex flex-col gap-2">
-        <h5 class="text-xl"> {{ title }} </h5>
-        <p class="text-sm text-secondary-color"> {{ description }} </p>
+        <h5 class="text-xl font-medium"> {{ title }} </h5>
+        <p class="text-sm text-color-secondary"> {{ description }} </p>
       </div>
       <form>
-        <slot name="fieldset" />
+        <slot name="content" />
       </form>
     </div>
     <div class="py-4 px-8 border-t surface-border flex flex-row gap-3 justify-end">
-      <Button :label="buttonText" @click="onSubmit" size="small" />
+      <slot name="actions" />
     </div>
   </div>
 </div>
@@ -19,8 +19,6 @@
 </template>
 
 <script setup>
-import Button from "primevue/button";
-
 defineProps({
   title: {
     type: String,
@@ -29,16 +27,6 @@ defineProps({
   description: {
     type: String,
     required: true
-  },
-  onSubmit: {
-    type: Function,
-    required: true
-  },
-  buttonText: {
-    type: String,
-    required: false,
-    default: 'Submit'
   }
 })
-
 </script>
