c909fdcfa22de1011e421be7557a0534daf28db5
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2023-12-15 15:37:11 -0300

    fix: surface ground no fundo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,6 +12,16 @@ import '../src/assets/themes/scss/themes/azion-light/theme.scss';
 import '../src/assets/themes/scss/themes/azion-dark/theme.scss';
 //end required //
 
+setTimeout(function() {
+    let sbdocs = document.querySelectorAll('.docs-story');
+    if(sbdocs.length) {
+        for(var i = 0; i <= sbdocs.length; i++) {
+            let item = sbdocs[i];
+            item.style.background = 'surface-ground';
+        }
+    }
+}, 1000)
+
 function setTheme(theme) {
     let html = window.document.querySelector('html');
     html.classList.forEach((className) => {
@@ -20,7 +30,7 @@ function setTheme(theme) {
         }
     });
     
-    html.classList.add(`azion-${theme}`, 'surface-ground');
+    html.classList.add(`azion-${theme}`);
 }
 
 let currentTheme = 'dark';
@@ -45,16 +55,26 @@ const parameters = {
 };
 
 export const decorators = [
-    (Story, context) => {
-        const { globals } = context;
-        const { theme } = globals;
+  (Story, context) => {
+    const { globals } = context;
+    const { theme } = globals;
 
-        if (theme !== currentTheme) {
-            currentTheme = theme;
-            setTheme(currentTheme);
-        }
-        return Story();
-    },
+    if (theme !== currentTheme) {
+      currentTheme = theme;
+      setTheme(currentTheme);
+    }
+
+    // Ajuste o plano de fundo conforme necessário
+    document.body.style.background = 'transparent';
+
+    // Adiciona a classe 'surface-ground' ao elemento 'iframe' com a classe 'css-xzp052'
+    const iframe = document.querySelector('.css-xzp052');
+    if (iframe) {
+      iframe.classList.add('surface-ground');
+    }
+
+    return Story();
+  },
 ];
 
 export default parameters;
