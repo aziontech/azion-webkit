b90382e122f1f5ee2d3c81f798e2bd34f8ce24c5
Luis Igreja <lfsigreja@gmail.com>
2024-01-10 15:33:54 -0300

    FEAT: Error State Block

---
+++ src/templates/error-state-block/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="flex flex-col gap-20 pt-12 pb-28">
+      <div class="flex flex-col gap-10 items-center">
+        <slot name="illustration"></slot>
+        <div class="text-center flex flex-col gap-6 max-w-4xl	">
+          <h1 class="text-5xl	font-medium">
+            {{ title }}
+          </h1>
+          <p class="text-lg	text-color-secondary max-w-xl"> {{ description }} </p>
+        </div>
+        <slot name="actions"></slot>
+      </div>
+      <slot name="main"></slot>
+  </section>
+</template>
+
+<script setup>
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+})
+
+</script>
