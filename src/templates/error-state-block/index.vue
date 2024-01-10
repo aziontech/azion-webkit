856050af842ef7131b19b36366357fbe99e85d03
Luis Igreja <lfsigreja@gmail.com>
2024-01-10 16:17:12 -0300

    FIX: content div centralized and slot actions wrapper

--- src/templates/error-state-block/index.vue
+++ src/templates/error-state-block/index.vue
@@ -2,13 +2,15 @@
   <section class="flex flex-col gap-20 pt-12 pb-28">
       <div class="flex flex-col gap-10 items-center">
         <slot name="illustration"></slot>
-        <div class="text-center flex flex-col gap-6 max-w-4xl	">
+        <div class="text-center flex flex-col gap-6 max-w-4xl	items-center">
           <h1 class="text-5xl	font-medium">
             {{ title }}
           </h1>
           <p class="text-lg	text-color-secondary max-w-xl"> {{ description }} </p>
         </div>
-        <slot name="actions"></slot>
+        <div class="flex gap-3">
+          <slot name="actions"></slot>
+        </div>
       </div>
       <slot name="main"></slot>
   </section>
