0bd6b195f659881da3a0f86b97b116e9f4bcc57b
Luis Igreja <lfsigreja@gmail.com>
2024-01-10 16:16:14 -0300

    CHORE: Block added to Storybook

---
+++ src/stories/error-state-block.stories.js
@@ -0,0 +1,40 @@
+import ErrorStateBlock from '../templates/error-state-block/index.vue';
+import Error404Illustration from '../assets/svg/error-404.vue'
+import LinkButton from '../templates/button/link.vue';
+
+export default {
+  title: 'Blocks/Error Block Base',
+  component: ErrorStateBlock,
+  tags: ['autodocs'],
+  argsType: {
+    title: String,
+    overline: String,
+    description: String,
+    centered: Boolean
+  }
+}
+
+const Template = (args) => ({
+  components: { ErrorStateBlock, LinkButton, Error404Illustration },
+  setup() {
+    return { args }
+  },
+  template: ` <ErrorStateBlock v-bind="args">
+                <template #illustration>
+                  <Error404Illustration />
+                </template>
+                <template #main>
+                  <img src="src/assets/images/mocks/_slot.png" />
+                </template>
+                <template #actions>
+                  <LinkButton label="Button" link="./" />
+                  <LinkButton label="Button" link="./" outlined />
+                </template>
+              </ErrorStateBlock>`
+})
+
+export const Default = Template.bind({});
+Default.args = {
+    title: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariata',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con',
+}
