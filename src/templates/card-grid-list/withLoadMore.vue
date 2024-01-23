4cc0fb583351d7648c2e709a95a1633b6bcbe8bb
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2024-01-23 10:09:43 -0300

    FIX: Increase load more count

--- src/templates/card-grid-list/withLoadMore.vue
+++ src/templates/card-grid-list/withLoadMore.vue
@@ -42,7 +42,7 @@ const props = defineProps({
 const { data, defaultTab } = props;
 const cardDataList = ref([])
 const disableButton = ref(false)
-const LOADMORE_MAGIC_NUMBER = 4
+const LOADMORE_MAGIC_NUMBER = 12
 
 const createCardsContext = (array) => {
   cardDataList.value = []
