d56e4a36e50f9faa4c4b7d962f3c4fb532c3b863
Luis Igreja <lfsigreja@gmail.com>
2024-01-30 18:59:55 -0300

    fix(pencil): post hero reorder

--- src/templates/post/hero/index.vue
+++ src/templates/post/hero/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-10 ">
+    <div class="flex flex-col gap-10">
       <div class="flex gap-8 flex-col w-full">
         <Breadcrumb :model="breadcrumbItems" class="-ml-1" />
         <div class="flex flex-col gap-6">
