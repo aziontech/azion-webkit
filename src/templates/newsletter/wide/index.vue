ff5b96c47f5cb09d37d1ff4478f0a20b91a6915b
Luis Igreja <lfsigreja@gmail.com>
2024-02-07 18:50:53 -0300

    fix(newslleter): border top

--- src/templates/newsletter/wide/index.vue
+++ src/templates/newsletter/wide/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="border border-top surface-border surface-section flex justify-center py-12 px-3 md:px-8">
+  <section class="border border-t surface-border surface-section flex justify-center py-12 px-3 md:px-8">
     <div>
       <div class="flex flex-col gap-8 items-center">
         <div class="max-w-md w-full text-center">
