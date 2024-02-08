fdf03df93146abc205649af081056726c766f1b5
Robson Junior <robson.ga.junior@gmail.com>
2024-02-08 11:06:06 -0300

    fix: right block with props

--- src/templates/header/navigation-tabpanel/index.vue
+++ src/templates/header/navigation-tabpanel/index.vue
@@ -94,33 +94,18 @@
 
                       <div class="border-l surface-border p-3 surface-50 min-h-52">
                         <div v-if="index === 0">
-                          <strong class="text-xs text-color-secondary  mb-6 block">Success Cases</strong>
+                          <strong class="text-xs text-color-secondary  mb-6 block">{{ menuitem.rightBlock.label }}</strong>
                           <div class="grid gap-4 p-3">
-                            <article class="flex gap-4 w-96">
-                              <figure class="w-[160px] h-[90]px">
-                                <img src="?ims=160x90" alt="" class="rounded border surface-border" width="160" height="90" lazy />
+                            <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="flex gap-4 w-96">
+                              <figure class="w-[160px] h-[90px] overflow-hidden rounded border surface-border">
+                                <img :src="`${block.img.src}`" :alt="block.img.alt" class="w-full" width="160" height="90" lazy />
                               </figure>
-                              <div class="w-full">
+                              <div class="max-w-52">
                                 <p class="text-sm">
-                                  VTEX builds ultra low latency serveless applications with
-                                  Azion and allow thousands...
+                                 {{ block.description }}
                                 </p>
-                                <a href="" title="" class="p-button p-button-link p-button-sm p-0 mt-2">
-                                  Read more
-                                  <i class="pi pi-angle-right"></i>
-                                </a>
-                              </div>
-                            </article>
-                            <article class="flex gap-4 w-96">
-                              <figure class="w-[160px] h-[90]px">
-                                <img src="?ims=160x90" alt="" class="rounded border surface-border" width="160" height="90" lazy />
-                              </figure>
-                              <div class="w-full">
-                                <p class="text-sm">
-                                  Magalu guarantees high availability for hundreds of global-scale applications...
-                                </p>
-                                <a href="" title="" class="p-button p-button-link p-button-sm p-0 mt-2">
-                                  Read more
+                                <a :href="block.link.href" :title="block.link.label" class="p-button p-button-link p-button-sm p-0 mt-2">
+                                  {{ block.link.label }}
                                   <i class="pi pi-angle-right"></i>
                                 </a>
                               </div>
@@ -131,21 +116,21 @@
                         <div v-if="index === 3">
                           <strong class="text-xs text-color-secondary mb-6 block">New on blog</strong>
                           <div class="grid gap-4 p-3">
-                            <article class="w-96">
-                              <figure class="w-[160px] h-[90px] mb-4">
-                                <img src="?ims=160x90" alt="" class="rounded border surface-border" width="160" height="90" lazy />
+                            <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="w-96">
+                              <figure class="w-[160px] h-[90px] mb-4 overflow-hidden rounded border surface-border">
+                                <img :src="`${block.img.src}`" :alt="block.img.alt" class="w-full" width="160" height="90" lazy />
                               </figure>
                               <div class="w-full">
                                 <p class="text-base mb-2">
                                   <strong>
-                                    Women in Tech: Celebrating the Women's Month Wit
+                                    {{ block.title }}
                                   </strong>
                                 </p>
                                 <p class="text-sm">
-                                  Women in Tech: Celebrating the Women's Month With a conversation on Equity Diversi...
+                                  {{ block.description }}
                                 </p>
-                                <a href="" title="" class="p-button p-button-link p-button-sm p-0 mt-2">
-                                  View all articles
+                                <a :href="block.link.href" :title="block.link.label" class="p-button p-button-link p-button-sm p-0 mt-2">
+                                  {{ block.link.label }}
                                   <i class="pi pi-angle-right"></i>
                                 </a>
                               </div>
