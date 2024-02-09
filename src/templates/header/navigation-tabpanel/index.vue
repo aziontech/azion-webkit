e5f42f5f528b6d995b96e212ada585d63c4bde7d
Robson Junior <robson.ga.junior@gmail.com>
2024-02-08 23:30:05 -0300

    fix: new on blog should be param

--- src/templates/header/navigation-tabpanel/index.vue
+++ src/templates/header/navigation-tabpanel/index.vue
@@ -94,7 +94,9 @@
 
                       <div class="border-l surface-border p-3 surface-50 min-h-52">
                         <div v-if="index === 0">
-                          <strong class="text-xs text-color-secondary mb-4 block">{{ menuitem.rightBlock.label }}</strong>
+                          <strong class="text-xs text-color-secondary mb-4 block">
+                            {{ menuitem.rightBlock.label }}
+                          </strong>
                           <div class="grid gap-4 p-3">
                             <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="flex gap-4 w-96">
                               <figure class="w-[160px] h-[90px] overflow-hidden rounded border surface-border">
@@ -114,7 +116,9 @@
                         </div>
 
                         <div v-if="index === 3">
-                          <strong class="text-xs text-color-secondary mb-6 block">New on blog</strong>
+                          <strong class="text-xs text-color-secondary mb-6 block">
+                            {{ menuitem.rightBlock.label }}
+                          </strong>
                           <div class="grid gap-4 p-3">
                             <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="w-96">
                               <figure class="w-[160px] h-[90px] mb-4 overflow-hidden rounded border surface-border">
