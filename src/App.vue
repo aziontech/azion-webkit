07030df8bc05b4dde1f0e544fa0faaaa7ce775fa
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-02-28 10:33:14 -0300

    chore: update app for tests

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <CardCustom title="This is the Heading of a Card"></CardCustom>
-  <Overline label="Overline"></Overline>
+  <div class="surface-section py-20">
+    <div class="px-container w-full flex flex-col gap-40">
+      <HeroBlockBase
+        v-bind="{title:'Edge platform to build, secure, and scale applications anywhere', description:'Over 100+ Azion edge locations around the globe, plus your remote devices, on-premises, and cloud infrastructure.',isCentralized:true}"
+      >
+            <template #main>
+                <div class="flex justify-center items-center">
+                    <div class="w-[1440px] -mt-[400px] overflow-hidden absolute"
+                        ref="movableDiv"
+                    >
+                        <Vue3Spline
+                            :scene="{
+                                url: 'https://prod.spline.design/ZSqulu6Q0K6sPCXc/scene.splinecode',
+                            }"
+                        />
+                    </div>
+                </div>
+            </template>
+            <template #actions>
+                <LinkButton label="Free Account" link="" />
+                <LinkButton label="Talk to an Expert" link="" outlined />
+            </template>
+    </HeroBlockBase>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  
-  import Overline from './templates/overline/index.vue';
-  import CardCustom from './templates/card/index.vue'; 
-</script>
+  // Imports Components
+  import Card from 'primevue/card'
 
-<style lang="scss">
-  
-</style>
+  // Imports Templates 
+  import HeroBlockBase from './templates/hero-block-base/index.vue'
+</script>
