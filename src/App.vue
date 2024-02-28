db74388434b3444501b462adb5944bf424803674
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-02-28 08:31:42 -0300

    fix: update app content for tests

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,80 @@
 <template>
-  <CardCustom title="This is the Heading of a Card"></CardCustom>
-  <Overline label="Overline"></Overline>
+  <div class="surface-section py-20">
+    <div class="px-container w-full gap-40">
+      <ContentSectionBlock
+          v-bind="{
+              overline:'Get Started',
+              title:'Sign-up and get U$300 to use for 12 months',
+              description:'All products and features, no upfront commitment required—cancel anytime.',
+              position:'center',
+              isContentCentralized:'true'}"
+      >
+        <template #actions>
+            <LinkButton label="Free Account" link="" />
+            <LinkButton label="Talk to an Expert" link="" outlined />
+        </template>
+      </ContentSectionBlock>
+      <ContentSectionBlock
+                v-bind="{
+                    overline:'Results with Azion',
+                    title:'Our customers succeed, we succeed',
+                    position:'center',
+                    isContentCentralized:'true'}"
+        >
+            <template #main>
+                <Card>
+                    <template #content>
+                        <div class="flex flex-wrap gap-8 justify-center md:flex-row lg:justify-between w-full py-10 md:px-10 px-4">
+                            <Stats
+                                v-bind="{
+                                    bigNumber:'80%',
+                                    description:'Better TCO',
+                                    isCentralized:true
+                                }"
+                            />
+                            <Stats
+                                v-bind="{
+                                    bigNumber:'20%',
+                                    description:'Faster Deployments',
+                                    isCentralized:true
+                                }"
+                            />
+                            <Stats
+                                v-bind="{
+                                    bigNumber:'100%',
+                                    description:'Availability',
+                                    isCentralized:true
+                                }"
+                            />
+                            <Stats
+                                v-bind="{
+                                    bigNumber:'10x',
+                                    description:'Faster Applications',
+                                    isCentralized:true
+                                }"
+                            />
+                            <Stats
+                                v-bind="{
+                                    bigNumber:'15m',
+                                    description:'Response Time',
+                                    isCentralized:true
+                                }"
+                            />
+                        </div>
+                    </template>
+                </Card>
+            </template>
+        </ContentSectionBlock>
+  </div>
+  </div>
 </template>
 
 <script setup>
-  
-  import Overline from './templates/overline/index.vue';
-  import CardCustom from './templates/card/index.vue'; 
-</script>
+  // Imports Components
+  import Card from 'primevue/card'
 
-<style lang="scss">
-  
-</style>
+  // Imports Templates 
+  import ContentSectionBlock from './templates/content-section-block/index.vue'
+  import LinkButton from './templates/button/link.vue';
+  import Stats from './templates/stats/index.vue';
+</script>
