9e7d506ac847769a4a27b0e74b7e7d50a9c4a1b7
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2024-02-28 10:57:44 -0300

    fix: adding iconPos and icon prop

--- src/templates/button/link.vue
+++ src/templates/button/link.vue
@@ -1,28 +1,49 @@
 <template>
-	<a :href="link" :title="label" target="_blank" rel="noopener noreferrer"
-      class="p-button p-component p-button-sm min-w-fit no-underline"
-      :class="{'p-button-outlined' : outlined }"
-  >
-		{{ label }}
+	<a
+    :href="link"
+    :title="label"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="p-button p-component p-button-sm min-w-fit no-underline flex gap-2"
+    :class="{'p-button-outlined' : outlined }">
+
+    <span
+      v-if="iconPos === 'left'"
+      :class="`pi p-button-icon ${icon}`"
+      data-pc-section="icon"></span>
+
+    {{ label }}
+
+    <span
+      v-if="iconPos === 'right'"
+      :class="`pi p-button-icon ${icon}`"
+      data-pc-section="icon"></span>
 	</a>
 </template>
 
 <script setup>
-
-defineProps({
-	label: {
-		type: String,
-		required: true
-	},
-	link: {
-		type: String,
-		required: true
-	},
-  outlined: {
-    type: Boolean,
-    required: false,
-    default: false
-  }
-});
+  defineProps({
+    icon: {
+      type: String,
+      required: false
+    },
+    iconPos: {
+      type: String,
+      required: false
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    outlined: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  });
 </script>
 
