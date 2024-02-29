c5b815d4dd2a3b43efc9c182e8e27b3eeedc34d5
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2024-02-29 12:28:15 -0300

    fix: export GridBentoBlock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import AlgoliaDialog from './templates/algolia-dialog/index.vue';
 import AlgoliaSearch from './templates/algolia/index.vue';
-import ArticleHero from "./templates/post/hero/index.vue";
+import ArticleHero from './templates/post/hero/index.vue';
 import Button from './templates/button/index.vue';
 import ButtonBlockList from './templates/button-block-list/index.vue';
 import CardGridBlock from './templates/card-grid-block/index.vue';
@@ -11,11 +11,12 @@ import ContentQuoteBlock from './templates/content-quote-block/index.vue';
 import ContentSection from "./templates/content-section-block/index.vue";
 import DropdownLangSelector from './templates/dropdown-lang-selector/index.vue';
 import DropdownThemeSwitch from './templates/dropdown-theme-switch/index.vue';
-import EmptyState from "./templates/empty-state-block/index.vue";
-import ErrorStateBlock from "./templates/error-state-block/index.vue";
+import EmptyState from './templates/empty-state-block/index.vue';
+import ErrorStateBlock from './templates/error-state-block/index.vue';
 import FeaturedPost from './templates/featured-post/index.vue';
 import Footer from './templates/footer/index.vue';
-import FormContactUs from './templates/forms/contact-us.vue'
+import FormContactUs from './templates/forms/contact-us.vue';
+import GridBentoBlock from './templates/grid-bento-block/index.vue';
 import Header from './templates/header/index.vue';
 import HeaderDialogAlgoliaSearch from './templates/header/dialog-algolia-search/index.vue';
 import HeaderLogoAzion from './templates/header/visualid/azion/index.vue';
@@ -28,12 +29,12 @@ import MobileRightSidebar from './templates/header/mobile.vue';
 import NewsletterCard from './templates/newsletter/card/index.vue';
 import NewsletterSingleLine from './templates/newsletter/single-line/index.vue';
 import NewsletterWide from './templates/newsletter/wide/index.vue';
-import Overline from "./templates/overline/index.vue";
-import ReadableContent from "./templates/post/readable-content/index.vue";
+import Overline from './templates/overline/index.vue';
+import ReadableContent from './templates/post/readable-content/index.vue';
 import Sharer from './templates/sharer-block/index.vue';
-import TabMenuSearch from "./templates/tab-menu/with-search.vue";
-import TabMenuSearchLink from "./templates/tab-menu/with-search-link.vue";
-import TagListBlock from "./templates/tag-block-list/index.vue";
+import TabMenuSearch from './templates/tab-menu/with-search.vue';
+import TabMenuSearchLink from './templates/tab-menu/with-search-link.vue'
+import TagListBlock from './templates/tag-block-list/index.vue';
 import TitleFlexList from './templates/title-grid-list/index.vue';
 import TitleVerticalList from './templates/title-list/index.vue';
 
@@ -62,6 +63,8 @@ export {
   FeaturedPost,
   Footer,
   FormContactUs,
+
+  GridBentoBlock,
 
   Header,
   HeaderDialogAlgoliaSearch,
