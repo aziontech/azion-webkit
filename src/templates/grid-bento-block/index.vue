abd148e532dab9a92c153c44e24dbbe715eb41dc
Luis Igreja <lfsigreja@gmail.com>
2024-03-01 10:07:14 -0300

    fix: bento grid usability

--- src/templates/grid-bento-block/index.vue
+++ src/templates/grid-bento-block/index.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="grid gap-8 grid-cols-1" :class="grids[gridType].columns">
       <div v-for="(item, i) in grids[gridType].array" :key="i" class="rounded-md surface-ground row-span-1" :class="grids[gridType].rows(i)">
-        <slot :name="`item-${i}`" />
+        <slot :name="`item-${i + 1}`" />
       </div>
     </div>
   </section>
@@ -32,7 +32,7 @@ const grids = {
   "2-columns-5-items": {
     array: Array(5),
     columns: "md:grid-cols-6",
-    rows: (i) => i === 0 ? 'md:col-span-3 md:row-span-2' : i === 3 ? 'md:col-span-2' : i === 4 ? 'md:col-span-4' : 'md:col-span-3'
+    rows: (i) => i === 0 ? 'md:col-span-3 md:row-span-2' : 'md:col-span-3'
   },
   "2-columns-6-items": {
     array: Array(6),
