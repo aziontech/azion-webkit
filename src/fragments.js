714c6a9cc6e2022a723e4e1f253424b2bb5976e0
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2024-03-06 19:00:33 -0300

    feat: add icon-text-segment fragment

--- src/fragments.js
+++ src/fragments.js
@@ -1,19 +1,23 @@
 import UnorderedList from './fragments/list/unordered-list.vue';
 import CardTitle from './fragments/text/card-title.vue';
 import CardDescription from './fragments/text/card-description.vue';
+import IconTextSegment from './fragments/icon-text-segment.vue'
 import Overline from './fragments/text/overline.vue';
 import Quote from './fragments/quote/index.vue';
 import QuoteCarousel from './fragments/quote/carousel.vue';
 import StatHighlightBanner from './fragments/stat-highlight-banner.vue';
 import Tile from './fragments/tile/index.vue';
 
+export default {}
+
 export {
   UnorderedList,
   CardTitle,
   CardDescription,
+  IconTextSegment,
   Overline,
   Quote,
   QuoteCarousel,
   StatHighlightBanner,
-  Tile,
+  Tile
 }
---
+++ src/fragments/icon-text-segment.vue
@@ -0,0 +1,74 @@
+<template>
+  <a
+    :href="href"
+    :title="title"
+    class="flex gap-3 p-3 group hover:surface-hover rounded-md transition-all">
+
+    <div>
+      <span class="w-[26px] h-[26px] flex items-center justify-center rounded surface-200 group-hover:bg-[var(--surface-100)]">
+        <i
+          :class="icon"
+          class="text-sm text-color-secondary group-hover:text-[var(--text-color)]">
+        </i>
+      </span>
+    </div>
+    <div>
+      <div class="flex gap-3 items-center">
+        <p class="text-sm font-medium leading-tight">
+          {{ title }}
+        </p>
+        <Tag
+          v-if="tag"
+          :value="tag"
+          :severity="tagSeverity"
+          :pt="{
+            root: {
+              class: 'h-6'
+            }
+          }"
+        />
+      </div>
+      <p
+        v-if="description"
+        class="text-xs font-normal text-color-secondary leading-none mt-2">
+
+        {{ description }}
+      </p>
+    </div>
+  </a>
+</template>
+
+<script setup>
+  import Tag from 'primevue/tag';
+
+  const props = defineProps({
+    href: {
+      type: String,
+      required: true,
+      default: '#'
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    tag: {
+      type: String,
+      required: false,
+    },
+    tagSeverity: {
+      type: String,
+      required: false,
+      default: 'info'
+    }
+  });
+
+  const { href, title, description, icon, tag, tagSeverity} = props;
+</script>
