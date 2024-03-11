60927b65658d0670c9fd5e398a534ffd5d960714
Luis Igreja <lfsigreja@gmail.com>
2024-03-11 14:07:26 -0300

    fix: card-title font size

--- src/fragments/text/card-title.vue
+++ src/fragments/text/card-title.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1 v-if="titleTag === 'h1'" class="text-3xl font-medium"><slot /></h1>
-  <h2 v-if="titleTag === 'h2'" class="text-3xl font-medium"><slot /></h2>
-  <h3 v-if="titleTag === 'h3'" class="text-3xl font-medium"><slot /></h3>
-  <h4 v-if="titleTag === 'h4'" class="text-3xl font-medium"><slot /></h4>
-  <h5 v-if="titleTag === 'h5'" class="text-3xl font-medium"><slot /></h5>
-  <h6 v-if="titleTag === 'h6'" class="text-3xl font-medium"><slot /></h6>
-  <p v-if="titleTag === ''" class="text-xl font-medium max-w-lg"> <slot /> </p>
+  <h1 v-if="titleTag === 'h1'" class="text-xl font-medium max-w-lg"><slot /></h1>
+  <h2 v-else-if="titleTag === 'h2'" class="text-xl font-medium max-w-lg"><slot /></h2>
+  <h3 v-else-if="titleTag === 'h3'" class="text-xl font-medium max-w-lg"><slot /></h3>
+  <h4 v-else-if="titleTag === 'h4'" class="text-xl font-medium max-w-lg"><slot /></h4>
+  <h5 v-else-if="titleTag === 'h5'" class="text-xl font-medium max-w-lg"><slot /></h5>
+  <h6 v-else-if="titleTag === 'h6'" class="text-xl font-medium max-w-lg"><slot /></h6>
+  <p v-else class="text-xl font-medium max-w-lg"> <slot /> </p>
 </template>
 
 <script setup>
