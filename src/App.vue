49ab0d21f9c42cdd63efcd7d251242a79d42ff22
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-03-11 11:52:49 -0300

    feat: update icons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="surface-section py-20">
     <div class="px-container w-full flex flex-col gap-20">
+      <div class="flex flex-wrap w-full gap-4 items-center justify-center text-5xl">
+        <i class="ai ai-twitter"></i>
+        <i class="ai ai-column"></i>
+        <i class="ai ai-home"></i>
+        <i class="ai ai-marketplace"></i>
+        <i class="ai ai-domains"></i>
+        <i class="ai ai-edge-application"></i>
+        <i class="ai ai-build-pillar"></i>
+        <i class="ai ai-variables"></i>
+        <i class="ai ai-secure-pillar"></i>
+        <i class="ai ai-edge-firewall"></i>
+        <i class="ai ai-edge-dns"></i>
+        <i class="ai ai-deploy-pillar"></i>
+        <i class="ai ai-edge-nodes"></i>
+        <i class="ai ai-observe-pillar"></i>
+        <i class="ai ai-data-stream"></i>
+        <i class="ai ai-edge-pulse"></i>
+        <i class="ai ai-real-time-metrics"></i>
+        <i class="ai ai-real-time-events"></i>
+        <i class="ai ai-real-time-purge"></i>
+        <i class="ai ai-edge-libraries"></i>
+        <i class="ai ai-edge-functions"></i>
+        <i class="ai ai-edge-services"></i>
+        <i class="ai ai-digital-certificates"></i>
+        <i class="ai ai-network-lists"></i>
+        <i class="ai ai-waf-rules"></i>
+        <i class="ai ai-store"></i>
+        <i class="ai ai-edge-storage"></i>
+        <i class="ai ai-edge-sql"></i>
+        <i class="ai ai-edge-kv"></i>
+        <i class="ai ai-edge-orchestration"></i>
+
+      </div>
       <ContentSectionBlock
           v-bind="{
               overline:'Get Started',
