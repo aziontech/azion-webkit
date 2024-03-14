7b0c159e6909c226c3b71660c275aa1005e87cf4
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-03-14 09:31:00 -0300

    chore: update example

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="surface-section py-20">
     <div class="px-container w-full flex flex-col gap-20">
-      <div class="flex flex-wrap w-full gap-4 items-center justify-center">
-        <!-- <Quote
-          :data="{0:{text:'”A Azion acelera a entrega de conteúdo para a maior empresa de entretenimento do Brasil.”',label:'Largest entertainment company in Brazil',imageLight:'https://www.azion.com/assets/pages/products/images/edge-application/omelete-co-logo-white.png',imageDark:'https://www.azion.com/assets/pages/products/images/edge-application/omelete-co-logo-white.png'}}"
-          isCarousel
-        /> -->
+      <div class="flex flex-col w-full gap-20 items-center justify-center">
+        <QuoteCarousel
+          text='“A Azion acelera a entrega de conteúdo para a maior empresa de entretenimento do Brasil. A Azion acelera a entrega de conteúdo para a maior empresa de entretenimento do Brasil.  A Azion acelera a entrega de conteúdo para a maior empresa de entretenimento do Brasil.  ”'
+          label="Largest entertainment company in Brazil"
+          imageLight="https://www.azion.com/assets/pages/products/images/edge-application/omelete-co-logo-white.png"
+          imageDark="https://www.azion.com/assets/pages/products/images/edge-application/omelete-co-logo-white.png"
+          alt="Alternative text for image"
+          direction="row"
+        />
         <QuoteCarousel
           text='“A Azion acelera a entrega de conteúdo para a maior empresa de entretenimento do Brasil.”'
           label="Largest entertainment company in Brazil"
