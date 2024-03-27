5a61678e041c74fb5d3eefe8cc3baf9f518fee80
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2024-03-27 15:04:58 -0300

    fix: ajustes de header (#71)
    * fix: ajustes gerais
    
    * fix: removido styles
    
    * fix: ajuste responsivo
    
    * fix: sumir mobile
    
    * FIX: blog card + link button target
    
    * chore(release): 1.14.3
    
    [skip ci]
    ## [1.14.3](https://github.com/aziontech/azion-web-kit/compare/v1.14.2...v1.14.3) (2024-03-18)
    
    ### Bug Fixes
    
    * blog card + link button target ([2735182](https://github.com/aziontech/azion-web-kit/commit/2735182f0c328f9a4ce3bb8a6d17f9ac8ebd3b31))
    
    * feat: card-description, tile and grid-bento variations
    
    * chore(release): 1.15.0
    
    [skip ci]
    ## [1.15.0](https://github.com/aziontech/azion-web-kit/compare/v1.14.3...v1.15.0) (2024-03-19)
    
    ### Features
    
    * card-description, tile and grid-bento variations ([6dacc57](https://github.com/aziontech/azion-web-kit/commit/6dacc57e09c3e80b436aa7fcbede5ab51b9f06f0))
    
    * fix: remove empty state border + added max-w
    
    * chore(release): 1.15.1
    
    [skip ci]
    ## [1.15.1](https://github.com/aziontech/azion-web-kit/compare/v1.15.0...v1.15.1) (2024-03-20)
    
    ### Bug Fixes
    
    * remove empty state border + added max-w ([9ce7865](https://github.com/aziontech/azion-web-kit/commit/9ce78652264821ba6e2fb78e6ee2f9d8d1df389c))
    
    * fix: add hs-forms to forms to trigger events
    
    * chore(release): 1.15.2
    
    [skip ci]
    ## [1.15.2](https://github.com/aziontech/azion-web-kit/compare/v1.15.1...v1.15.2) (2024-03-22)
    
    ### Bug Fixes
    
    * add hs-forms to forms to trigger events ([8954259](https://github.com/aziontech/azion-web-kit/commit/89542591e33ce84d7425648eaaf1a02345d252e7))
    
    * fix: add card props to card clickable
    
    * chore(release): 1.15.3
    
    [skip ci]
    ## [1.15.3](https://github.com/aziontech/azion-web-kit/compare/v1.15.2...v1.15.3) (2024-03-22)
    
    ### Bug Fixes
    
    * add card props to card clickable ([f116f56](https://github.com/aziontech/azion-web-kit/commit/f116f568a2558d7c06a53c41dcc3a57ff77f6a27))
    
    * feat: job application form
    
    * fix: form description
    
    * chore(release): 1.16.0
    
    [skip ci]
    ## [1.16.0](https://github.com/aziontech/azion-web-kit/compare/v1.15.3...v1.16.0) (2024-03-26)
    
    ### Features
    
    * job application form ([8501e22](https://github.com/aziontech/azion-web-kit/commit/8501e22fc5b9269ce1281b12de25e9c114d7d214))
    
    ### Bug Fixes
    
    * form description ([91447fb](https://github.com/aziontech/azion-web-kit/commit/91447fbc50044d6559829ed5ffb4e710f18088f9))
    
    * fix: ajustes gerais
    
    * fix: removido styles
    
    * fix: ajuste responsivo
    
    * fix: sumir mobile
    
    * feat: header overline + header tag + spacings
    
    ---------
    
    Co-authored-by: Luis Igreja <lfsigreja@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- src/templates/header/navigation-tabpanel/index.vue
+++ src/templates/header/navigation-tabpanel/index.vue
@@ -27,41 +27,49 @@
               icon: { class: 'text-white' }
             }" />
 
-          <OverlayPanel 
+          <OverlayPanel
             unstyled
-            class="left-8 lg:left-36"
             :ref="menuitem.ref"
-            :pt="{ 
-              content: { class: 'fixed p-0 flex flex-row border surface-border rounded-md surface-50' },
+            :pt="{
+              content: { class: 'fixed p-0 hidden lg:flex flex-row border surface-border rounded-md surface-100 w-full max-w-[calc(100%-4rem)] lg:max-w-[calc(100%-11rem)] xl:max-w-[1052px]' },
               root: { class: 'left-8 lg:left-36 top-12 z-50'}
             }"
           >
-            <div class="flex flex-col gap-1 border-r surface-border p-3">
-              <Button
-                text
-                size="small"
-                v-for="(subitem, index) in menuitem.items"
-                :key="index"
-                :class="{ 'surface-hover': active === index }"
-                class="flex gap-2 justify-between w-full items-centerm min-w-[175px]"
-                @click="active = index">
-
-                <span class="flex gap-2 items-center">
-                  <i :class="subitem.icon"></i>
-                  {{ subitem.label }}
-                </span>
-                <i class="pi pi-angle-right"></i>
-              </Button>
+            <div class="flex flex-col gap-1 border-r surface-border p-3 surface-50 rounded-l-md">
+              <template v-for="(subitem, index) in menuitem.items" v-bind:key="index">
+                <template v-if="subitem.items">
+                  <Button
+                    text
+                    size="small"
+                    :class="{ 'surface-hover': active === index }"
+                    class="flex gap-2 justify-between w-full items-centerm min-w-52"
+                    @click="active = index">
+                    <span class="flex gap-2 items-center">
+                      <i :class="subitem.icon"></i>
+                      {{ subitem.label }}
+                    </span>
+                    <i class="pi pi-angle-right"></i>
+                  </Button>
+                </template>
+                <template v-else>
+                  <a class="p-button p-component p-button-text p-button-sm flex gap-2 hover:surface-hover justify-between w-full items-centerm min-w-52"
+                     :href="subitem.href"
+                  >
+                    <span class="flex gap-2 items-center">
+                      <i :class="subitem.icon"></i>
+                        {{ subitem.label }}
+                    </span>
+                 </a>
+                </template>
+              </template>
             </div>
 
-            <div class="w-full max-w-[1090px]">
-              <TabView v-model:activeIndex="active" :pt="{ navContainer: { class: 'hidden' } }">
+              <TabView v-model:activeIndex="active" :pt="{ navContainer: { class: 'hidden' }, root: { class: 'w-full'} }">
                 <TabPanel v-for="(subitem, jIndex) in menuitem.items" :key="jIndex">
                   <div class="flex flex-row">
-                    <ul class="grid grid-cols-1 lg:grid-cols-2 m-0 p-3 h-fit min-h-16">
-                      <li v-for="(link, index) in subitem.items" :key="index" class="h-fit">
+                    <ul class="grid grid-cols-1 xl:grid-cols-2 m-0 p-3 h-fit min-h-20 max-w-[627px] w-full">
+                      <li v-for="(link, index) in subitem.items" :key="index" class="flex flex-col gap-2">
                         <a :href="link.href" :title="link.label" class="p-button p-button-text p-button-sm w-full hover:surface-hover">
-
                           <div class="flex gap-3">
                             <div v-if="link.icon">
                               <span class="py-1 px-1.5 flex rounded-md surface-200">
@@ -69,47 +77,56 @@
                               </span>
                             </div>
                             <div class="flex flex-col gap-1">
-                              <p class="text-left">
-                                {{ link.label }}
-                              </p>
+                              <div class="flex gap-2 items-center">
+                                <p class="text-left">
+                                  {{ link.label }}
+                                </p>
+                                <template v-if="link.tag">
+                                  <Tag :value="link.tag" severity="info" />
+                                </template>
+                              </div>
                               <p v-if="link.description" class="text-xs text-color-secondary text-left">
                                 {{ link.description }}
                               </p>
                             </div>
                           </div>
                         </a>
-
-                        <ul class="grid pl-[2.25rem] mt-2">
-                          <li v-for="(sublink, subIndex) in link.items" :key="subIndex">
-                            <a :href="sublink.href" :title="sublink.label" class="w-full p-button p-button-text p-button-sm text-xs mb-2">
-                              <div class="flex gap-3">
-                                <div v-if="sublink.icon">
-                                  <span class="py-1 px-1.5 flex rounded-md surface-200">
-                                    <i :class="sublink.icon" class="text-xs"></i>
-                                  </span>
+                        <div class="pl-9 flex flex-col gap-2" v-if="link.items">
+                          <template v-if="link.overline">
+                            <Overline :label="link.overline" class="px-[10.5px]" />
+                          </template>
+                          <ul>
+                            <li v-for="(sublink, subIndex) in link.items" :key="subIndex" class="flex- flex-col gap-2">
+                              <a :href="sublink.href" :title="sublink.label" class="w-full p-button p-button-text p-button-sm text-xs hover:surface-hover">
+                                <div class="flex gap-3">
+                                  <div v-if="sublink.icon">
+                                    <span class="py-1 px-1.5 flex rounded-md surface-200">
+                                      <i :class="sublink.icon" class="text-xs"></i>
+                                    </span>
+                                  </div>
+                                  <div>
+                                    <p class="text-left">
+                                      {{ sublink.label }}
+                                    </p>
+                                  </div>
                                 </div>
-                                <div>
-                                  <p class="text-left">
-                                    {{ sublink.label }}
-                                  </p>
-                                </div>
-                              </div>
-                            </a>
-                          </li>
-                        </ul>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
                       </li>
                     </ul>
 
-                    <div class="border-l surface-border p-3 min-h-52 hidden md:flex">
+                    <div class="border-l surface-border p-6 gap-3 flex-col min-h-52 hidden lg:flex w-full max-w-[340px] surface-50 rounded-r-md">
                       <div v-if="index === 0">
-                        <Overline :label="menuitem.rightBlock.label" />
-                        <div class="grid gap-4 p-3">
+                        <Overline :label="menuitem.rightBlock.label" class="mb-6 flex"/>
+                        <div class="flex flex-col gap-4 m-0 w-full">
                           <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="flex gap-4 w-full">
-                            <figure class="overflow-hidden rounded border surface-border w-full h-fit">
+                            <figure class="mb-4 overflow-hidden rounded border surface-border h-fit w-[280px]">
                               <img :src="`${block.img.src}`" :alt="block.img.alt" class="w-full" lazy />
                             </figure>
-                            <div class="max-w-52">
-                              <p class="text-sm">
+                            <div class="w-full flex flex-col">
+                              <p class="text-xs leading-relaxed text-color-secondary">
                                 {{ block.description }}
                               </p>
                               <a :href="block.link.href" target="_blank" :title="block.link.label" class="p-button p-button-link p-button-sm p-0 mt-2">
@@ -122,19 +139,17 @@
                       </div>
 
                       <div v-if="index === 3">
-                        <Overline :label="menuitem.rightBlock.label" />
-                        <div class="grid gap-4 p-3 w-full">
+                        <Overline :label="menuitem.rightBlock.label" class="mb-6 flex" />
+                        <div class="grid gap-4 m-0 w-full">
                           <article v-for="(block, idx) in menuitem.rightBlock.items" :key="idx" class="w-full">
                             <figure class="w-[160px] h-[90px] mb-4 overflow-hidden rounded border surface-border">
                               <img :src="`${block.img.src}`" :alt="block.img.alt" class="w-full" width="160" height="90" lazy />
                             </figure>
-                            <div class="w-full">
-                              <p class="text-base mb-2">
-                                <strong>
+                            <div class="w-full flex flex-col gap-1">
+                              <p class="text-base font-medium text-color mb-2 leading-normal">
                                   {{ block.title }}
-                                </strong>
                               </p>
-                              <p class="text-sm">
+                              <p class="text-xs text-color-secondary leading-relaxed">
                                 {{ block.description }}
                               </p>
                               <a :href="block.link.href" :title="block.link.label" class="p-button p-button-link p-button-sm p-0 mt-2">
@@ -149,7 +164,6 @@
                   </div>
                 </TabPanel>
               </TabView>
-            </div>
           </OverlayPanel>
         </div>
       </li>
@@ -163,7 +177,7 @@
   import OverlayPanel from 'primevue/overlaypanel';
   import TabView from 'primevue/tabview';
   import TabPanel from 'primevue/tabpanel';
-
+  import Tag from 'primevue/tag';
   import Overline from '../../../fragments/text/overline.vue';
 
   const props = defineProps({
