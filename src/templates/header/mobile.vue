320587df010b5e1aed6fe484f48a1388d1ddea13
Luis Igreja <lfsigreja@gmail.com>
2024-04-04 15:41:53 -0300

    fix: mobile button color variation

--- src/templates/header/mobile.vue
+++ src/templates/header/mobile.vue
@@ -104,48 +104,50 @@
       </template>
     </PanelMenu>
 
-    <Divider class="my-8" />
+    <template v-if="menuSecondary">
+      <Divider class="my-8" />
+      <Menu
+        class="w-full border-none bg-transparent"
+        :model="menuSecondary"
+        :pt="{
+          root: {
+            class: 'p-0 w-full border-none bg-transparent'
+          }
+        }">
 
-    <Menu
-      class="w-full border-none bg-transparent"
-      :model="menuSecondary"
-      :pt="{
-        root: {
-          class: 'p-0 w-full border-none bg-transparent'
-        }
-      }">
+        <template #item="{ item, props }">
+          <a
+            v-if="item.url"
+            v-bind="props.action"
+            :target="item.target"
+            :href="item.url"
+            class="p-2 flex gap-2">
 
-      <template #item="{ item, props }">
+              <span v-if="item.icon" :class="item.icon"></span>
+              <span class="ml-2 font-medium">
+                {{ item.label }}
+              </span>
+              <Tag v-for="tag in item.tags" :key='tag' :value="tag" severity="info"/>
+          </a>
+        </template>
+      </Menu>
+    </template>
+    <template v-if="bottomButtons">
+      <div class="fixed bottom-6 flex gap-2 items-center">
         <a
-          v-if="item.url"
-          v-bind="props.action"
-          :target="item.target"
-          :href="item.url"
-          class="p-2 flex gap-2">
-
-            <span v-if="item.icon" :class="item.icon"></span>
-            <span class="ml-2 font-medium">
-              {{ item.label }}
-            </span>
-            <Tag v-for="tag in item.tags" :key='tag' :value="tag" severity="info"/>
+          v-for="(button, index) in bottomButtons"
+          :key="index"
+          :href="button.url"
+          :title="button.urlTitle"
+          :class="[button.destak ?
+            'flex gap-2 justify-between p-button p-button-primary p-button-sm whitespace-nowrap' :
+            'flex gap-2 p-button p-button-primary p-button-outlined p-button-sm  text-white hover:bg-header-button-hover whitespace-nowrap',
+            { 'p-button-info' : button.severity === 'info'}]">
+          {{ button.label }}
+          <i v-if="button.icon" :class="button.icon"></i>
         </a>
-      </template>
-    </Menu>
-
-    <div class="fixed bottom-6 flex gap-2 items-center">
-      <a
-        v-for="(button, index) in bottomButtons"
-        :key="index"
-        :href="button.url"
-        :title="button.urlTitle"
-        :class="button.destak ?
-          'flex gap-2 justify-between p-button p-button-primary p-button-sm whitespace-nowrap' :
-          'flex gap-2 p-button p-button-primary p-button-outlined p-button-sm  text-white hover:bg-header-button-hover whitespace-nowrap'">
-
-        {{ button.label }}
-        <i v-if="button.icon" :class="button.icon"></i>
-      </a>
-    </div>
+      </div>
+    </template>
   </Sidebar>
 </template>
 
