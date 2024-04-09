7b2bfdef56d48d1124d4cd543d1cf772868b1416
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-09 16:22:03 -0300

    fix: paddings responsivos no mobile

--- src/templates/big-numbers/index.vue
+++ src/templates/big-numbers/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8" :class="[{ 'border surface-border' : border}]">
-    <li class="flex flex-col gap-5 py-10 px-6 justify-evenly max-w-64" v-for="item in items" :key="item.title">
+  <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8 md:py-0 py-5" :class="[{ 'border surface-border' : border}]">
+    <li class="flex flex-col gap-5 py-5 md:py-10 px-6 justify-evenly max-w-64" v-for="item in items" :key="item.title">
       <strong class="text-6xl text-center">
         {{ item.title }}
       </strong>
