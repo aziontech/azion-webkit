46c30020431860ae7586ac3af162f3eee632d81b
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-09 12:41:57 -0300

    fix: responsividade melhorada

--- src/fragments/banner/index.vue
+++ src/fragments/banner/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="items-center px-3 p-2 rounded-md border surface-border max-w-3xl">
-    <div class="flex items-start justify-center flex-row gap-2">
-      <Tag value="News" />
-      <p class="text-sm text-color-secondary mt-1" style="display: inline-block;">
-        {{ description }}
-        <a :href="link" class="text-sm font-medium" style="color: var(--text-color-link); display: inline;">{{ cta }}</a>
-      </p>
-    </div>
+  <div class="items-center px-3 p-2 rounded-md border surface-border max-w-3xl w-fit">
+      <div class="w-fit text-center" style="display: inline-block;">
+        <Tag value="News" class="mr-1.5" />
+        <span class="text-sm text-color-secondary text-center *:">
+          {{ description }}
+        </span>
+        <a :href="link" class="text-sm font-medium ml-1" style="color: var(--text-color-link); display: inline;">{{ cta }}</a>
+      </div>
   </div>
 </template>
 
