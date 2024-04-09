89844cdfcc46465d2a6808934c3d5eef134226a3
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-09 12:54:49 -0300

    fix: ajuste tag p

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="surface-section py-20">
     <div class="px-container w-full flex flex-col gap-20">
       <div class="flex flex-col w-full gap-20 items-center justify-center">
-        <HeroBlock :banner-news="{ description: 'Azion ganha o Prêmio de Inovação 2023 da Frost & Sullivan na categoria edge serveless.', cta: { label: 'Confira!', link: 'https://www.azion.com/pt-br/blog/azion-premio-frost-sullivan/' } }"
+        <HeroBlock :banner-news="{ description: 'Azion ganha o Prêmio de Inovação 2023 da Frost & Sullivan na categoria edge serveless dasuhdh uashdu ashu duhau hdhuashu asuhadsuhas uhsda.', cta: { label: 'Confira!', link: 'https://www.azion.com/pt-br/blog/azion-premio-frost-sullivan/' } }"
                :title="'Edge Computing platform to build, secure, and scale applications anywhere'"
                :title-tag="'h1'"
                :description="'Over 100+ Azion edge locations around the globe, plus your remote devices, on-premises, and cloud infrastructure.'"
--- src/fragments/banner/index.vue
+++ src/fragments/banner/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="px-3 p-2 rounded-md border surface-border max-w-3xl">
-      <div class="text-center">
+      <p class="text-center">
         <Tag value="News" class="mr-1.5" />
         <span class="text-sm text-color-secondary text-center *:">
           {{ description }}
         </span>
         <a :href="link" class="text-sm font-medium ml-1" style="color: var(--text-color-link); display: inline;">{{ cta }}</a>
-      </div>
+      </p>
   </div>
 </template>
 
