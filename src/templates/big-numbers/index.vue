ce77ccfbd3cd7bf0f33a80d62b0459368d7016a7
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-09 16:12:33 -0300

    fix: ajuste no padding

--- src/templates/big-numbers/index.vue
+++ src/templates/big-numbers/index.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8" :class="[{ 'border surface-border' : border}]">
-    <li class="flex flex-col gap-5 py-10 px-5 justify-evenly max-w-64" v-for="item in items" :key="item.title">
+    <li class="flex flex-col gap-5 py-10 px-6 justify-evenly max-w-64" v-for="item in items" :key="item.title">
       <strong class="text-6xl text-center">
         {{ item.title }}
       </strong>
