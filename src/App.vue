6203a3f45a85697e724ca6629f07962ac0d7f707
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-16 12:57:12 -0300

    feat: new block poc

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,14 @@
 <template>
   <div class="surface-section py-20">
+    <MenuNavigation/>
     <div class="px-container w-full flex flex-col gap-20">
       <div class="flex flex-col w-full gap-20 items-center justify-center">
-        <SectionBlock :banner-news="{ description: 'Azion ganha o Prêmio de Inovação 2023 da Frost & Sullivan na categoria edge serveless dasuhdh uashdu ashu duhau hdhuashu asuhadsuhas uhsda.', cta: { label: 'Confira!', link: 'https://www.azion.com/pt-br/blog/azion-premio-frost-sullivan/' } }"
-               :title="'Quando nossos clientes tem sucesso, nós temos sucesso'"
-               :overline="'Resultados com a Azion'"
-               :title-tag="'h1'"
-               isContentCentralized="true"
-               position="center">
-               <template v-slot:content>
-                 <BigNumbers :items="itens" :border="true" />
-              </template>
-        </SectionBlock>
+        Conteúdo
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import SectionBlock from './templates/content-section-block/index.vue';
-  import BigNumbers from './templates/big-numbers/index.vue';
-
-  const itens = [
-    { title: '80%', description: 'Melhor TCO' },
-    { title: '20%', description: 'Implantações mais rápidas' },
-    { title: '100%', description: 'De disponibilidade, garantida por SLA' },
-    { title: '10x', description: 'Aplicações mais rápidas' },
-    { title: '15ms', description: 'Tempo de resposta' },
-    // Adicione mais itens conforme necessário
-  ];
-
+  import MenuNavigation from './templates/menu-navigation-block/index.vue';
 </script>
---
+++ src/templates/menu-navigation-block/index.vue
@@ -0,0 +1,110 @@
+<template>
+    <Menubar
+        :model="items"
+        class="px-container w-full border-none hidden lg:flex gap-2 customBreakpointHide"
+        :pt="{
+            root: {
+                class: 'surface-200',
+            },
+            button: {
+                class: 'hidden',
+            }
+        }"
+    >
+        <template #start>
+            <Button text tabindex="1">
+                <div>
+                    <div class="text-lg font-medium">Learning Center</div>
+                </div>
+            </Button>
+        </template>
+        <template #item="{ item, props, hasSubmenu, root }">
+            <a class="flex align-items-center px-3.5" v-bind="props.action">
+                <span>{{ item.label }}</span>
+                <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{ item.shortcut }}</span>
+                <i v-if="hasSubmenu" :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
+            </a>
+        </template>
+    </Menubar>
+    <PanelMenu
+        :model="teste"
+        class="block lg:hidden"
+        :pt="{
+            root: {
+                class: 'w-full surface-c',
+            },
+            headerContent: {
+                class: 'w-full border-none px-2 flex items-center min-h-11 bg-[var(--surface-200)]',
+            },
+            menuContent: {
+                class: 'border-none',
+            }
+        }"
+    >
+        <template #item="{ item }">
+            <a class="flex align-items-center cursor-pointer text-color px-3 py-2 w-full" :href="item.url" :target="item.target">
+                <span class="ml-2">{{ item.label }}</span>
+                <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
+            </a>
+        </template>
+    </PanelMenu>
+</template>
+
+<script setup>
+import Menubar from 'primevue/menubar';
+import PanelMenu from 'primevue/panelmenu';
+
+const customBreakpoint = "1024";
+
+const items = [
+    {
+        label: 'Home',
+    },
+    {
+        label: 'Features',
+    },
+    {
+        label: 'Projects',
+        items: [
+            {
+                label: 'Components',
+            },
+            {
+                label: 'Blocks',
+            },
+            {
+                label: 'UI Kit',
+            },
+            {
+                label: 'Templates',
+                items: [
+                    {
+                        label: 'Apollo',
+                    },
+                    {
+                        label: 'Ultima',
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Contact',
+    }
+];
+const teste = [
+    {
+        label: 'Highlighted',
+        items: [
+            {
+                label: 'Styled',
+                route: '/theming'
+            },
+            {
+                label: 'Unstyled',
+                route: '/unstyled'
+            }
+        ]
+    }
+];
+</script>
