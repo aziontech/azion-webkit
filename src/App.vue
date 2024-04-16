2be841a46867d1fb98eaaab03533e81b78463442
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-04-16 13:14:41 -0300

    fix: ajustes de layout

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="surface-section py-20">
     <MenuNavigation/>
     <div class="px-container w-full flex flex-col gap-20">
-      <div class="flex flex-col w-full gap-20 items-center justify-center">
+      <div class="flex flex-col w-full gap-20 items-center justify-center py-16 text-color-secondary">
         Conteúdo
       </div>
     </div>
--- src/templates/menu-navigation-block/index.vue
+++ src/templates/menu-navigation-block/index.vue
@@ -1,110 +1,154 @@
 <template>
     <Menubar
-        :model="items"
-        class="px-container w-full border-none hidden lg:flex gap-2 customBreakpointHide"
-        :pt="{
-            root: {
-                class: 'surface-200',
-            },
-            button: {
-                class: 'hidden',
-            }
-        }"
+      :model="mergedItems"
+      class="px-container w-full border-none hidden lg:flex gap-5"
+      :pt="{
+        root: {
+          class: 'surface-200',
+        },
+        button: {
+          class: 'hidden',
+        }
+      }"
     >
-        <template #start>
-            <Button text tabindex="1">
-                <div>
-                    <div class="text-lg font-medium">Learning Center</div>
-                </div>
-            </Button>
-        </template>
-        <template #item="{ item, props, hasSubmenu, root }">
-            <a class="flex align-items-center px-3.5" v-bind="props.action">
-                <span>{{ item.label }}</span>
-                <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{ item.shortcut }}</span>
-                <i v-if="hasSubmenu" :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
-            </a>
-        </template>
+      <template #start>
+        <Button text tabindex="1">
+          <div>
+            <div class="text-lg font-medium leading-relaxed">Learning Center</div>
+          </div>
+        </Button>
+      </template>
+      <template #item="{ item, props, hasSubmenu, root }">
+        <a class="flex align-items-center p-3 leading-relaxed" v-bind="props.action">
+          <span class="text-sm">{{ item.label }}</span>
+          <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{ item.shortcut }}</span>
+          <i v-if="hasSubmenu" :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
+        </a>
+      </template>
     </Menubar>
     <PanelMenu
-        :model="teste"
-        class="block lg:hidden"
-        :pt="{
-            root: {
-                class: 'w-full surface-c',
-            },
-            headerContent: {
-                class: 'w-full border-none px-2 flex items-center min-h-11 bg-[var(--surface-200)]',
-            },
-            menuContent: {
-                class: 'border-none',
-            }
-        }"
+      :model="mobile"
+      class="block lg:hidden"
+      :pt="{
+        root: {
+          class: 'w-full surface-c',
+        },
+        headerContent: {
+          class: 'w-full border-none px-2 py-3 flex items-center min-h-11 bg-[var(--surface-200)] text-sm',
+        },
+        menuContent: {
+          class: 'border-none text-sm',
+        }
+      }"
     >
-        <template #item="{ item }">
-            <a class="flex align-items-center cursor-pointer text-color px-3 py-2 w-full" :href="item.url" :target="item.target">
-                <span class="ml-2">{{ item.label }}</span>
-                <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
-            </a>
-        </template>
+      <template #item="{ item }">
+        <a class="flex align-items-center cursor-pointer text-color px-3 py-2 w-full" :href="item.url" :target="item.target">
+          <span class="ml-2">{{ item.label }}</span>
+          <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
+        </a>
+      </template>
     </PanelMenu>
-</template>
-
-<script setup>
-import Menubar from 'primevue/menubar';
-import PanelMenu from 'primevue/panelmenu';
-
-const customBreakpoint = "1024";
-
-const items = [
+  </template>
+  
+  <script setup>
+  import Menubar from 'primevue/menubar';
+  import PanelMenu from 'primevue/panelmenu';
+  
+  const items = [
     {
-        label: 'Home',
+      label: 'What is IAM?',
     },
     {
-        label: 'Features',
+      label: 'What is SASE?',
     },
     {
-        label: 'Projects',
-        items: [
+      label: 'Authentication',
+      items: [
+        {
+          label: 'Item 1',
+        },
+        {
+          label: 'Item 2',
+        },
+        {
+          label: 'Item 3',
+        },
+        {
+          label: 'Item 4',
+          items: [
             {
-                label: 'Components',
+              label: 'Item 4.1',
             },
             {
-                label: 'Blocks',
-            },
-            {
-                label: 'UI Kit',
-            },
-            {
-                label: 'Templates',
-                items: [
-                    {
-                        label: 'Apollo',
-                    },
-                    {
-                        label: 'Ultima',
-                    }
-                ]
+              label: 'Item 4.2',
             }
-        ]
+          ]
+        }
+      ]
     },
     {
-        label: 'Contact',
-    }
-];
-const teste = [
-    {
-        label: 'Highlighted',
-        items: [
+      label: 'Remote access',
+      items: [
+        {
+          label: 'Item 1',
+        },
+        {
+          label: 'Item 2',
+        },
+        {
+          label: 'Item 3',
+        },
+        {
+          label: 'Item 4',
+          items: [
             {
-                label: 'Styled',
-                route: '/theming'
+              label: 'Item 4.1',
             },
             {
-                label: 'Unstyled',
-                route: '/unstyled'
+              label: 'Item 4.2',
             }
-        ]
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Access glossary',
+      items: [
+        {
+          label: 'Item 1',
+        },
+        {
+          label: 'Item 2',
+        },
+        {
+          label: 'Item 3',
+        },
+        {
+          label: 'Item 4',
+          items: [
+            {
+              label: 'Item 4.1',
+            },
+            {
+              label: 'Item 4.2',
+            }
+          ]
+        }
+      ]
+    },
+  ];
+  
+  const mobile = [
+    {
+      label: 'What is IAM?',
+      items: []
     }
-];
-</script>
+  ];
+  
+  // Adicionando os itens de 'items' para 'teste[0].items'
+  items.forEach(item => mobile[0].items.push(item));
+  
+  const mergedItems = [...items];
+  
+  </script>
+  
