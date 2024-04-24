b8d1dacddd6f598e667f3a410f36b4098a3957e5
Luis Igreja <lfsigreja@gmail.com>
2024-04-24 12:11:23 -0300

    feat: simple footer

---
+++ src/templates/footer/simple-footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="flex flex-col gap-8 justify-center items-center pb-12">
+      <Divider />
+      <div class="px-container">
+        <p class="text-color-secondary text-xs">
+          {{ text }}
+        </p>
+      </div>
+    </footer>
+</template>
+
+<script setup>
+import Divider from "primevue/divider"
+
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
