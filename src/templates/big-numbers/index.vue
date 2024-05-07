1c9168c6bcaf20ddd185efb1b5eade1db8aab333
Eduardo de Cesaro <44036260+cesaroeduardo@users.noreply.github.com>
2024-05-07 15:41:17 -0300

    [UXE-2847] Big number adjusts (#79)
    * chore: update exemplo app
    
    * feat: incrementos big numbers
    
    * chore: fonte medium nos p

--- src/templates/big-numbers/index.vue
+++ src/templates/big-numbers/index.vue
@@ -1,10 +1,25 @@
 <template>
   <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8 gap-y-6 py-10" :class="[{ 'border surface-border' : border}]">
-    <li class="flex flex-col gap-5 justify-evenly min-w-32 md:px-4 max-w-64" v-for="item in items" :key="item.title">
-      <strong class="text-6xl text-center">
+    <li class="flex flex-col gap-2 justify-evenly min-w-32 md:px-4 max-w-64" v-for="item in items" :key="item.title">
+      <p class="text-base text-color-secondary font-medium" 
+        :class="[
+            {'text-center' : centralized },
+            {'text-left' : !centralized }
+          ]">
+        {{ item.titleup }}
+      </p>
+      <strong class="text-6xl font-medium"
+      :class="[
+            {'text-center' : centralized },
+            {'text-left' : !centralized }
+          ]">
         {{ item.title }}
       </strong>
-      <p class="text-sm text-color-secondary text-center">
+      <p class="text-base text-color-secondary font-medium"
+      :class="[
+            {'text-center' : centralized },
+            {'text-left' : !centralized }
+          ]">
         {{ item.description }}
       </p>
     </li>
@@ -20,6 +35,9 @@ defineProps({
   }
   */
   items: { type: Array, required: true },
+  centralized: {
+    type: Boolean, required: false, default: true
+  },
   border: {
     type: Boolean, required: false, default: true
   }
