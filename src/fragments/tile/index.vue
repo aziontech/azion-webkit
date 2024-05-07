a00d99e817a51d94bff7f49feb55ca978c193a04
Luis Igreja <lfsigreja@gmail.com>
2024-05-07 11:04:06 -0300

    fix: trigger deploy

--- src/fragments/tile/index.vue
+++ src/fragments/tile/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="bg-[--surface-400] rounded h-6 w-6 flex justify-center items-center">
-    <p class="text-sm flex justify-center items-center"> <slot /> </p>
+    <p class="text-sm flex justify-center items-center">
+      <slot />
+    </p>
   </div>
 </template>
