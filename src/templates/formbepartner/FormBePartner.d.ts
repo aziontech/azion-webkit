25eb5c5def492f53185f788a4c154467c167380d
Robson Junior <robson.ga.junior@gmail.com>
2024-05-20 12:16:09 -0300

    WIP: migrated formblocks

---
+++ src/templates/formbepartner/FormBePartner.d.ts
@@ -0,0 +1,53 @@
+/**
+ *
+ * formbepartner
+ *
+ *
+ * @module formbepartner
+ */
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+/**
+ * Defines valid properties in titlelist component.
+ */
+export interface FormBePartnerProps {
+  t: object;
+  hubspot: object;
+  requester: string;
+  requesterName: string;
+  responseMessages?: object;
+}
+
+/**
+ * Defines valid slots in Avatar component.
+ */
+export interface FormBePartnerSlots {
+  /**
+   * Content can easily be customized with the default slot instead of using the built-in modes.
+   */
+  default(): VNode[];
+}
+
+/**
+ * Defines valid emits in Avatar component.
+ */
+export interface FormBePartnerEmits {
+  /**
+   * Triggered when an error occurs while loading an image file.
+   */
+  error(event: Event): void;
+}
+
+/**
+ * @group Component
+ */
+declare class FormBePartner extends ClassComponent<FormBePartnerProps, FormBePartnerSlots, FormBePartnerEmits> { }
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    FormBePartner: GlobalComponentConstructor<FormBePartner>;
+  }
+}
+
+export default FormBePartner;
---
+++ src/templates/formjobapplication/FormJobApplication.d.ts
@@ -0,0 +1,50 @@
+/**
+ *
+ * formjobapplication
+ *
+ *
+ * @module formjobapplication
+ */
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+/**
+ * Defines valid properties in titlelist component.
+ */
+export interface FormJobApplicationProps {
+  t: object;
+  jobid: object;
+}
+
+/**
+ * Defines valid slots in Avatar component.
+ */
+export interface FormJobApplicationSlots {
+  /**
+   * Content can easily be customized with the default slot instead of using the built-in modes.
+   */
+  default(): VNode[];
+}
+
+/**
+ * Defines valid emits in Avatar component.
+ */
+export interface FormJobApplicationEmits {
+  /**
+   * Triggered when an error occurs while loading an image file.
+   */
+  error(event: Event): void;
+}
+
+/**
+ * @group Component
+ */
+declare class FormJobApplication extends ClassComponent<FormJobApplicationProps, FormJobApplicationSlots, FormJobApplicationEmits> { }
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    FormJobApplication: GlobalComponentConstructor<FormJobApplication>;
+  }
+}
+
+export default FormJobApplication;
