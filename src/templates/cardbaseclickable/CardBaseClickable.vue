5d72122edd4f878c475b03de71be3b41dfc6cd56
Robson Junior <robson.ga.junior@gmail.com>
2024-05-20 14:00:52 -0300

    fix: reexport cards

---
+++ src/templates/cardbaseclickable/CardBaseClickable.vue
@@ -0,0 +1,61 @@
+<template>
+  <a :href="link" :title="title" class="no-underline flex group border-radius group focus:outline-[rgba(243,100,43,.624)]">
+    <Card :pt="{
+      root: { class: 'w-full shadow-none bg-transparent flex flex-col rounded-md border surface-border border surface-border overflow-clip group-hover:border-current h-full' },
+      body: { class: 'h-full' },
+      content: { class: 'h-full' }
+    }">
+      <template v-if="$slots.header" #header>
+        <div class="flex flex-col gap-8" :class="[
+          { 'p-4 md:p-6': spacing === 'compact' },
+          { 'p-5 md:p-8': spacing === 'base' },
+          { 'p-6 md:p-10': spacing === 'relaxed' },
+        ]">
+          <slot name="header" />
+        </div>
+      </template>
+      <template #content>
+        <template v-if="$slots.content || $slots.actions">
+          <div class="flex flex-col gap-8" :class="[
+            { 'p-4 md:p-6': spacing === 'compact' },
+            { 'p-5 md:p-8': spacing === 'base' },
+            { 'p-6 md:p-10': spacing === 'relaxed' },
+          ]">
+            <div class="flex flex-col gap-3">
+              <slot name="content" />
+            </div>
+            <template v-if="$slots.actions">
+              <div class="flex flex-col md:flex-row flex-wrap gap-2">
+                <slot name="actions" />
+              </div>
+            </template>
+          </div>
+        </template>
+        <template v-if="$slots['content-raw']">
+          <slot name="content-raw" />
+        </template>
+      </template>
+    </Card>
+  </a>
+</template>
+
+<script setup>
+import Card from "primevue/card"
+
+defineProps({
+  spacing: {
+    type: String,
+    required: false,
+    options: ['compact', 'relaxed', 'base'],
+    default: 'base'
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: false
+  }
+})
+</script>
---
+++ src/templates/cardcases/CardCases.d.ts
@@ -0,0 +1,52 @@
+/**
+ *
+ * cardcases
+ *
+ * @module cardcases
+ */
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+/**
+ * Defines valid properties in cardbase component.
+ */
+export interface CardCasesProps {
+  image?: string;
+  imgAlt?: string;
+  tagList: string;
+  description: string;
+  link: URL;
+}
+
+/**
+ * Defines valid slots in Avatar component.
+ */
+export interface CardCasesSlots {
+  /**
+   * Content can easily be customized with the default slot instead of using the built-in modes.
+   */
+  default(): VNode[];
+}
+
+/**
+ * Defines valid emits in Avatar component.
+ */
+export interface CardCasesEmits {
+  /**
+   * Triggered when an error occurs while loading an image file.
+   */
+  error(event: Event): void;
+}
+
+/**
+ * @group Component
+ */
+declare class CardCases extends ClassComponent<CardCasesProps, CardCasesSlots, CardCasesEmits> { }
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    CardCases: GlobalComponentConstructor<CardCases>;
+  }
+}
+
+export default CardCases;
