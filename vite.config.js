a538b5e6efa8c3aa2dd5eec797a18eea17a5a3b3
Robson Junior <robson.ga.junior@gmail.com>
2024-05-22 21:51:11 -0300

    chore: updates to launch package

--- vite.config.js
+++ vite.config.js
@@ -9,33 +9,23 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
-
-        // 'webkit/LinkButton': resolve(__dirname, 'src/templates/button/link/LinkButton.vue'),
-        // 'webkit/ReadableContent': resolve(__dirname, 'src/templates/post/readble-content/ReadbleContent.vue')
       },
     },
-    build: {
-      minify: 'esbuild',
-      cssMinify: true,
-      sourcemap: 'inline',
-      // LIB or ROLLUP input
-      // lib: {
-      //   entry: resolve(__dirname, 'src/index.js'),
-      //   name: 'webkit',
-      // },
-      rollupOptions: {
-        external: ['vue'],
-        input: [
-          resolve(__dirname, 'src/templates/button/link/LinkButton.vue'),
-          resolve(__dirname, 'src/templates/post/readable-content/ReadableContent.vue')
-        ],
-        output: {
-          // inlineDynamicImports: false,
-          globals: {vue: 'vue'},
-          dir: 'dist'
-        }
-      }
-    }
+    // build: {
+    //   minify: 'esbuild',
+    //   cssMinify: true,
+    //   sourcemap: 'inline',
+    //   rollupOptions: {
+    //     external: ['vue'],
+    //     input: [
+    //     ],
+    //     output: {
+    //       // inlineDynamicImports: false,
+    //       globals: {vue: 'vue'},
+    //       dir: 'dist'
+    //     }
+    //   }
+    // }
   }
 
   return config;
