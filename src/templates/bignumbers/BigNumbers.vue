73d94b1aaae27094c35403f9c1bacfb4ff4205b8
Luis Igreja <lfsigreja@gmail.com>
2024-06-03 11:50:45 -0300

    style: big numbers fonts + tile variation

--- src/templates/bignumbers/BigNumbers.vue
+++ src/templates/bignumbers/BigNumbers.vue
@@ -1,25 +1,15 @@
 <template>
-  <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8 gap-y-6 py-10" :class="[{ 'border surface-border' : border}]">
+  <ul class="flex flex-col md:flex-row items-center md:items-start flex-wrap justify-center rounded px-8 gap-y-6 py-10"
+    :class="[{ 'border surface-border surface-card': border }]">
     <li class="flex flex-col gap-2 justify-evenly min-w-32 md:px-4 max-w-64" v-for="item in items" :key="item.title">
-      <p class="text-base text-color-secondary font-medium" 
-        :class="[
-            {'text-center' : centralized },
-            {'text-left' : !centralized }
-          ]">
+      <Tile v-if="item.icon" severity="primary"> <i :class="item.icon" /> </Tile>
+      <p v-if="item.titleup" class="text-sm text-color-secondary">
         {{ item.titleup }}
       </p>
-      <strong class="text-6xl font-medium"
-      :class="[
-            {'text-center' : centralized },
-            {'text-left' : !centralized }
-          ]">
+      <strong class="text-7xl font-semibold ">
         {{ item.title }}
       </strong>
-      <p class="text-base text-color-secondary font-medium"
-      :class="[
-            {'text-center' : centralized },
-            {'text-left' : !centralized }
-          ]">
+      <p v-if="item.description" class="text-sm text-color-secondary">
         {{ item.description }}
       </p>
     </li>
@@ -27,19 +17,22 @@
 </template>
 
 <script setup>
+import Tile from "../tile"
+
 defineProps({
-  /*
-  {
-    title: { type: String, required: true },
-    description: { type: String, required: true }
-  }
-  */
-  items: { type: Array, required: true },
+  items: {
+    type: Array,
+    required: true
+  },
   centralized: {
-    type: Boolean, required: false, default: true
+    type: Boolean,
+    required: false,
+    default: true
   },
   border: {
-    type: Boolean, required: false, default: true
+    type: Boolean,
+    required: false,
+    default: false
   }
 });
 </script>
