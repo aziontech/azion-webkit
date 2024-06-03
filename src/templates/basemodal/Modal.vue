dc6bdc1e1850fa030d4ca5cb7b6bb7c4501f175f
Luis Igreja <lfsigreja@gmail.com>
2024-06-03 14:41:06 -0300

    feat: modal outlined

--- src/templates/basemodal/Modal.vue
+++ src/templates/basemodal/Modal.vue
@@ -1,13 +1,15 @@
 <template>
-  <div @click="visible = true">
+  <div @click="visible = true" class="cursor-pointer">
     <slot name="action"/>
   </div>
   <div class="px-container">
-    <Dialog v-model:visible="visible" modal dismissableMask closeOnEscape :showHeader="showHeader" :style="customStyle">
+    <Dialog v-model:visible="visible" modal dismissableMask closeOnEscape :showHeader="showHeader" :pt="getPTOptions()">
       <template #header v-if="$slots.header">
         <slot name="header" />
       </template>
-      <slot name="content" />
+      <div :class="{ 'border surface-border rounded' : backgroundColor == 'outlined'}">
+        <slot name="content" />
+      </div>
       <template #footer v-if="$slots.footer">
         <slot name="footer" />
       </template>
@@ -19,18 +21,31 @@
 import Dialog from 'primevue/dialog';
 import { ref } from "vue";
 
-defineProps({
-  customStyle: {
+const props = defineProps({
+  backgroundColor: {
     type: String,
     required: false,
-    default: ''
+    default: 'default',
+    options: ['outlined', 'default']
   },
   showHeader: {
     type: Boolean,
     required: false,
     default: true
-  }
+  },
+
 })
 
 const visible = ref(false);
+
+const getPTOptions = () => {
+  if (props.backgroundColor == 'outlined') {
+    return {
+      content: 'bg-transparent p-0',
+      root: 'shadow-transparent shadow-none',
+      header: 'bg-transparent border-none p-0 pb-4 ml-auto',
+      closeButton: 'border-none hover:bg-[--surface-400] hover:text-color-secondary text-color',
+    }
+  }
+}
 </script>
