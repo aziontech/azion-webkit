be0a89711c5442017677e0ff2742d281f5485e2b
Robson Júnior <robson.ga.junior@gmail.com>
2024-06-13 21:57:03 -0300

    fix: max-w-48 to description of bignumbers, not title

--- src/templates/bignumbers/BigNumbers.vue
+++ src/templates/bignumbers/BigNumbers.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="flex flex-col md:flex-row gap-8 md:gap-16 md:items-center flex-wrap justify-center rounded px-8 py-10">
-    <li class="flex flex-col gap-4 justify-evenly max-w-48" v-for="item in items" :key="item.title">
+    <li class="flex flex-col gap-4 justify-evenly" v-for="item in items" :key="item.title">
       <Tile v-if="item.icon" severity="primary"> <i :class="item.icon" /> </Tile>
       <div class="flex flex-col gap-0.5">
         <p v-if="item.titleup" class="text-sm text-color-secondary">
@@ -9,7 +9,7 @@
         <strong class="text-7xl font-semibold leading-tight">
           {{ item.title }}
         </strong>
-        <p v-if="item.description" class="text-sm text-color-secondary md:min-h-10">
+        <p v-if="item.description" class="text-sm text-color-secondary md:min-h-10 max-w-48">
           {{ item.description }}
         </p>
       </div>
