ba23f22a10b638c286b5b3a1cb68cfd5d0566a13
Luis Igreja <lfsigreja@gmail.com>
2024-06-24 17:57:16 -0300

    fix: big numbers responsiveness

--- src/templates/bignumbers/BigNumbers.vue
+++ src/templates/bignumbers/BigNumbers.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-wrap gap-8 sm:gap-10 md:gap-16 justify-center px-0 sm:px-4 md:px-8 py-6 md:py-10">
-    <div class="flex flex-col gap-3 md:gap-5 w-fit" v-for="item in items" :key="item.title">
+    <div class="flex basis-5/12 sm:basis-0 justify-center" v-for="item in items" :key="item.title">
+      <div class="flex flex-col gap-3 md:gap-5 w-fit min-w-32 sm:min-w-none">
       <Tile v-if="item.icon" severity="primary"> <i :class="item.icon" class="text-sm md:text-md" /> </Tile>
       <div class="flex flex-col gap-2 justify-evenly w-fit">
         <p v-if="item.titleup" class="text-xs md:text-sm text-color-secondary">
@@ -12,6 +13,7 @@
         <p v-if="item.description" class="text-xs md:text-sm text-color-secondary md:min-h-10 text-wrap w-full max-w-36 md:max-w-52">
           {{ item.description }}
         </p>
+      </div>
       </div>
     </div>
   </div>
