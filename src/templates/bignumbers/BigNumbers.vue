a7c78176a31d9124f202612c1d2e11c4a323ac32
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-06-24 11:14:50 -0300

    fix: responsive big numbers

--- src/templates/bignumbers/BigNumbers.vue
+++ src/templates/bignumbers/BigNumbers.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-wrap gap-8 sm:gap-10 md:gap-16 justify-center px-0 sm:px-4 md:px-8 py-10">
-    <div class="flex flex-col gap-4 w-fit max-w-48" v-for="item in items" :key="item.title">
+    <div class="flex flex-col gap-3 md:gap-5 w-fit" v-for="item in items" :key="item.title">
       <Tile v-if="item.icon" severity="primary"> <i :class="item.icon" class="text-sm md:text-md" /> </Tile>
-      <div class="flex flex-col gap-2 justify-evenly">
+      <div class="flex flex-col gap-2 justify-evenly w-fit">
         <p v-if="item.titleup" class="text-xs md:text-sm text-color-secondary">
           {{ item.titleup }}
         </p>
-        <strong class="md:text-7xl text-6xl font-semibold leading-tight">
+        <strong class="md:text-7xl text-5xl font-semibold w-fit leading-tight text-nowrap">
           {{ item.title }}
         </strong>
-        <p v-if="item.description" class="text-xs md:text-sm text-color-secondary md:min-h-10">
+        <p v-if="item.description" class="text-xs md:text-sm text-color-secondary md:min-h-10 text-wrap w-full max-w-36 md:max-w-52">
           {{ item.description }}
         </p>
       </div>
