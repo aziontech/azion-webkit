ca41056976ed276b0baa5ce5bd0e4c32d8dbc022
Luis Igreja <lfsigreja@gmail.com>
2024-06-24 14:00:03 -0300

    feat: table of contents > scroll observer to show current heading

--- src/templates/tableofcontents/TableOfContents.vue
+++ src/templates/tableofcontents/TableOfContents.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-col gap-2">
-    <p class="font-medium text-base"> {{ title }} </p>
-    <TableOfContentsItem :data="toc" />
+    <p class="font-medium text-base">{{ title }}</p>
+    <TableOfContentsItem :data="toc" :currentHeading="currentHeading" />
   </div>
 </template>
 
 <script setup>
   import TableOfContentsItem from './TableOfContentsItem.vue'
+  import { ref, onBeforeMount } from 'vue'
+
   const props = defineProps({
     headings: {
       required: true
@@ -14,7 +16,7 @@
     title: {
       type: String,
       required: false,
-      default: "Jump to Section"
+      default: 'Jump to Section'
     }
   })
 
@@ -54,4 +56,30 @@
   }
 
   const toc = generateToc(props.headings)
+  const currentHeading = ref(toc[0].slug)
+
+  const observerOptions = { rootMargin: '-40px 0% -80%', threshold: 1 }
+
+  onBeforeMount(() => {
+    if (currentHeading.value) {
+      const setCurrent = (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            if (currentHeading.value == entry.target.id) {
+              break
+            }
+            currentHeading.value = entry.target.id
+            console.log(currentHeading.value)
+            break
+          }
+        }
+      }
+
+      const headingsObserver = new IntersectionObserver(setCurrent, observerOptions)
+
+      const h1 = document.getElementById('overview')
+      if (h1) headingsObserver.observe(h1)
+      document.querySelectorAll('article :is(h1,h2,h3)').forEach((h) => headingsObserver.observe(h))
+    }
+  })
 </script>
--- src/templates/tableofcontents/TableOfContentsItem.vue
+++ src/templates/tableofcontents/TableOfContentsItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul class="flex flex-col gap-2">
-    <li v-for="{ slug, text, children } in data" class="flex flex-col gap-2 overflow-hidden">
-      <a class="rounded hover:underline text-color-secondary text-sm truncate max-w-full" :href="`#${slug}`">
+  <ul class="flex flex-col gap-1">
+    <li v-for="{ slug, text, children } in data" class="flex flex-col gap-1 overflow-hidden">
+      <a :class="['rounded hover:underline text-color-secondary text-sm max-w-full px-2 py-1', currentHeading === slug && 'surface-200']" :href="`#${slug}`">
         {{ text }}
       </a>
       <template v-if="children.length > 0">
@@ -16,6 +16,10 @@
     data: {
       type: Array,
       required: true,
+    },
+    currentHeading: {
+      type: String,
+      required: false,
     }
   })
 </script>
