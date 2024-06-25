ab95a216ae33845aa6eed434b1b7fc523323b44f
Luis Igreja <lfsigreja@gmail.com>
2024-06-25 14:24:31 -0300

    fix: increase big numbers gap

--- src/templates/bignumbers/BigNumbers.vue
+++ src/templates/bignumbers/BigNumbers.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap gap-8 sm:gap-10 md:gap-16 justify-center px-0 sm:px-4 md:px-8 py-6 md:py-10">
+  <div class="flex flex-wrap gap-8 sm:gap-10 md:gap-20 justify-center px-0 sm:px-4 md:px-8 py-6 md:py-10">
     <div class="flex basis-5/12 sm:basis-0 justify-center" v-for="item in items" :key="item.title">
       <div class="flex flex-col gap-3 md:gap-5 w-fit min-w-32 sm:min-w-none">
       <Tile v-if="item.icon" severity="primary"> <i :class="item.icon" class="text-sm md:text-md" /> </Tile>
