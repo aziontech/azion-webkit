6377040d22d2a2453342477767a039ecc861360d
Luis Igreja <lfsigreja@gmail.com>
2024-07-02 19:05:33 -0300

    chore: azion logos

---
+++ src/stories/Illustrations/AzionLogo.stories.js
@@ -0,0 +1,17 @@
+import AzionLogo from '../../templates/azionlogo'
+
+export default {
+  title: 'Azion Web Kit/Illustrations/Azion Logo',
+  component: AzionLogo,
+  tags: ['autodocs'],
+  argTypes: {
+  }
+}
+
+export const Default = {
+  args: {
+    hrefTitle: 'azion logo',
+    href: 'https://www.azion.com/en/',
+    target: '_self'
+  }
+}
