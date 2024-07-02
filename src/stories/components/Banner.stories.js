8e1b10ad14ab4c4af810253d8c5450653fa20344
Luis Igreja <lfsigreja@gmail.com>
2024-07-02 19:05:04 -0300

    chore: Banner component

---
+++ src/stories/components/Banner.stories.js
@@ -0,0 +1,20 @@
+import Banner from '../../templates/banner'
+
+export default {
+  title: 'Azion Web Kit/components/Banner',
+  component: Banner,
+  tags: ['autodocs'],
+  argTypes: {
+    cta: {
+      description: "Call to Action - anchor text"
+    }
+  }
+}
+
+export const Default = {
+  args: {
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis urna dui, elementum eget euismod ut, scelerisque id erat. Fusce iaculis velit consectetur",
+    cta: "congue dapibuse.",
+    link: ""
+  }
+}
