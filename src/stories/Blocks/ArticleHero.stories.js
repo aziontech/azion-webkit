b625a9a10a3e43baa67638b4b3a7bde741ca7f44
Luis Igreja <lfsigreja@gmail.com>
2024-07-04 10:37:42 -0300

    chore: Article Hero storie

---
+++ src/stories/Blocks/ArticleHero.stories.js
@@ -0,0 +1,26 @@
+import HeroArticle from '../../templates/heroarticle';
+import data from '../../mock/articleHero'
+
+export default {
+  title: 'Azion Web Kit/Blocks/Article Hero',
+  component: HeroArticle,
+  tags: ['autodocs'],
+  argTypes: {}
+};
+
+const ArticleHeroTemplate = `
+<HeroArticle v-bind="args">
+  <template #breadcrumbs> [#breadcrumbs] </template>
+</HeroArticle>
+`
+
+const Template = (args) => ({
+  components: { HeroArticle },
+  setup() {
+    return { args }
+  },
+  template: ArticleHeroTemplate
+})
+
+export const Default = Template.bind({})
+Default.args = data
