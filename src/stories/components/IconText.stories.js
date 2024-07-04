20ef86ca8a97bd421e48b48620d6d1db97491ae0
Luis Igreja <lfsigreja@gmail.com>
2024-07-04 10:44:40 -0300

    chore: IconText storie

---
+++ src/stories/components/IconText.stories.js
@@ -0,0 +1,40 @@
+import IconText from "../../templates/icontext"
+
+export default {
+  title: 'Azion Web Kit/Components/Icon Text',
+  component: IconText,
+  tags: ['autodocs'],
+  argTypes: {
+    severity: {
+      options: ['default', 'primary'],
+      control: {
+        type: 'select',
+      },
+      description: "Chnage icon color"
+    },
+    tagSeverity: {
+      description: "Check PrimeVue tags"
+    }
+  }
+};
+
+const props = {
+  title: "Lorem ipsun magna",
+  description: "lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor et aliquet",
+  icon: "pi pi-check",
+}
+
+export const Default = {
+  args: {
+    ...props,
+    severity: 'primary'
+  }
+}
+
+export const WithTags = {
+ args: {
+  tag: 'Azion',
+  tagSeverity: 'info',
+  ...props
+ }
+}
