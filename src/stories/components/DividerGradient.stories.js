0c55aacaabb921eecde4b2bc85d0988271e80d5b
Luis Igreja <lfsigreja@gmail.com>
2024-07-04 09:42:54 -0300

    chore: divider gradient storie

---
+++ src/stories/components/DividerGradient.stories.js
@@ -0,0 +1,9 @@
+import DividerGradient from "../../templates/dividergradient"
+
+export default {
+  title: 'Azion Web Kit/Components/Divider Gradient',
+  component: DividerGradient,
+  tags: ['autodocs'],
+}
+
+export const Default = {}
