fe723bfa341824725366914b028f6ca6378454d6
Luis Igreja <lfsigreja@gmail.com>
2024-07-04 11:43:33 -0300

    chore: overline storie

---
+++ src/stories/components/Overline.stories.js
@@ -0,0 +1,13 @@
+import Overline from "../../templates/Overline"
+
+export default {
+  title: 'Azion Web Kit/Components/Overline',
+  component: Overline,
+  tags: ['autodocs'],
+}
+
+export const Default =  {
+  args: {
+    label: 'Sign up'
+  }
+}
