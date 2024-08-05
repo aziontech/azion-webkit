d9028b33bc99b7652e65415480e369d43e6b3f26
Luis Igreja <lfsigreja@gmail.com>
2024-08-05 15:24:28 -0300

    [UXE-3304] feat: handle default theme and system preference color (#100)
    * chore: refactor dropdown theme switcher
    
    * chore: handle system prefers-color-scheme
    
    * refactor: set theme by class

--- src/templates/dropdownthemeswitcher/DropdownThemeSwitcher.vue
+++ src/templates/dropdownthemeswitcher/DropdownThemeSwitcher.vue
@@ -1,5 +1,5 @@
 <template>
-  <Dropdown :options="themes" :modelValue="selectedTheme" :autoOptionFocus="false" @update:modelValue="onClick"
+  <Dropdown :options="themes" :modelValue="selectedTheme" :autoOptionFocus="false" @update:modelValue="handleThemeChange"
     optionLabel="label">
     <template #value="slotProps">
       <div v-if="slotProps.value" class="flex gap-2 align-items-center">
@@ -19,11 +19,6 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import Dropdown from 'primevue/dropdown';
-
-const theme = {
-  light: 'azion-light',
-  dark: 'azion-dark'
-};
 
 const selectedTheme = ref({
     label: 'System',
@@ -45,48 +40,58 @@ const themes = [
   },
 ]
 
-let systemColorScheme; /** onMounted variables, needed for Astro to use browser api */
-let colorScheme;
+let systemColorScheme = ref();
+let isWatchingSystemScheme = ref(false)
 
 onMounted(() => {
-  systemColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
-  colorScheme = localStorage.getItem("prefers-color-scheme");
+  systemColorScheme.value = window.matchMedia('(prefers-color-scheme: dark)'); // Get system prefered color scheme
+  const colorScheme = localStorage.getItem("prefers-color-scheme"); // Check if there is pre-defined site prefered color scheme
 
-  if (colorScheme && colorScheme !== 'System') {
-    selectedTheme.value = themes.find(theme => theme.label === colorScheme);
-  }
+  if (colorScheme && colorScheme !== 'System') selectedTheme.value = themes.find(theme => theme.label === colorScheme); // update dropdown placeholder value
+  else watchSystemColorSchemePreferences()
 })
 
-function getHTML() {
-  return document.querySelector('html');
-};
+// Watch for changes on system color scheme changes
+function watchSystemColorSchemePreferences() {
+  isWatchingSystemScheme.value = true
+  systemColorScheme.value.addEventListener('change', handlePreferColorSchemeChange);
+}
 
-function getTheme(selectedTheme) {
-  return selectedTheme === "Dark" ? theme.dark : theme.light;
-};
 
-function resetTheme() {
-  const html = getHTML();
-
-  html.classList.remove('azion-light');
-  html.classList.remove('azion-dark');
-};
+function removeWatchSystemColorSchemePreferences() {
+  isWatchingSystemScheme.value = false
+  systemColorScheme.value.removeEventListener('change', handlePreferColorSchemeChange);
+}
 
 function getStystemDefaultTheme() {
-  return systemColorScheme.matches ? 'Dark' : 'Light';
+  return systemColorScheme.value.matches ? 'Dark' : 'Light';
+}
+
+function toggleTheme(add, remove) {
+  document.documentElement.classList.add(add)
+  document.documentElement.classList.remove(remove)
 }
 
 function changeTheme(theme) {
+  if (selectedTheme.value.label === theme.label && theme.label !== 'System') return
+
   selectedTheme.value = theme
   const themeLabel = theme.label === 'System' ? getStystemDefaultTheme() : theme.label
 
-  resetTheme();
-  getHTML().classList.add(getTheme(themeLabel));
+  if (themeLabel === 'Light') toggleTheme('azion-light', 'azion-dark')
+  if (themeLabel === 'Dark') toggleTheme('azion-dark', 'azion-light')
 }
 
-function onClick(theme) {
+function handlePreferColorSchemeChange() {
+  const colorScheme = localStorage.getItem("prefers-color-scheme");
+  if (colorScheme == 'System' || !colorScheme) changeTheme(themes[0]) // Select the 'System' in the themes array
+}
+
+function handleThemeChange(theme) {
   changeTheme(theme)
+
+  if (!isWatchingSystemScheme.value && theme.label === 'System') watchSystemColorSchemePreferences()
+  if (isWatchingSystemScheme.value && theme.label !== 'System') removeWatchSystemColorSchemePreferences()
   localStorage.setItem('prefers-color-scheme', theme.label)
 };
-
 </script>
