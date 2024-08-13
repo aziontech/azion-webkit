dde0f6d679351606a83652072a728cfd29c32a93
Robson Júnior <robson.ga.junior@gmail.com>
2024-08-13 12:10:19 -0300

    chore: adding new ArticleNavigation content variation, using docs and blog

--- src/stories/components/ArticleNavigation.stories.js
+++ src/stories/components/ArticleNavigation.stories.js
@@ -19,21 +19,39 @@ const Template = (args) => ({
   template: '<ArticleNavigation v-bind="args" />',
 })
 
-export const Default = Template.bind({})
-Default.args = {
+export const Docs = Template.bind({})
+Docs.args = {
   cards: {
     prev: {
-      text: 'Lorem Ipsum',
-      link: ''
+      text: 'Get help',
+      link: 'https://www.azion.com/en/documentation/products/get-help/'
     },
     next: {
-      text: 'Lorem Ipsum',
-      link: ''
+      text: 'Start with a template',
+      link: 'https://www.azion.com/en/documentation/products/start-with-a-template/'
     }
   },
   overline: {
-    next: "Next",
-    prev: "Previous"
+    next: "Next Page",
+    prev: "Back"
+  }
+}
+
+export const Blog = Template.bind({});
+Blog.args = {
+  cards: {
+    prev: {
+      text: 'Implement a Tiered Cache and Reduce Costs for Your E-commerce',
+      link: 'https://www.azion.com/en/blog/implement-tiered-cache/'
+    },
+    next: {
+      text: "Enhancing Data Management with Azion's Edge Storage",
+      link: 'https://www.azion.com/en/blog/enhancing-data-management-with-azion-edge-storage/'
+    }
+  },
+  overline: {
+    next: "Next Article",
+    prev: "Previous Article"
   }
 }
 
