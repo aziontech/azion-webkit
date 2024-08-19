42918e7b3e2516803be7c3666096790020755c91
Robson Junior <robson.ga.junior@gmail.com>
2024-08-19 15:36:30 -0300

    wip: globe css dark

--- src/templates/globe/Globe.vue
+++ src/templates/globe/Globe.vue
@@ -14,33 +14,33 @@
 
   /* Globe */
   .globe {
-    width: 600px;
-    height: 600px;
+    width: 300px;
+    height: 300px;
     border-radius: 50%;
-    background: conic-gradient(from 180deg at 50% 50%, #0077be, #004a87, #0077be);
-    transform-style: preserve-3d;
-    animation: rotate 10s infinite linear;
+    background: radial-gradient(600px at 20px 464px, rgba(111, 111, 111, 0.14), transparent 60%);
     position: relative;
     overflow: hidden;
+    box-shadow: 0 0 10px rgba(243,101,43, .2);
   }
 
   /* Inner Globe */
   .globe-inner {
     width: 100%;
     height: 100%;
-    background: url('map-dark@2x.png') center/cover;
+    background: url('./map-dark@2x.png');
+    background-size: 600px;
     border-radius: 50%;
-    transform: translateZ(100px);
+    transform-style: preserve-3d;
+    animation: rotate 36.5s infinite linear;
     box-shadow: 0 0 30px rgba(0, 0, 0, 0.5) inset;
   }
 
   /* Rotation Animation */
   @keyframes rotate {
-    from {
-      transform: rotateY(0deg);
-    }
-    to {
-      transform: rotateY(360deg);
+    0% {
+      background-position: 0 -60px;
+    } 100% {
+      background-position: -600px -60px;
     }
   }
 </style>
