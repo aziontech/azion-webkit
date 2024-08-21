f20bcef7d9dc628315f6f3f840d849d513c19728
Luis Igreja <lfsigreja@gmail.com>
2024-08-21 14:13:29 -0300

    chore: List with hero

---
+++ src/stories/templates/Lists/ListWithHero.stories.js
@@ -0,0 +1,360 @@
+import FeaturedPost from '../../../templates/featuredpost'
+import CardGridBlock from '../../../templates/cardgridblock'
+
+export default {
+  title: 'Blocks/List/Featured Hero',
+  tags: ['autodocs'],
+  argTypes: {
+    isSearchEnabled: {
+      options: [true,  false],
+      control: { type: 'radio' },
+      description: 'Enable/Disable Search'
+    },
+    isLoadMoreEnabled: {
+      options: [true,  false],
+      control: { type: 'radio' },
+      description: 'Enable/Disable Load More button'
+    },
+  }
+}
+
+const MOCK = {
+  isSearchEnabled: true,
+  isLoadMoreEnabled: false,
+  cards: {
+    "data": [
+      {
+        "tagList": [
+          "Developers"
+        ],
+        "title": "Enhancing Data Management with Azion's Edge Storage",
+        "description": "Explore Azion Edge Storage: a direct S3 API compatible object storage solution offering scalability, no egress charges, and serverless architecture for improved data management and application performance.",
+        "link": "/en/blog/enhancing-data-management-with-azion-edge-storage/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/storage-image.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/image-1-1.png",
+            "title": "Gabriel França",
+            "role": "Dev Writer"
+          }
+        ],
+        "date": "MAY 9, 2024",
+        "estimateReadTime": "8 min read"
+      },
+      {
+        "tagList": [
+          "Developers"
+        ],
+        "title": "Enhancing Edge Functions Debugging with Azion CLI Logs",
+        "description": "Explore how to debug and monitor edge functions using Azion CLI with real-time logs. Master command usage for local and network edge insights.",
+        "link": "/en/blog/enhancing-edge-functions-debugging-with-azion-cli-logs/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/azion-logs.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/image-1-1.png",
+            "title": "Gabriel França",
+            "role": "Dev Writer"
+          }
+        ],
+        "date": "MAY 9, 2024",
+        "estimateReadTime": "6 min read"
+      },
+      {
+        "tagList": [
+          "Developers",
+          "Caching"
+        ],
+        "title": "Implement a Tiered Cache and Reduce Costs for Your E-commerce",
+        "description": "Optimize e-commerce with Edge Computing: Enhance performance and scalability by implementing an efficient Tiered Cache strategy with Azion Edge Application.",
+        "link": "/en/blog/implement-tiered-cache/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/transformtheonlineshopping_2022_blogpost_v2.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/img-prof.png",
+            "title": "Mariana Bellorín Aguilera",
+            "role": "Technical Writer"
+          }
+        ],
+        "date": "APR 24, 2024",
+        "estimateReadTime": "7 min read"
+      },
+      {
+        "tagList": [
+          "Developers"
+        ],
+        "title": "Boost Software Quality: Local Development with Azion CLI",
+        "description": "Master Edge Functions with Azion CLI for local development and testing. Enhance software security, optimize performance, and streamline debugging.",
+        "link": "/en/blog/boost-software-quality-with-azion-cli-local-dev/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/local-dev.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/image-1-1.png",
+            "title": "Gabriel França",
+            "role": "Dev Writer"
+          }
+        ],
+        "date": "APR 19, 2024",
+        "estimateReadTime": "8 min read"
+      },
+      {
+        "tagList": [
+          "Developers"
+        ],
+        "title": "Unlock Effortless Edge Deployment with Azion Vulcan and Node.js Polyfills",
+        "description": "Explore how Azion's Edge Runtime and Vulcan enhance web development by enabling frameworks to run natively at the edge, optimizing speed and security.",
+        "link": "/en/blog/effortless-edge-deployment-with-vulcan/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/azion-cells-technical-approach-and-the-future-of-serverless-computing.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/image-1-1.png",
+            "title": "Gabriel França",
+            "role": "Dev Writer"
+          }
+        ],
+        "date": "FEB 26, 2024",
+        "estimateReadTime": "8 min read"
+      },
+      {
+        "tagList": [
+          "Developers"
+        ],
+        "title": "Azion Console Kit: Building a new interface",
+        "description": "Explore the Azion Console Kit, enhancing user interface for Edge Computing with innovative Azion Blocks and open source toolkits for robust app development.",
+        "link": "/en/blog/console-kit-building-a-new-interface/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbnailblog-codeeditor-chatgpt-440x343px.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/morgana-johann.png",
+            "title": "Morgana Johann",
+            "role": "Product Design Manager"
+          }
+        ],
+        "date": "FEB 21, 2024",
+        "estimateReadTime": "6 min read"
+      },
+      {
+        "tagList": [
+          "Company News",
+          "Developers"
+        ],
+        "title": "Supercharge Your Content Delivery Strategy With These Azion Templates",
+        "description": "Discover top-notch templates for Edge Applications to enhance content delivery, reduce latency, and optimize user experience with Azion's robust Edge Computing platform.",
+        "link": "/en/blog/supercharge-content-delivery-azion-templates/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/cdn-abre.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/whatsapp-image-2021-12-08-at-16-22-01.jpeg",
+            "title": "Thiago Silva",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "FEB 14, 2024",
+        "estimateReadTime": "5 min read"
+      },
+      {
+        "tagList": [
+          "Security"
+        ],
+        "title": "Azion’s WAF Is the Best Option for Businesses of All Sectors",
+        "description": "Explore how Azion's Web Application Firewall (WAF) fosters cybersecurity in diverse sectors, optimizes digital transformation, and uses edge computing for swift, efficient security solutions.",
+        "link": "/en/blog/azion-waf-best-option-businesses/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/waf-abre-02.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/retrato_branco_quadrado.png",
+            "title": "Rafael Rigues",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "FEB 9, 2024",
+        "estimateReadTime": "9 min read"
+      },
+      {
+        "tagList": [
+          "Security"
+        ],
+        "title": "The Experts Speak: Cybersecurity Quotes About Zero-Trust, WAF, Social Engineering, and More",
+        "description": "Explore expert insights on cybersecurity, Zero Trust, social engineering, and WAF capabilities at Azion Edge.",
+        "link": "/en/blog/the-experts-speak-cybersecurity-quotes-about-zero-trust-waf-social-engineering/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbblog-whatiswaf-440x343px-1.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/foto-isidro-iturat-2022.png",
+            "title": "Isidro Iturat Hernández",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "JAN 31, 2024",
+        "estimateReadTime": "7 min read"
+      },
+      {
+        "tagList": [
+          "Developers",
+          "Company News"
+        ],
+        "title": "Leverage the Power of Edge Computing with Azion's New Set of Templates",
+        "description": "Discover Azion's Edge Computing templates to enhance web app development, streamline deployment, and optimize performance with tools like MongoDB, Hexo, and Fauna.",
+        "link": "/en/blog/azion-new-set-templates-dev/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbnailblog-4templatesimproveexperience-440x343px-1.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/whatsapp-image-2021-12-08-at-16-22-01.jpeg",
+            "title": "Thiago Silva",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "JAN 29, 2024",
+        "estimateReadTime": "5 min read"
+      },
+      {
+        "tagList": [
+          "Security",
+          "Market Trends"
+        ],
+        "title": "Five Ways HTTP/3 Can Increase the Security of Your Applications",
+        "description": "Explore how HTTP/3 ensures web security with TLS 1.3 encryption, QUIC protocol, and IP spoofing prevention, enhancing performance for web applications.",
+        "link": "/en/blog/five-ways-http3-increase-application-security/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbnailblog-http-3-440x343.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/retrato_branco_quadrado.png",
+            "title": "Rafael Rigues",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "JAN 26, 2024",
+        "estimateReadTime": "7 min read"
+      },
+      {
+        "tagList": [
+          "Observability",
+          "Security",
+          "Company News"
+        ],
+        "title": "Enhance the security of your applications with the new Fingerprint feature of the Azion Marketplace",
+        "description": "Explore Azion Fingerprint for enhanced application security and device identification. Secure, scalable, and quick to implement.",
+        "link": "/en/blog/enhance-security-azion-fingerprint/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbnailblog-fingerprint-v2-440x343px.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/whatsapp-image-2021-12-08-at-16-22-01.jpeg",
+            "title": "Thiago Silva",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "JAN 19, 2024",
+        "estimateReadTime": "6 min read"
+      },
+      {
+        "tagList": [
+          "Security",
+          "Market Trends"
+        ],
+        "title": "Why did the WAF become essential for cybersecurity?",
+        "description": "Explore how Azion WAF ensures advanced cybersecurity against evolving threats with flexible rules and minimal operational complexity.",
+        "link": "/en/blog/why-waf-essential-cybersecurity/",
+        "linkTarget": "_self",
+        "image": "https://www.azion.com/assets/blog/images/uploads/thumbblog-why-choose-the-azion-waf.png",
+        "authors": [
+          {
+            "avatar": "https://www.azion.com/assets/blog/images/uploads/retrato_branco_quadrado.png",
+            "title": "Rafael Rigues",
+            "role": "Technical Researcher"
+          }
+        ],
+        "date": "JAN 18, 2024",
+        "estimateReadTime": "5 min read"
+      },
+    ],
+    "categories": [
+      "All Articles",
+      "Market Trends",
+      "Company News",
+      "Developers",
+      "Security",
+      "Observability",
+      "Serverless",
+      "Orchestration",
+      "Caching",
+      "Routing & Networking",
+      "Routing"
+    ],
+    "buttonText": "Show more",
+    "algoliaAppId": "PYJUZH6VNQ",
+    "algoliaApiKey": "7c1795c333053265edd2aeb199745797",
+    "algoliaIndex": [
+      {
+        "name": "azion-blog-en",
+        "label": "blog",
+        "activeIndex": 1
+      }
+    ],
+    "algoliaModel": [
+      {
+        "label": "Blog"
+      }
+    ]
+
+  },
+  featuredPost: {
+    "title": "Azion Edge SQL: enabling access and analysis of structured data at the edge",
+    "description": "Explore how Azion Edge SQL enhances database operations with edge computing for high scalability, low latency, and cost-effectiveness.",
+    "link": "/en/blog/azion-edge-sql/",
+    "image": "https://www.azion.com/assets/blog/images/uploads/thumbblog-openstandards-mobile.png",
+    "authors": [
+      {
+        "avatar": "https://www.azion.com/assets/blog/images/uploads/img-prof.png",
+        "title": "Mariana Bellorín Aguilera",
+        "role": "Technical Writer"
+      }
+    ],
+    "date": "JUN 27, 2024",
+    "estimateReadTime": "15 min read",
+    "linkTarget": "_self"
+  }
+
+
+}
+
+const template = `
+<div class="flex flex-col gap-10 md:gap-20 px-container w-full surface-ground">
+  <FeaturedPost v-bind="args.featuredPost" />
+  <CardGridBlock
+    :isSearchEnabled="args.isSearchEnabled"
+    :isLoadMoreEnabled="args.isLoadMoreEnabled"
+    cardType="blog"
+    v-bind="args.cards"
+  />
+</div>`
+
+const Template = (args) => ({
+  components: { FeaturedPost, CardGridBlock },
+  setup() {
+    return { args }
+  },
+  template: template
+})
+
+export const Default = Template.bind({})
+Default.args = MOCK
+
+Default.parameters = {
+  docs: {
+    description: {
+      story: ''
+    },
+    source: { code: template }
+  },
+};
