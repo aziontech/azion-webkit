6fc56e517bc93ce3010557823c87fd0c9f045169
Robson Junior <robson.ga.junior@gmail.com>
2024-08-25 18:01:31 -0300

    feat: adding card show case

---
+++ src/templates/cardshowcase/CardShowCase.d.ts
@@ -0,0 +1,48 @@
+/**
+ *
+ * cardshowcase
+ *
+ *
+ * @module cardshowcase
+ */
+import { VNode } from 'vue';
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers';
+
+/**
+ * Defines valid properties in cardshowcase component.
+ */
+export interface CardShowCaseProps {
+}
+
+/**
+ * Defines valid slots in Avatar component.
+ */
+export interface CardShowCaseSlots {
+  /**
+   * Content can easily be customized with the default slot instead of using the built-in modes.
+   */
+  default(): VNode[];
+}
+
+/**
+ * Defines valid emits in Avatar component.
+ */
+export interface CardShowCaseEmits {
+  /**
+   * Triggered when an error occurs while loading an image file.
+   */
+  error(event: Event): void;
+}
+
+/**
+ * @group Component
+ */
+declare class CardShowCase extends ClassComponent<CardShowCaseProps, CardShowCaseSlots, CardShowCaseEmits> { }
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    CardShowCase: GlobalComponentConstructor<CardShowCase>;
+  }
+}
+
+export default CardShowCase;
