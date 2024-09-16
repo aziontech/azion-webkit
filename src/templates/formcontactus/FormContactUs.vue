28bddf62827863631eec182479ea36545b960a41
Luis Igreja <lfsigreja@gmail.com>
2024-09-16 17:10:00 -0300

    feat: add fields (surname + phone) to contact form

--- src/templates/formcontactus/FormContactUs.vue
+++ src/templates/formcontactus/FormContactUs.vue
@@ -1,34 +1,112 @@
 <template>
-  <FormBlock :title="t.title" :description="t.description">
+  <FormBlock
+    :title="t.title"
+    :description="t.description"
+  >
     <template #content>
-      <form id="contact-us" class="w-full hs-form">
+      <form
+        id="contact-us"
+        class="w-full hs-form"
+      >
         <div class="flex flex-col gap-8">
           <div class="flex flex-column gap-2 max-w-xl">
-            <label class="text-sm" for="name">{{ t.name }}</label>
-            <InputText id="name" v-model="name" :placeholder="t.namePlaceholder" />
+            <label
+              class="text-sm"
+              for="name"
+              >{{ t.name }}</label
+            >
+            <InputText
+              id="name"
+              v-model="name"
+              :placeholder="t.namePlaceholder"
+            />
           </div>
+          <template v-if="t.lastName">
+            <div class="flex flex-column gap-2 max-w-xl">
+              <label
+                class="text-sm"
+                for="lastName"
+                >{{ t.lastName }}</label
+              >
+              <InputText
+                id="lastName"
+                v-model="lastName"
+                :placeholder="t.lastNamePlaceholder"
+              />
+            </div>
+          </template>
+          <template v-if="t.phone">
+            <div class="flex flex-column gap-2 max-w-xl">
+              <label
+                class="text-sm"
+                for="phone"
+                >{{ t.phone }}</label
+              >
+              <InputText
+                id="phone"
+                v-model="phone"
+                :placeholder="t.phonePlaceholder"
+              />
+            </div>
+          </template>
           <div class="flex flex-column gap-2 max-w-xl">
-            <label class="text-sm" for="email">{{ t.email }}</label>
-            <InputText v-model="email" id="email" class="w-full"
-              :placeholder="t.emailPlaceholder"/>
+            <label
+              class="text-sm"
+              for="email"
+              >{{ t.email }}</label
+            >
+            <InputText
+              v-model="email"
+              id="email"
+              class="w-full"
+              :placeholder="t.emailPlaceholder"
+            />
           </div>
           <div class="flex flex-column gap-2 max-w-xs">
-            <label class="text-sm" for="segment">{{ t.segment }}</label>
+            <label
+              class="text-sm"
+              for="segment"
+              >{{ t.segment }}</label
+            >
             <Dropdown
-              id="segment" v-model="segment" :options="t.segmentData" optionLabel="label"
-              :placeholder="t.segmentPlaceholder"/>
+              id="segment"
+              v-model="segment"
+              :options="t.segmentData"
+              optionLabel="label"
+              :placeholder="t.segmentPlaceholder"
+            />
           </div>
           <div class="flex flex-column gap-2">
-            <label class="text-sm" for="message">{{ t.message }}</label>
-            <Textarea id="message" v-model="message" rows="5" cols="30" />
+            <label
+              class="text-sm"
+              for="message"
+              >{{ t.message }}</label
+            >
+            <Textarea
+              id="message"
+              v-model="message"
+              rows="5"
+              cols="30"
+            />
           </div>
         </div>
       </form>
-      <div class="mt-2" v-if="responseStatus !== 'default'">
-        <InlineMessage v-if="responseStatus === 'success'" severity="success" class="flex justify-start">
+      <div
+        class="mt-2"
+        v-if="responseStatus !== 'default'"
+      >
+        <InlineMessage
+          v-if="responseStatus === 'success'"
+          severity="success"
+          class="flex justify-start"
+        >
           {{ responseMessages.success }}
         </InlineMessage>
-        <InlineMessage v-if="responseStatus === 'error'" severity="error" class="flex justify-start">
+        <InlineMessage
+          v-if="responseStatus === 'error'"
+          severity="error"
+          class="flex justify-start"
+        >
           {{ responseMessages.error }}
         </InlineMessage>
       </div>
@@ -37,14 +115,35 @@
       <div class="w-full flex flex-col gap-8">
         <div class="flex flex-col md:flex-row gap-6 justify-between w-full">
           <div class="flex gap-2 items-center">
-            <Checkbox v-model="termsAcceptance" aria-labelledby="terms" :binary="true" />
-            <label id="terms" class="text-sm"> {{ t.terms }}</label>
+            <Checkbox
+              v-model="termsAcceptance"
+              aria-labelledby="terms"
+              :binary="true"
+            />
+            <label
+              id="terms"
+              class="text-sm"
+            >
+              {{ t.terms }}</label
+            >
           </div>
-          <Button class="justify-center lg:justify-start" size="small" @click="onSubmit()"> {{ t.buttonText }} </Button>
+          <Button
+            class="justify-center lg:justify-start"
+            size="small"
+            @click="onSubmit()"
+          >
+            {{ t.buttonText }}
+          </Button>
         </div>
         <p class="text-color-secondary text-sm">
-          {{ t.azionPrivacy.description}}
-          <a class="text-[--text-color-link] font-medium hover:underline"  target="_blank" :href="t.azionPrivacy.link"> {{ t.azionPrivacy.buttonText}}</a>
+          {{ t.azionPrivacy.description }}
+          <a
+            class="text-[--text-color-link] font-medium hover:underline"
+            target="_blank"
+            :href="t.azionPrivacy.link"
+          >
+            {{ t.azionPrivacy.buttonText }}</a
+          >
         </p>
       </div>
     </template>
@@ -52,59 +151,60 @@
 </template>
 
 <script setup>
-import InputText from "primevue/inputtext";
-import Button from "primevue/button";
-import Textarea from 'primevue/textarea';
-import Dropdown from "primevue/dropdown";
-import Checkbox from 'primevue/checkbox';
-import InlineMessage from 'primevue/inlinemessage'
-import FormBlock from "../formblock/FormBlock.vue";
-import { hbspPostHandler } from "../src/services/hubspot-service"
-import { ref } from 'vue'
+  import InputText from 'primevue/inputtext'
+  import Button from 'primevue/button'
+  import Textarea from 'primevue/textarea'
+  import Dropdown from 'primevue/dropdown'
+  import Checkbox from 'primevue/checkbox'
+  import InlineMessage from 'primevue/inlinemessage'
+  import FormBlock from '../formblock/FormBlock.vue'
+  import { hbspPostHandler } from '../src/services/hubspot-service'
+  import { ref } from 'vue'
 
-const props = defineProps({
-  t: {
-    type: Object,
-    required: true
-  },
-  hubspot: {
-    formId: String,
-    companyId: String,
-  },
-  requester: {
-    type: String,
-    required: true
-  },
-  requesterName: {
-    type: String,
-    required: true
-  },
-  responseMessages: {
-    success: {
-      type: String,
-      required: false,
-      default: 'Thanks for subscribing to Azion newsletter'
+  const props = defineProps({
+    t: {
+      type: Object,
+      required: true
     },
-    error: {
+    hubspot: {
+      formId: String,
+      companyId: String
+    },
+    requester: {
       type: String,
-      required: false,
-      default: 'Error while subscribing to Azion newsletter, try again later'
+      required: true
+    },
+    requesterName: {
+      type: String,
+      required: true
+    },
+    responseMessages: {
+      success: {
+        type: String,
+        required: false,
+        default: 'Thanks for subscribing to Azion newsletter'
+      },
+      error: {
+        type: String,
+        required: false,
+        default: 'Error while subscribing to Azion newsletter, try again later'
+      }
     }
-  }
-})
+  })
 
-const { hubspot, t, requester, requesterName } = props
+  const { hubspot, t, requester, requesterName } = props
 
-const responseStatus = ref('default');
-const segment = ref("");
-const name = ref("")
-const email = ref("")
-const message = ref("")
-const termsAcceptance = ref(false)
+  const responseStatus = ref('default')
+  const segment = ref('')
+  const name = ref('')
+  const email = ref('')
+  const message = ref('')
+  const termsAcceptance = ref(false)
+  const lastName = ref('')
+  const phone = ref('')
 
-
-const onSubmit = async () => {
-  const requestBody = {
+  const onSubmit = async () => {
+    const requestBody = {
       fields: [
         {
           objectTypeId: '0-1',
@@ -125,7 +225,7 @@ const onSubmit = async () => {
           objectTypeId: '0-1',
           name: 'segmento_de_neg_cio',
           value: segment.value.value
-        },
+        }
       ],
       context: {
         pageUri: requester,
@@ -138,17 +238,32 @@ const onSubmit = async () => {
         }
       }
     }
-  const postURL = `https://api.hsforms.com/submissions/v3/integration/submit/${hubspot.companyId}/${hubspot.formId}`
 
-  const response = await hbspPostHandler(postURL, requestBody)
+    if (lastName.value) {
+      requestBody.fields.push({
+        objectTypeId: '0-1',
+        name: 'lastname',
+        value: lastName.value
+      })
+    }
 
-  if (response.status !== 200) {
-    responseStatus.value = 'error'
+    if (phone.value) {
+      requestBody.fields.push({
+        objectTypeId: '0-1',
+        name: 'phone',
+        value: phone.value
+      })
+    }
+    const postURL = `https://api.hsforms.com/submissions/v3/integration/submit/${hubspot.companyId}/${hubspot.formId}`
 
-    return
+    const response = await hbspPostHandler(postURL, requestBody)
+
+    if (response.status !== 200) {
+      responseStatus.value = 'error'
+
+      return
+    }
+
+    responseStatus.value = 'success'
   }
-
-  responseStatus.value = 'success'
-}
-
 </script>
