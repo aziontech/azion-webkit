06e227dc26135f405db80ea636c4cd757c9e9d18
Luis Igreja <lfsigreja@gmail.com>
2024-09-19 12:52:02 -0300

    feat: content section big numbers

---
+++ src/templates/sectionbignumbers/SectionBigNumbers.vue
@@ -0,0 +1,37 @@
+<template>
+  <ContentSection titleTag="h2" :overline="overline" :title="title">
+    <template v-if="button" #main>
+      <div class="flex justify-end lg:items-end">
+        <LinkButton v-bind="button" textLink />
+      </div>
+    </template>
+    <template #principal>
+      <BigNumbers v-bind="data" />
+    </template>
+  </ContentSection>
+</template>
+
+<script setup>
+  import ContentSection from '../contentsection'
+  import BigNumbers from '../bignumbers'
+  import LinkButton from '../linkbutton'
+
+  defineProps({
+    data: {
+      type: Object,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    overline: {
+      type: String,
+      required: false,
+    },
+    button: {
+      type: Object,
+      required: false,
+    }
+  });
+</script>
---
+++ src/templates/sectionbignumbers/sectionbignumbers.js
@@ -0,0 +1,2 @@
+import SectionBigNumbers from './SectionBigNumbers.vue';
+export default SectionBigNumbers;
