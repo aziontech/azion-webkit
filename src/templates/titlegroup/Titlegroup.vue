19b38efa4ae123ad4ca54afe7ca76acb730e67b4
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2024-10-01 17:41:09 -0300

    feat: update line height

--- src/templates/titlegroup/Titlegroup.vue
+++ src/templates/titlegroup/Titlegroup.vue
@@ -1,7 +1,7 @@
 <template>
   <hgroup>
     <p class="text-[--surface-700] text-3xl md:text-4xl font-medium">{{ subtitle }}</p>
-    <h1 class="text-3xl md:text-4xl font-medium text-balance">{{ title }}</h1>
+    <h1 class="text-3xl md:text-4xl font-medium text-balance" style="line-height: 125% !important;">{{ title }}</h1>
   </hgroup>
 </template>
 
