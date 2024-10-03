94d1092b63128c9c67d9831b46165063202869c5
Robson Junior <robson.ga.junior@gmail.com>
2024-10-03 10:15:24 -0300

    fix: resize push to wrong direction during browser resize

--- src/templates/sectioncarousel/SectionCarousel.vue
+++ src/templates/sectioncarousel/SectionCarousel.vue
@@ -102,16 +102,11 @@
     },
     {
       breakpoint: '1199px',
-      numVisible: 3,
-      numScroll: 1
-    },
-    {
-      breakpoint: '767px',
       numVisible: 2,
       numScroll: 1
     },
     {
-      breakpoint: '575px',
+      breakpoint: '767px',
       numVisible: 1,
       numScroll: 1
     }
