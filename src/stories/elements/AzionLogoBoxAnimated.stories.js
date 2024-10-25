50bc13a679bd2886fc195f7c03fe3fe5d0184255
Robson Junior <robson.ga.junior@gmail.com>
2024-10-25 08:54:53 -0300

    feat: adding azion logo box animated

---
+++ src/stories/elements/AzionLogoBoxAnimated.stories.js
@@ -0,0 +1,17 @@
+import AzionLogoBoxAnimated from '../../templates/azionlogoboxanimated'
+
+export default {
+  title: 'Elements/svgs/AzionLogoBoxAnimated',
+  component: AzionLogoBoxAnimated,
+  tags: ['autodocs'],
+  argTypes: {
+  }
+}
+
+export const Default = {
+  // args: {
+  //   hrefTitle: 'azion logo',
+  //   href: 'https://www.azion.com/en/',
+  //   target: '_self'
+  // }
+}
