752b4f96753c97ff9324d8bcd8be80b32daef014
Robson Junior <robson.ga.junior@gmail.com>
2024-12-18 10:25:28 -0300

    chore: wip in the netinfo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="px-container py-8 relative">
-    <Livemap lang="pt-br" />
-  </div>
+  <Container>
+    <ContentSection
+      titleTag="h1"
+      overline="Report"
+      title="Azion Netinfo"
+    >
+      <template #principal>
+        <Netinfo />
+      </template>
+    </ContentSection>
+  </Container>
 </template>
 
 <script setup>
-  import Livemap from './templates/livemap/Livemap.vue';
+  import Container from './templates/container/Container.vue'
+  import ContentSection from './templates/contentsection/ContentSection.vue'
+  import Netinfo from './templates/netinfo/Netinfo.vue'
 </script>
