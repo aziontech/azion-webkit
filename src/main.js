63838e28522bae62a6e35371c9494737424acc8d
robsongajunior <robson.ga.junior@gmail.com>
2025-02-25 16:46:29 -0300

    chore: remove comment

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
 import './assets/icons/azionicons.scss'
 import './assets/styles/main.css'
-import './assets/styles/avatar.css' // hack to load right
-import './assets/styles/hsform.css' // hubspot form
+import './assets/styles/avatar.css'
+import './assets/styles/hsform.css'
 import './assets/styles/text.css'
 import 'azion-theme'
 
