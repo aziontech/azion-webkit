298ff130a44ab87343ed2cfaac251ccd349a9e1e
peterpaulo-azion <peterson.silva@azion.com>
2025-03-17 10:57:10 -0300

    refactor: responsive gridhiglight layout added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,70 @@
   <Container>
     <GridHighlight :cards="MOCK.items" />
   </Container>
+    <div class="grid grid-cards grid-flow-row-dense grid-cols-5 gap-4">
+      <template
+        v-for="(item, index) in cards"
+        :key="index"
+      >
+        <div
+          v-if="item.description"
+          class="row-span-2"
+        >
+          <CardBaseClickable
+            :link="item.link"
+            disableAction
+          >
+            <template v-slot:header>
+              <div>
+                <img
+                  :src="item.image"
+                  alt="Image"
+                />
+              </div>
+            </template>
+            <template v-slot:content>
+              <div>
+                <p class="text-color-secondary">{{ item.description }}</p>
+              </div>
+            </template>
+          </CardBaseClickable>
+        </div>
+
+        <div
+          v-else
+          class="flex flex-col gap-4"
+        >
+          <CardBaseClickable
+            :link="item.link"
+            disableAction
+          >
+            <template v-slot:content>
+              <div>
+                <img
+                  :src="item.image"
+                  alt="Image"
+                />
+              </div>
+            </template>
+          </CardBaseClickable>
+          <CardBaseClickable
+            :link="cards[index + 1].link"
+            v-if="cards[index + 1]"
+            disableAction
+          >
+            <template v-slot:content>
+              <div>
+                <img
+                  :src="cards[index + 1].image"
+                  alt="Image"
+                />
+              </div>
+            </template>
+          </CardBaseClickable>
+        </div>
+      </template>
+    </div>
+  </div> -->
 </template>
 
 <script setup>
@@ -11,44 +75,47 @@
   const MOCK = {
     items: [
       {
-        description: 'Item 1',
+        description: '',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
       },
       {
-        description: 'Item 2',
+        description: '',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
       },
       {
-        description: 'Item 3',
+        description: 'VTEX builds ultra-low latency serverless applications with Azion and allows thousands of customers to accelerate their sales.',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
       },
       {
-        description: 'Item 4',
+        description: '',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
       },
       {
-        description: 'Item 5',
+        description: '',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
       },
       {
-        description: 'Item 6',
+        description: '',
         link: 'https://google.com',
         image:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
-      }
+      },
+      {
+        description: '',
+        link: 'https://google.com',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
+      },
     ]
   }
 </script>
-Sure! The translation of "feat: Substituir SectionImageRight por GridHighlight com mock de itens" into English is:
-
-"feat: Replace SectionImageRight with GridHighlight using mock items"
--- src/templates/gridhighlight/GridHighlight.vue
+++ src/templates/gridhighlight/GridHighlight.vue
@@ -1,65 +1,78 @@
 <template>
-  <div class="grid grid-cards grid-flow-row-dense auto-cols-max gap-4">
-    <template
-      v-for="(item, index) in cards"
-      :key="index"
-    >
-      <div
-        v-if="isTwoCardsColumn(index)"
-        class="flex flex-col gap-4"
+  <div class="min-h-screen p-6">
+    <div class="grid gap-4 m-0 grid-cols-[repeat(auto-fill,_220px_278px)] grid-rows-2">
+      <template
+        v-for="(item, index) in cards"
+        :key="index"
       >
-        <CardBaseClickable
-          :link="item.link"
-          disableAction
+        <div
+          v-if="isTwoCardsColumn(index)"
+          class="flex flex-col gap-4 w-100%"
         >
-          <template v-slot:content>
-            <div>
-              <img
-                :src="item.image"
-                alt="Image"
-              />
-            </div>
-          </template>
-        </CardBaseClickable>
-        <CardBaseClickable
-          :link="cards[index + 1].link"
-          v-if="cards[index + 1]"
-          disableAction
+          <CardBaseClickable
+            :link="item.link"
+            disableAction
+          >
+            <template v-slot:content>
+              <div class="flex justify-center">
+                <img
+                  :src="item.image"
+                  alt="Image"
+                  class="w-[7rem]"
+                />
+              </div>
+            </template>
+          </CardBaseClickable>
+          <CardBaseClickable
+            :link="cards[index + 1].link"
+            v-if="cards[index + 1]"
+            disableAction
+          >
+            <template v-slot:content>
+              <div class="flex justify-center">
+                <img
+                  :src="item.image"
+                  alt="Image"
+                  class="w-[7rem]"
+                />
+              </div>
+            </template>
+          </CardBaseClickable>
+        </div>
+        <div
+          v-if="isSingleCardColumn(index)"
+          class="row-span-2"
         >
-          <template v-slot:content>
-            <div>
-              <img
-                :src="cards[index + 1].image"
-                alt="Image"
-              />
-            </div>
-          </template>
-        </CardBaseClickable>
-      </div>
-      <div
-        v-if="isSingleCardColumn(index)"
-        class="row-span-2"
-      >
-        <CardBaseClickable
-          :link="item.link"
-          disableAction
-        >
-          <template v-slot:header>
-            <div>
-              <img
-                :src="item.image"
-                alt="Image"
-              />
-            </div>
-          </template>
-          <template v-slot:content>
-            <div>
-              <p class="text-color-secondary">{{ item.description }}</p>
-            </div>
-          </template>
-        </CardBaseClickable>
-      </div>
-    </template>
+          <CardBaseClickable
+            :link="item.link"
+            disableAction
+            class=""
+          >
+            <!-- <template v-slot:header>
+              <div>
+                <img
+                  :src="item.image"
+                  alt="Image"
+                  class="w-[7rem]"
+                />
+              </div>
+            </template> -->
+            <template v-slot:content>
+              <div class="mb-6">
+                <img
+                  :src="item.image"
+                  alt="Image"
+                  class="w-[7rem]"
+                />
+              </div>
+              <div>
+                <p class="text-color-secondary text-sm">{{ item.description }}</p>
+              </div>
+            </template>
+          </CardBaseClickable>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -78,9 +91,3 @@
   const isTwoCardsColumn = (index) => index % 3 === 0
   const isSingleCardColumn = (index) => index % 3 === 2
 </script>
-
-<style scoped>
-  .grid {
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  }
-</style>
