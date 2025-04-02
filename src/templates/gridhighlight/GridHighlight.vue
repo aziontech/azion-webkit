c4ee47158b6e6b1f7f27581baff6013f6093ec26
peterpaulo-azion <peterson.silva@azion.com>
2025-04-02 16:37:08 -0300

    refactor: adjust grid layout and adds svg support

--- src/templates/gridhighlight/GridHighlight.vue
+++ src/templates/gridhighlight/GridHighlight.vue
@@ -1,78 +1,157 @@
 <template>
   <div class="min-h-screen p-6">
-    <div class="grid gap-4 m-0 grid-cols-[repeat(auto-fill,_220px_278px)] grid-rows-2">
-      <template
-        v-for="(item, index) in cards"
-        :key="index"
-      >
-        <div
-          v-if="isTwoCardsColumn(index)"
-          class="flex flex-col gap-4 w-100%"
+    <template class="hidden lg:block">
+      <div class="grid gap-4 m-0 grid-cols-[repeat(auto-fit,_minmax(220px,_1fr))] grid-rows-2">
+        <template
+          v-for="(item, index) in cards"
+          :key="index"
         >
-          <CardBaseClickable
-            :link="item.link"
-            disableAction
+          <div
+            v-if="isTwoCardsColumn(index)"
+            class="flex flex-col gap-4 row-span-2"
           >
-            <template v-slot:content>
-              <div class="flex justify-center">
-                <img
-                  :src="item.image"
-                  alt="Image"
-                  class="w-[7rem]"
-                />
-              </div>
-            </template>
-          </CardBaseClickable>
-          <CardBaseClickable
-            :link="cards[index + 1].link"
-            v-if="cards[index + 1]"
-            disableAction
+            <CardBaseClickable
+              :link="item.link"
+              disableAction
+            >
+              <template v-slot:content>
+                <div class="flex justify-center">
+                  <div
+                    class="w-[7rem]"
+                    v-if="isSvgContent(item.image)"
+                    v-html="item.image"
+                  ></div>
+                  <img
+                    v-else
+                    :src="item.image"
+                    :alt="item.alt"
+                    class="w-[7rem]"
+                  />
+                </div>
+              </template>
+            </CardBaseClickable>
+
+            <CardBaseClickable
+              :link="cards[index + 1].link"
+              v-if="cards[index + 1]"
+              disableAction
+            >
+              <template v-slot:content>
+                <div class="flex justify-center">
+                  <div
+                    class="w-[7rem]"
+                    v-if="isSvgContent(item.image)"
+                    v-html="item.image"
+                  ></div>
+                  <img
+                    v-else
+                    :src="item.image"
+                    :alt="item.alt"
+                    class="w-[7rem]"
+                  />
+                </div>
+              </template>
+            </CardBaseClickable>
+          </div>
+          <div
+            v-if="isSingleCardColumn(index)"
+            class="row-span-2"
           >
-            <template v-slot:content>
-              <div class="flex justify-center">
-                <img
-                  :src="item.image"
-                  alt="Image"
-                  class="w-[7rem]"
-                />
-              </div>
-            </template>
-          </CardBaseClickable>
-        </div>
-        <div
-          v-if="isSingleCardColumn(index)"
-          class="row-span-2"
+            <CardBaseClickable
+              :link="item.link"
+              disableAction
+              class=""
+            >
+              <template v-slot:content>
+                <div class="mb-6">
+                  <div
+                    class="w-[7rem]"
+                    v-if="isSvgContent(item.image)"
+                    v-html="item.image"
+                  ></div>
+                  <img
+                    v-else
+                    :src="item.image"
+                    :alt="item.alt"
+                    class="w-[7rem]"
+                  />
+                </div>
+                <div>
+                  <p class="text-color-secondary text-sm">{{ item.description }}</p>
+                </div>
+              </template>
+            </CardBaseClickable>
+          </div>
+        </template>
+      </div>
+    </template>
+
+    <!-- Mobile Grid -->
+    <template class="block lg:hidden">
+      <div class="grid gap-4 m-0 grid-cols-2">
+        <template
+          v-for="(item, index) in cards"
+          :key="index"
         >
-          <CardBaseClickable
-            :link="item.link"
-            disableAction
-            class=""
+          <div
+            v-if="isLargeMobileCard(index)"
+            class="col-span-2 w-full"
           >
-            <!-- <template v-slot:header>
-              <div>
-                <img
-                  :src="item.image"
-                  alt="Image"
-                  class="w-[7rem]"
-                />
-              </div>
-            </template> -->
-            <template v-slot:content>
-              <div class="mb-6">
-                <img
-                  :src="item.image"
-                  alt="Image"
-                  class="w-[7rem]"
-                />
-              </div>
-              <div>
-                <p class="text-color-secondary text-sm">{{ item.description }}</p>
-              </div>
-            </template>
-          </CardBaseClickable>
-        </div>
-      </template>
-    </div>
+            <CardBaseClickable
+              :link="item.link"
+              disableAction
+            >
+              <template v-slot:content>
+                <div class="mb-6">
+                  <div
+                    class="w-[7rem]"
+                    v-if="isSvgContent(item.image)"
+                    v-html="item.image"
+                  ></div>
+                  <img
+                    v-else
+                    :src="item.image"
+                    :alt="item.alt"
+                    class="w-[7rem]"
+                  />
+                </div>
+                <div>
+                  <p class="text-color-secondary text-sm">
+                    {{ item.description }}
+                  </p>
+                </div>
+              </template>
+            </CardBaseClickable>
+          </div>
+
+          <div
+            v-else
+            class="w-full"
+          >
+            <CardBaseClickable
+              :link="item.link"
+              disableAction
+            >
+              <template v-slot:content>
+                <div class="flex justify-center">
+                  <div
+                    class="w-[7rem]"
+                    v-if="isSvgContent(item.image)"
+                    v-html="item.image"
+                  ></div>
+                  <img
+                    v-else
+                    :src="item.image"
+                    :alt="item.alt"
+                    class="w-[7rem]"
+                  />
+                </div>
+              </template>
+            </CardBaseClickable>
+          </div>
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -88,6 +167,9 @@
 
   const { cards } = props
 
+  const isSvgContent = (image) => image.trim().startsWith('<svg')
+
   const isTwoCardsColumn = (index) => index % 3 === 0
   const isSingleCardColumn = (index) => index % 3 === 2
+  const isLargeMobileCard = (index) => (index + 1) % 3 === 0
 </script>
