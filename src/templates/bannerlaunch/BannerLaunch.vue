8c9d2c9c66ce09006855fcc801483f8c168ff91d
robsongajunior <robson.ga.junior@gmail.com>
2025-04-14 10:45:29 -0300

    fix: BannerLaunch.vue validation/default of titleta

--- src/templates/bannerlaunch/BannerLaunch.vue
+++ src/templates/bannerlaunch/BannerLaunch.vue
@@ -82,12 +82,8 @@
   defineProps({
     titleTag: {
       type: String,
-      default() {
-        return 'h2'
-      },
-      validator(value) {
-        return ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(value)
-      }
+      default: () => 'h1',
+      validator: (value) => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(value)
     },
     title: {
       type: String,
