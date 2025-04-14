2f8abffceace2ebde14f0dde1c115611eb1886f8
Eduardo de Cesaro <cesaroeduardo@icloud.com>
2025-04-14 16:10:41 -0300

    fix: update banner launch

--- src/stories/templates/banner/banner-launch.stories.js
+++ src/stories/templates/banner/banner-launch.stories.js
@@ -9,10 +9,10 @@ export default {
 const MOCK = {
   titleTag: 'h1',
   title:
-    "Product</br><span class='animate-pretty bg-clip-text text-transparent bg-gradient-to-r from-orange-300 to-orange-500'>LaunchWeek#01</span></br>May 05-06",
+    "PRODUCT</br><i class='text-orange-500 font-mono not-italic font-bold'>LAUNCHWEEK#01</i></br>MAY 05-06",
   description: '2 days of new features</br>and ways change the game',
-  imageDark: 'https://www.azion.com/assets/images/components/bannerlaunch/a_dark.png',
-  imageLight: 'https://www.azion.com/assets/images/components/bannerlaunch/a_light.png'
+  imageDark: 'https://i.imgur.com/zLE6fPd.png',
+  imageLight: 'https://i.imgur.com/4i2vTxM.png'
 }
 
 const template = `
--- src/templates/bannerlaunch/BannerLaunch.vue
+++ src/templates/bannerlaunch/BannerLaunch.vue
@@ -7,49 +7,49 @@
     >
       <template #content-raw>
         <div
-          class="flex flex-col md:flex-row justify-between md:gap-8 items-center md:pl-10 md:pl-20 md:lg:pl-40"
+          class="flex flex-col surface-border border rounded-lg md:flex-row justify-between md:gap-8 items-center md:pl-8 lg:pl-16"
         >
           <div class="flex flex-col gap-8 py-10 md:py-0">
             <h1
               v-if="titleTag === 'h1'"
               v-html="title"
-              class="leading-loose font-medium text-heading-3 text-balance"
+              class="leading-loose text-heading-3  text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
             <h2
               v-if="titleTag === 'h2'"
               v-html="title"
-              class="text-heading-3 leading-relaxed font-medium text-balance"
+              class="text-heading-3 leading-relaxed text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
             <h3
               v-if="titleTag === 'h3'"
               v-html="title"
-              class="text-heading-3 leading-relaxed font-medium text-balance"
+              class="text-heading-3 leading-relaxed text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
             <h4
               v-if="titleTag === 'h4'"
               v-html="title"
-              class="text-heading-3 leading-relaxed font-medium text-balance"
+              class="text-heading-3 leading-relaxed text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
             <h5
               v-if="titleTag === 'h5'"
               v-html="title"
-              class="text-heading-3 leading-relaxed font-medium text-balance"
+              class="text-heading-3 leading-relaxed text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
             <h6
               v-if="titleTag === 'h6'"
               v-html="title"
-              class="text-heading-3 leading-relaxed font-medium text-balance"
+              class="text-heading-3 leading-relaxed text-balance font-mono uppercase font-bold"
               style="line-height: 114% !important"
             />
 
             <p
               v-html="description"
-              class="text-overline text-color-secondary text-sm lg:text-base"
+              class="text-body-2 font-mono uppercase text-color-secondary"
               style="line-height: 150% !important; letter-spacing: 5%"
             />
           </div>
