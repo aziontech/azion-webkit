bc066888602f456e51d7fd40833a36aafc48ef2a
robsongajunior <robson.ga.junior@gmail.com>
2025-04-15 01:38:37 -0300

    chore: storybook image to light but using dark image

--- src/stories/templates/banner/banner-launch-background.stories.js
+++ src/stories/templates/banner/banner-launch-background.stories.js
@@ -12,7 +12,7 @@ const MOCK = {
     "Product</br><i class='text-orange-500 font-mono not-italic font-bold'>LaunchWeek#01</i></br>May 05-06",
   description: '2 days of new features</br>and ways change tnpmhe game',
   imageDark: 'https://www.azion.com/assets/images/components/bannerlaunch/a-background-wide.png',
-  imageLight: 'https://www.azion.com/assets/images/components/bannerlaunch/a-background.png'
+  imageLight: 'https://www.azion.com/assets/images/components/bannerlaunch/a-background-wide.png'
 }
 
 const template = `
