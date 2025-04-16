f0c9d12883918ee3637cb828f0da499d027f985d
robsongajunior <robson.ga.junior@gmail.com>
2025-04-16 17:13:41 -0300

    feat: added sectionbasiccontent

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@
     titleTag: 'h2',
     // title: "Product</br><i class='text-orange-500 font-mono not-italic font-bold'>LaunchWeek#01</i></br>May 05-06",
     title: 'Construa com a Azion',
-    description: 'Atendemos várias das maiores empresas globais. Isso significa que você usa a Azion diariamente, quando compra, se comunica e até mesmo quando trabalha. Procuramos pessoas talentosas que compartilhem nossos valores de inovação, trabalho coletivo e a missão de impulsionar uma economia hiperconectada. ',
+    description:
+      'Atendemos várias das maiores empresas globais. Isso significa que você usa a Azion diariamente, quando compra, se comunica e até mesmo quando trabalha. Procuramos pessoas talentosas que compartilhem nossos valores de inovação, trabalho coletivo e a missão de impulsionar uma economia hiperconectada. ',
     wysisyg: false,
     buttons: [
       {
---
+++ src/templates/sectionbasiccontent/SectionBasicContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <ContentSection
+    position="center"
+    isContentCentralized
+    :overline="overline"
+    :titleTag="titleTag"
+    :title="title"
+    :description="description"
+    :wysisyg="wysisyg"
+  >
+    <template #actions>
+      <template
+        v-for="(button, index) in buttons"
+        :key="index"
+      >
+        <LinkButton
+          v-if="button.link"
+          v-bind="button"
+        />
+      </template>
+    </template>
+  </ContentSection>
+</template>
+
+<script setup>
+  import ContentSection from '../contentsection'
+  import LinkButton from '../linkbutton'
+
+  defineProps({
+    overline: {
+      type: String
+    },
+    titleTag: {
+      type: String,
+      default: () => 'h2',
+      validator: (value) => ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(value)
+    },
+    title: {
+      type: String,
+      rquired: true
+    },
+    wysisyg: {
+      type: Boolean,
+      default: () => false
+    },
+    description: {
+      type: String
+    },
+    buttons: {
+      type: Array,
+      default: () => []
+    }
+  })
+</script>
