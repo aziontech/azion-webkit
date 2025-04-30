528b69802a599a01a007b450a45a238e4f0bc134
robsongajunior <robson.ga.junior@gmail.com>
2025-04-30 14:58:11 -0300

    fix: rollback the deleted sectionvideoright

---
+++ src/stories/templates/sections/section-video-right.stories.js
@@ -0,0 +1,105 @@
+import SectionVideoRight from '../../../templates/sectionvideoright'
+import Rules from '../../rules'
+
+const MOCK = {
+  overline: 'ABOUT US',
+  titleTag: 'h2',
+  title: 'Our mission is to power the hyper-connected economy',
+  description: 'From the beginning, our goal has been to create technologies that unlock new possibilities.',
+  video: {
+    title: 'Azion Overview',
+    src: 'https://www.youtube.com/embed/your_video_id',
+    image: {
+      alt: "Image of a globe illustrating Azion's connections",
+      dark: {
+        src: 'https://www.azion.com/assets/pages/about/globe-about.png'
+      },
+      light: {
+        src: 'https://www.azion.com/assets/pages/about/globe-about-light.png'
+      }
+    }
+  }
+}
+
+const MOCK_WITH_CARDS = {
+  ...MOCK,
+  cards: [
+    {
+      title: 'Release',
+      description: 'Release conidently and consistently.',
+      cta: {
+        link: '',
+        action: {
+          label: 'Learn more'
+        }
+      }
+    },
+    {
+      title: 'Target',
+      description: 'Deliver personalized experiences',
+      cta: {
+        link: '',
+        action: {
+          label: 'Learn more'
+        }
+      }
+    },
+    {
+      title: 'Remediate',
+      description: 'Find and fix issues faster.',
+      cta: {
+        link: '',
+        action: {
+          label: 'Learn more'
+        }
+      }
+    },
+    {
+      title: 'Experiment',
+      description: 'Continuously measure and optimize.',
+      cta: {
+        link: '',
+        action: {
+          label: 'Learn more'
+        }
+      }
+    }
+  ]
+}
+
+const config = (args) => {
+  return {
+    components: { SectionVideoRight },
+    setup() {
+      return { args }
+    },
+    template: '<SectionVideoRight v-bind="args" />'
+  }
+}
+
+const Template = (args) => config(args)
+export const Default = Template.bind({})
+Default.args = MOCK
+
+const TemplateWithCards = (args) => config(args)
+export const DefaultWithCards = TemplateWithCards.bind({})
+DefaultWithCards.args = MOCK_WITH_CARDS
+
+export default {
+  title: 'Blocks/Sections/section-video-right',
+  component: SectionVideoRight,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+### Content rules
+${Rules.overline}
+${Rules.title}
+${Rules.description}
+${Rules.cards}
+        `
+      }
+    }
+  }
+}
---
+++ src/templates/sectionvideoright/SectionVideoRight.vue
@@ -0,0 +1,165 @@
+<template>
+  <ContentSection
+    justify="center"
+    :overline="overline"
+    :titleTag="titleTag"
+    :title="title"
+    :description="description"
+    :descriptionRawHtml="descriptionRawHtml"
+  >
+    <template #main>
+      <BaseModal backgroundColor="outlined">
+        <template #action>
+          <div class="relative flex items-center justify-center group">
+            <img
+              class="hide-on-light"
+              width="auto"
+              height="340"
+              fetchpriority="high"
+              :alt="video.image.alt"
+              :src="video.image.dark.src"
+            />
+
+            <img
+              class="hide-on-dark"
+              width="auto"
+              height="340"
+              fetchpriority="high"
+              :alt="video.image.alt"
+              :src="video.image.light.src"
+            />
+
+            <div class="absolute opacity-50 transition-all ease group-hover:opacity-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="45"
+                height="45"
+                fill="currentColor"
+                viewBox="0 0 45 45"
+              >
+                <g id="play button">
+                  <g id="Subtract">
+                    <mask id="path-1-inside-1_8160_2062">
+                      <path
+                        fill-rule="evenodd"
+                        d="M22.4 44.8a22.4 22.4 0 1 0 0-44.8 22.4 22.4 0 0 0 0 44.8Zm-5-32.7v20.5a1 1 0 0 0 1.6.7L31 23a1 1 0 0 0 0-1.5L19 11.3a1 1 0 0 0-1.6.8Z"
+                        clip-rule="evenodd"
+                      />
+                    </mask>
+                    <path
+                      fill-rule="evenodd"
+                      d="M22.4 44.8a22.4 22.4 0 1 0 0-44.8 22.4 22.4 0 0 0 0 44.8Zm-5-32.7v20.5a1 1 0 0 0 1.6.7L31 23a1 1 0 0 0 0-1.5L19 11.3a1 1 0 0 0-1.6.8Z"
+                      clip-rule="evenodd"
+                    />
+                    <path
+                      d="m19 33.3 2 2.3-2-2.3ZM31 23l2 2.3-2-2.3Zm0-1.5-1.9 2.3 2-2.3ZM19 11.3 21 9l-2 2.3Zm22.8 11.1c0 10.8-8.6 19.4-19.4 19.4v6c14 0 25.4-11.3 25.4-25.4h-6ZM22.4 3c10.8 0 19.4 8.7 19.4 19.4h6C47.8 8.4 36.5-3 22.4-3v6ZM3 22.4C3 11.7 11.7 3 22.4 3v-6C8.4-3-3 8.4-3 22.4h6Zm19.4 19.4A19.4 19.4 0 0 1 3 22.4h-6c0 14 11.4 25.4 25.4 25.4v-6Zm-2-9.2V12h-6v20.5h6ZM17 31a2 2 0 0 1 3.4 1.5h-6a4 4 0 0 0 6.6 3L17 31Zm12.1-10.4L17.1 31l3.9 4.5 12-10.3-3.9-4.6Zm0 3a2 2 0 0 1 0-3l4 4.6a4 4 0 0 0-.1-6.1l-4 4.7Zm-12-10 12 10 3.9-4.5L21 9l-4 4.6Zm3.3-1.6a2 2 0 0 1-3.3 1.5L20.9 9a4 4 0 0 0-6.5 3h6Z"
+                      mask="url(#path-1-inside-1_8160_2062)"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </template>
+
+        <template #content>
+          <div class="md:aspect-video lg:h-[480px] md:h-[400px] h-[220px] rounded">
+            <iframe
+              :src="video.src"
+              :title="video.title"
+              class="h-full w-full rounded"
+              allowfullscreen
+              height="480"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            >
+            </iframe>
+          </div>
+        </template>
+      </BaseModal>
+    </template>
+
+    <template
+      #principal
+      v-if="cards"
+    >
+      <div class="grid m-0 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 w-full">
+        <CardBaseClickable
+          v-for="card in cards"
+          v-bind="card.cta"
+          :key="card.title"
+        >
+          <template #content>
+            <div class="flex flex-col gap-8">
+              <Tile>
+                <i class="text-xs pi pi-check"></i>
+              </Tile>
+              <div class="flex flex-col gap-3">
+                <CardTitle>
+                  {{ card.title }}
+                </CardTitle>
+                <CardDescription>
+                  {{ card.description }}
+                </CardDescription>
+              </div>
+            </div>
+          </template>
+        </CardBaseClickable>
+      </div>
+    </template>
+  </ContentSection>
+</template>
+
+<script setup>
+  import ContentSection from '../contentsection'
+  import BaseModal from '../basemodal'
+  import CardBaseClickable from '../cardbaseclickable'
+  import CardTitle from '../cardtitle'
+  import CardDescription from '../carddescription'
+  import Tile from '../tile'
+
+  defineProps({
+    overline: {
+      type: String,
+      default: () => ''
+    },
+    title: {
+      type: String,
+      default: () => ''
+    },
+    titleTag: {
+      type: String,
+      default: 'h2'
+    },
+    description: {
+      type: String,
+      default: () => ''
+    },
+    descriptionRawHtml: {
+      type: String,
+      default: () => ''
+    },
+    video: {
+      type: Object,
+      required: true,
+      validator: (value) => {
+        return (
+          ['title', 'src', 'image'].every((key) => key in value) &&
+          ['alt', 'dark', 'light'].every((key) => key in value.image) &&
+          ['src'].every((key) => key in value.image.dark && key in value.image.light)
+        )
+      }
+    },
+    cards: {
+      type: Array,
+      required: false,
+      validator: (value) => {
+        return value.every(
+          (item) =>
+            ['title', 'description', 'cta'].every((key) => key in item) &&
+            ['link', 'action'].every((key) => key in item.cta) &&
+            'label' in item.cta.action
+        )
+      }
+    }
+  })
+</script>
