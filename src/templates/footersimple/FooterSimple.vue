6615223c08ef51366cb58df9aeac47b3eeee236b
robsongajunior <robson.ga.junior@gmail.com>
2025-06-11 10:29:17 -0300

    fix: Footer simple replace px-container class to px-shell

--- src/templates/footersimple/FooterSimple.vue
+++ src/templates/footersimple/FooterSimple.vue
@@ -1,8 +1,13 @@
 <template>
   <footer class="flex flex-col justify-center items-center">
     <Divider />
-    <div class="px-container py-4" 
-    :class="{ 'w-full flex gap-3 flex-col md:flex-row justify-between md:items-center': $slots['theme-switch'] }">
+    <div
+      class="px-shell py-4"
+      :class="{
+        'w-full flex gap-3 flex-col md:flex-row justify-between md:items-center':
+          $slots['theme-switch']
+      }"
+    >
       <div v-if="$slots['theme-switch']">
         <slot name="theme-switch" />
       </div>
