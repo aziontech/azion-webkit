4ff89c1be04d30552ff6752958fc70661ae86cac
robsongajunior <robson.ga.junior@gmail.com>
2025-06-25 10:46:36 -0300

    fix: renaming file QuoteCarousel.ts --> QuoteCarousel.d.ts

---
+++ src/templates/quotecarousel/QuoteCarousel.d.ts
@@ -0,0 +1,56 @@
+/**
+ *
+ * QuoteCarousel
+ *
+ *
+ * @module `quotecarousel`
+ */
+import { VNode } from 'vue'
+import { ClassComponent, GlobalComponentConstructor } from '../ts-helpers'
+// import { CardReleaseProps } from '../cardrelease/CardRelease'
+
+/**
+ * Defines valid properties in QuoteCarousel component.
+ */
+export interface QuoteCarouselProps {
+  data: Array<[]> // <CardReleaseProps>;
+  numVisible: Number
+  numScroll: Number
+}
+
+/**
+ * Defines valid slots in QuoteCarousel component.
+ */
+export interface QuoteCarouselSlots {
+  /**
+   * Content can easily be customized with the default slot instead of using the built-in modes.
+   */
+  default(): VNode[]
+}
+
+/**
+ * Defines valid emits in QuoteCarousel component.
+ */
+export interface QuoteCarouselEmits {
+  /**
+   * Triggered when an error occurs
+   */
+  error(event: Event): void
+}
+
+/**
+ * @group Component
+ */
+declare class QuoteCarousel extends ClassComponent<
+  QuoteCarouselProps,
+  QuoteCarouselSlots,
+  QuoteCarouselEmits
+> {}
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    QuoteCarousel: GlobalComponentConstructor<QuoteCarousel>
+  }
+}
+
+export default QuoteCarousel
