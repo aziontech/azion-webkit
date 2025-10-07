d0edc6419d23502dbd76a81616630481d6cbe27e
lfsigreja <lfsigreja@gmail.com>
2025-10-07 19:17:28 +0100

    feat: bento clients grid

---
+++ src/components/BentoClients/CardCompanyLogo.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="rounded flex items-center justify-center" :class="cardBackgroundColor[type]">
+        <div class="h-12 flex items-center justify-center">
+          <div
+                v-html="logo.src"
+                :alt="logo.alt"
+               class="max-h-full" :class="cardTextColor[type]"
+              />   
+        </div>
+    </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    type: {
+      type: String,
+      default: 'default',
+      options: ['default', 'orange', 'white', 'lavander']
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    action: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    logo: {
+      type: Object,
+      default: () => ({
+        src: '',
+        alt: ''
+      })
+    }
+  })
+
+  const cardBackgroundColor = {
+    default: 'bg-neutral-900',
+    orange: 'bg-orange-500',
+    lavander: 'bg-violet-500',
+    white: 'bg-white'
+  }
+
+  const cardTextColor = {
+    default: 'text-white',
+    orange: 'text-white', 
+    lavander: 'text-gray-950',
+    white: 'text-black'
+  }
+</script>
---
+++ src/components/BentoClients/ClientProfile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="px-8 py-4 rounded bg-cover bg-center bg-no-repeat h-full flex items-end" :style="{ backgroundImage: backgroundImage }">
+        <p class="text-xl font-sora"> {{ title }}</p>
+    </div>
+</template>
+
+<script setup>
+  import { computed } from 'vue'
+  const props = defineProps({
+    title: {
+      type: String,
+      default: ''
+    },
+    backgroundImage: {
+        type: String,
+        default: ''
+    }
+  })
+
+  const backgroundImage = computed(() => {
+    return `url(${props.backgroundImage})`
+  })
+</script>
