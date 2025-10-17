758365e7bb75d905c2196cc2d3965b017c20c319
Isaque Böck <isaquebockdos@gmail.com>
2025-10-17 08:55:28 -0300

    fix: update quote component with inverted logo and neutral color scheme

--- src/components/Quote/Quote.vue
+++ src/components/Quote/Quote.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex flex-col gap-5">
-    <div
-      class="text-white"
-      v-html="logo"
+  <div class="flex flex-col gap-3">
+    <img
+      :src="`${logo}?ims=x60`"
+      loading="lazy"
+      class="max-h-[60px] brightness-0 invert"
+      width="auto"
+      height="60"
+      :alt="source"
     />
-    <h3 class="text-xl font-sora text-gray-200">{{ testimonial }}</h3>
-    <p class="text-sm font-sora text-gray-500">{{ source }}</p>
+    <h3 class="text-base font-sora text-neutral-200">{{ testimonial }}</h3>
+    <p class="text-sm font-sora text-neutral-500">{{ source }}</p>
   </div>
 </template>
 
