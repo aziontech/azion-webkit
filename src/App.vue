566b19c76017c5003be7f45e0f6cd1a477779652
Isaque Böck <isaquebockdos@gmail.com>
2025-10-20 14:54:51 -0300

    fix: unused product grid cards section and component import

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,10 @@
         version="min"
       />
     </Container>
-    
-    <SectionProductGridCards
-      :cards="[
-        {
-          icon: 'pi pi-folder',
-          title: 'Bullet benefit',
-          description: 'Melhore a performance, segurança e escalabilidade dos seus sites e aplicações web'
-        },
-        {
-          icon: 'pi pi-folder',
-          title: 'Bullet benefit',
-          description: 'Melhore a performance, segurança e escalabilidade dos seus sites e aplicações web'
-        },
-        {
-          icon: 'pi pi-folder',
-          title: 'Bullet benefit',
-          description: 'Melhore a performance, segurança e escalabilidade dos seus sites e aplicações web'
-        }
-      ]"
-    />
   </div>
 </template>
 
 <script setup>
   import Container from './templates/container'
   import AzionLogo from './templates/azionlogo'
-  import SectionProductGridCards from './blocks/SectionProductGridCards'
 </script>
