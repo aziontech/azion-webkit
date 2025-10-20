994b1b2d1010e57254f9f81319cba7352fdc7d58
Isaque Böck <isaquebockdos@gmail.com>
2025-10-20 14:22:52 -0300

    fix: corrige estrutura do SectionGetStarted e remove exemplo do App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,10 @@
       />
     </Container>
     
-    <ProductHero
-      overline="Edge Computing"
-      title="Build Faster with Azion Edge Platform"
-      subtitle="Deploy your applications at the edge with unmatched performance, security, and scalability."
-      image="https://placehold.co/600x600/1a1a1a/f3652b?text=Product+Image"
-      :buttons="[
-        { label: 'Start Now', href: '/signup' },
-        { label: 'Learn More', href: '/docs' }
-      ]"
-    />
   </div>
 </template>
 
 <script setup>
   import Container from './templates/container'
   import AzionLogo from './templates/azionlogo'
-  import ProductHero from './blocks/ProductHero'
 </script>
