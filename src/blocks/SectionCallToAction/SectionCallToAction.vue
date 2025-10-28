79671c4ea3f49811d3dc7e3915f0091850866cc7
lfsigreja <lfsigreja@gmail.com>
2025-10-28 19:50:26 +0000

    fix: responsivity and import

--- src/blocks/SectionCallToAction/SectionCallToAction.vue
+++ src/blocks/SectionCallToAction/SectionCallToAction.vue
@@ -7,19 +7,19 @@
     <div v-if="content && type === '2-col-70-30'" class="lg:col-span-3 w-full flex flex-col justify-between gap-6 bg-neutral-900 rounded-md p-6 md:p-12">
       <div class="flex flex-col gap-3">
         <Overline color="orange"> {{ content.overline }} </Overline>
-        <h2 class="text-2xl font-sora">{{ content.title }}</h2>
+        <h2 class="text-2xl text-neutral-200 font-sora">{{ content.title }}</h2>
         <p class="text-neutral-400 font-sora" v-html="parseMarkdown(content.descriptionRawMarkdown)"></p>
       </div>
       <Button v-bind="content.button" type="linkSecondary" />
     </div>
     <div class="flex flex-col justify-between rounded-md p-6 md:p-12" 
          :class="[ctaColor[type]]">
-      <div class="flex gap-3" :class="type.includes('short') ? 'flex justify-between' : 'flex-col'">
+      <div class="flex gap-3" :class="type.includes('short') ? 'flex flex-col md:flex-row justify-between' : 'flex-col'">
         <Overline color="black"> {{ cta.overline }} </Overline>
         <p class="max-w-sm text-balance font-sora text-xl text-neutral-900" v-html="parseMarkdown(cta.descriptionRawMarkdown)"></p>
       </div>
       <div class="flex justify-between items-end">
-        <h2 class="max-w-sm font-sora font-bold text-5xl text-neutral-900">{{ cta.title }}</h2>
+        <h2 class="max-w-sm font-sora font-bold gap-2 text-4xl md:text-5xl text-neutral-900">{{ cta.title }}</h2>
         <Button v-bind="cta.button" icon="pi pi-angle-right" type="primary" />
       </div>
     </div>
@@ -29,7 +29,7 @@
 <script setup lang="ts">
   import Button from '../../components/Button/Button.vue'
   import Overline from '../../components/Overline/Overline.vue'
-  import { parseMarkdown } from '../../services/markdown-service'
+  import { parseMarkdown } from '../../src/services/markdown-service'
 
   interface CardProps {
     overline: string
