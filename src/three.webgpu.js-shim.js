8d79fc1cda39443d867a1a99b8090d69905218e0
lfsigreja <lfsigreja@gmail.com>
2026-01-06 20:11:27 +0000

    refactor: remove three

--- src/three.webgpu.js-shim.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-// Create a mock WebGPURenderer if you don't actually need the real implementation
-export class WebGPURenderer {
-  constructor() {
-    console.warn('WebGPURenderer shim is being used instead of actual implementation')
-    return new THREE.WebGLRenderer()
-  }
-}
